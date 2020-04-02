24420b1f8a1515d4fba3fb90c7426b6e4f4c442d
andy840119 <andy840119@gmail.com>
2020-04-02 18:18:46 +0900

    Add legacy column object and skin lookup

---
+++ osu.Game.Rulesets.Karaoke/Skinning/LegacyKaraokeColumnElement.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Bindables;
+using osu.Game.Rulesets.Karaoke.UI;
+using osu.Game.Skinning;
+
+namespace osu.Game.Rulesets.Karaoke.Skinning
+{
+    public class LegacyKaraokeColumnElement : LegacyKaraokeElement
+    {
+        protected NotePlayfield NotePlayfield => Playfield.NotePlayfield;
+
+        // TODO : get current index
+        protected override IBindable<T> GetKaraokeSkinConfig<T>(ISkin skin, LegacyKaraokeSkinConfigurationLookups lookup, int? index = null)
+            => base.GetKaraokeSkinConfig<T>(skin, lookup, index);
+    }
+}
