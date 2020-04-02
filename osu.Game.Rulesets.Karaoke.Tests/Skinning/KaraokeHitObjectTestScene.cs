8edad19c3d5e1d9739d704e31c4c2060334440f8
andy840119 <andy840119@gmail.com>
2020-04-02 21:00:32 +0900

    Add note testscene

---
+++ osu.Game.Rulesets.Karaoke.Tests/Skinning/KaraokeHitObjectTestScene.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Timing;
+using osu.Game.Rulesets.Karaoke.UI.Components;
+using osu.Game.Rulesets.Objects.Drawables;
+using osu.Game.Rulesets.UI.Scrolling;
+using osuTK.Graphics;
+
+namespace osu.Game.Rulesets.Karaoke.Tests.Skinning
+{
+    /// <summary>
+    /// A test scene for a karaoke hitobject.
+    /// </summary>
+    public abstract class KaraokeHitObjectTestScene : KaraokeSkinnableTestScene
+    {
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            SetContents(() => new FillFlowContainer
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                RelativeSizeAxes = Axes.Both,
+                Height = 0.7f,
+                Direction = FillDirection.Horizontal,
+                Children = new Drawable[]
+                {
+                    new NotePlayfieldTestContainer(COLUMN_NUMBER)
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        RelativeSizeAxes = Axes.X,
+                        Height = ColumnBackground.COLUMN_HEIGHT,
+                        Child = new ScrollingHitObjectContainer
+                        {
+                            RelativeSizeAxes = Axes.Both,
+                            Clock = new FramedClock(new StopwatchClock()),
+                        }.With(c =>
+                        {
+                            c.Add(CreateHitObject().With(h => h.AccentColour.Value = Color4.Orange));
+                        })
+                    },
+                }
+            });
+        }
+
+        protected abstract DrawableHitObject CreateHitObject();
+    }
+}
