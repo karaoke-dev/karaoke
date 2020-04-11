4ce12d25f7bd426da3345f858a8a2e3f2b4587e5
andy840119 <andy840119@gmail.com>
2020-04-11 14:43:36 +0900

    Add hitting step in testcase

--- osu.Game.Rulesets.Karaoke.Tests/Skinning/TestSceneNote.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Skinning/TestSceneNote.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
+using osu.Framework.Bindables;
+using osu.Framework.Testing;
 using osu.Game.Beatmaps;
 using osu.Game.Beatmaps.ControlPoints;
 using osu.Game.Rulesets.Karaoke.Objects;
@@ -11,6 +14,17 @@ namespace osu.Game.Rulesets.Karaoke.Tests.Skinning
 {
     public class TestSceneNote : KaraokeHitObjectTestScene
     {
+        public TestSceneNote()
+        {
+            AddToggleStep("toggle hitting", v =>
+            {
+                foreach (var holdNote in CreatedDrawables.SelectMany(d => d.ChildrenOfType<DrawableNote>()))
+                {
+                    ((Bindable<bool>)holdNote.IsHitting).Value = v;
+                }
+            });
+        }
+
         protected override DrawableHitObject CreateHitObject()
         {
             var note = new Note
