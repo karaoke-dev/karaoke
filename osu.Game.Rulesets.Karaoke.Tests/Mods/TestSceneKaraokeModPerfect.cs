08167c864e94516ea6aabcaae9381978133869c6
andy840119 <andy840119@gmail.com>
2020-04-13 22:47:14 +0900

    Add karaoke mod perfect test case.

---
+++ osu.Game.Rulesets.Karaoke.Tests/Mods/TestSceneKaraokeModPerfect.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Rulesets.Karaoke.Mods;
+using osu.Game.Rulesets.Karaoke.Objects;
+using osu.Game.Tests.Visual;
+using System.Collections.Generic;
+
+namespace osu.Game.Rulesets.Karaoke.Tests.Mods
+{
+    public class TestSceneKaraokeModPerfect : ModPerfectTestScene
+    {
+        public TestSceneKaraokeModPerfect()
+            : base(new KaraokeRuleset(), new KaraokeModPerfect())
+        {
+        }
+
+        [TestCase(false)]
+        [TestCase(true)]
+        public void TestLyric(bool shouldMiss) => CreateHitObjectTest(new HitObjectTestData(new LyricLine
+        {
+            StartTime = 1000,
+            EndTime = 2000,
+            Text = "カラオケ!",
+            TimeTags = new Dictionary<TimeTagIndex, double>()
+        }), shouldMiss);
+
+        [TestCase(false)]
+        [TestCase(true)]
+        public void TestNote(bool shouldMiss) => CreateHitObjectTest(new HitObjectTestData(new Note
+        {
+            StartTime = 1000,
+            EndTime = 2000,
+            Text = "カ-"
+        }), shouldMiss);
+    }
+}
