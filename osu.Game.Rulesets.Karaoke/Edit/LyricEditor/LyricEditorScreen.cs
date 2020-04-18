79d9f754b78b97396c5c978900758a6fd738a8ad
andy840119 <andy840119@gmail.com>
2020-04-18 15:21:32 +0900

    Add initial screen and testcase

---
+++ osu.Game.Rulesets.Karaoke/Edit/LyricEditor/LyricEditorScreen.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+using osu.Game.Graphics.Containers;
+using osu.Game.Screens.Edit;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.LyricEditor
+{
+    public class LyricEditorScreen : EditorScreenWithTimeline
+    {
+        protected override Drawable CreateMainContent()
+        {
+            return new OsuScrollContainer
+            {
+                RelativeSizeAxes = Axes.Both
+            };
+        }
+    }
+}
