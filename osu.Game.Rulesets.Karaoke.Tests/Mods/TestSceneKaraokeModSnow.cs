f2c773090f8b1377731f9a7e87002aef46d57c53
andy840119 <andy840119@gmail.com>
2020-05-02 15:36:14 +0900

    Remove unused using.

--- osu.Game.Rulesets.Karaoke.Tests/Mods/TestSceneKaraokeModSnow.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Mods/TestSceneKaraokeModSnow.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using NUnit.Framework;
-using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Karaoke.Mods;
 using osu.Game.Rulesets.Karaoke.Tests.Beatmaps;
 using osu.Game.Tests.Visual;
