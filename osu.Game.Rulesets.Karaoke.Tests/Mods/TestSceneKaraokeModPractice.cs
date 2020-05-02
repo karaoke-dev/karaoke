e9f47a176c237a418930b17f0fd186c61a26efc4
andy840119 <andy840119@gmail.com>
2020-05-02 15:31:52 +0900

    Add missing test.

--- osu.Game.Rulesets.Karaoke.Tests/Mods/TestSceneKaraokeModPractice.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Mods/TestSceneKaraokeModPractice.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using NUnit.Framework;
+using osu.Game.Rulesets.Karaoke.Mods;
+using osu.Game.Rulesets.Karaoke.Tests.Beatmaps;
 using osu.Game.Tests.Visual;
 
 namespace osu.Game.Rulesets.Karaoke.Tests.Mods
@@ -11,5 +14,14 @@ namespace osu.Game.Rulesets.Karaoke.Tests.Mods
             : base(new KaraokeRuleset())
         {
         }
+
+        [Test]
+        public void TestMod() => CreateModTest(new ModTestData
+        {
+            Mod = new KaraokeModPractice(),
+            Autoplay = false,
+            Beatmap = new TestKaraokeBeatmap(null),
+            PassCondition = () => true
+        });
     }
 }
