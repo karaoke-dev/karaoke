e2698da60371e862cbe48e7f7b3b486362db0c06
andy840119 <andy840119@gmail.com>
2020-05-08 23:59:52 +0900

    little clean-up

--- osu.Game.Rulesets.Karaoke/Skinning/LegacyStageBackground.cs
+++ osu.Game.Rulesets.Karaoke/Skinning/LegacyStageBackground.cs
@@ -3,8 +3,8 @@
 
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
-using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
 using osu.Game.Rulesets.UI.Scrolling;
 using osu.Game.Skinning;
