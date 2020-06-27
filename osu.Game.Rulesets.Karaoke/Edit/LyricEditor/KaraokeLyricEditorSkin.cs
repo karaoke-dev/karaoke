cce8cd4f72feb259caf34c0d25a79ae419310a02
andy840119 <andy840119@gmail.com>
2020-06-27 15:01:18 +0900

    Add lyric editor's own skin.

---
+++ osu.Game.Rulesets.Karaoke/Edit/LyricEditor/KaraokeLyricEditorSkin.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Audio.Sample;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Textures;
+using osu.Game.Audio;
+using osu.Game.Rulesets.Karaoke.Skinning;
+using osu.Game.Rulesets.Karaoke.Skinning.Components;
+using osu.Game.Skinning;
+using System;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.LyricEditor
+{
+    /// <summary>
+    /// This karaoke skin is using in lyric editor only.
+    /// </summary>
+    public class KaraokeLyricEditorSkin : ISkin
+    {
+        private readonly Bindable<KaraokeFont> bindableFont;
+        private readonly Bindable<KaraokeLayout> bindableLayout;
+
+        public KaraokeLyricEditorSkin()
+        {
+            bindableFont = new Bindable<KaraokeFont>();
+            bindableLayout = new Bindable<KaraokeLayout>();
+        }
+
+        public Drawable GetDrawableComponent(ISkinComponent component) => null;
+
+        public SampleChannel GetSample(ISampleInfo sampleInfo) => null;
+
+        public Texture GetTexture(string componentName) => null;
+
+        public IBindable<TValue> GetConfig<TLookup, TValue>(TLookup lookup)
+        {
+            if(!(lookup is KaraokeSkinLookup skinLookup))
+                throw new NotSupportedException();
+
+            var config = skinLookup.Config;
+
+            switch (config)
+            {
+                case KaraokeSkinConfiguration.LyricStyle:
+                    return SkinUtils.As<TValue>(bindableFont);
+                case KaraokeSkinConfiguration.LyricLayout:
+                    return SkinUtils.As<TValue>(bindableLayout);
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+    }
+}
--- osu.Game.Rulesets.Karaoke/Edit/LyricEditor/LyricEditorScreen.cs
+++ osu.Game.Rulesets.Karaoke/Edit/LyricEditor/LyricEditorScreen.cs
@@ -66,7 +66,7 @@ namespace osu.Game.Rulesets.Karaoke.Edit.LyricEditor
                     },
                     new Drawable[]
                     {
-                        new SkinProvidingContainer(new KaraokeLegacySkinTransformer(null))
+                        new SkinProvidingContainer(new KaraokeLyricEditorSkin())
                         {
                             RelativeSizeAxes = Axes.Both,
                             Child = new OsuScrollContainer
