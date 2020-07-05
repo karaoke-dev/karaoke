8566b1539168450d0c5cf791e6c9eff77f6654b6
andy840119 <andy840119@gmail.com>
2020-07-05 15:57:56 +0900

    1. Create `IPatternGenerator` to batch calculate like calculate karaoke text's layout and start time. 2. Fix lyric's start time issue.

---
+++ osu.Game.Rulesets.Karaoke/Beatmaps/Patterns/IPatternGenerator.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+
+namespace osu.Game.Rulesets.Karaoke.Beatmaps.Patterns
+{
+    public interface IPatternGenerator<T>
+    {
+        void Generate(IEnumerable<T> hitObjects);
+    }
+}
