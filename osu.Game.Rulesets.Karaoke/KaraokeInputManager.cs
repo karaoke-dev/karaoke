f142d339e35d7c3db344c649bd2fbc754a829d4f
andy840119 <andy840119@gmail.com>
2020-07-09 22:16:57 +0900

    Fix crash isseu.

--- osu.Game.Rulesets.Karaoke/KaraokeInputManager.cs
+++ osu.Game.Rulesets.Karaoke/KaraokeInputManager.cs
@@ -58,7 +58,7 @@ namespace osu.Game.Rulesets.Karaoke
         }
 
         protected override InputState CreateInitialState()
-            => new KaraokeRulesetInputManagerInputState<KaraokeSaitenAction>(new MicrophoneState());
+            => new KaraokeRulesetInputManagerInputState<KaraokeSaitenAction>(new MicrophoneInputState(new MicrophoneState()));
 
         public override void HandleInputStateChange(InputStateChangeEvent inputStateChange)
         {
@@ -119,9 +119,10 @@ namespace osu.Game.Rulesets.Karaoke
     {
         public MicrophoneState Microphone { get; }
 
-        public KaraokeRulesetInputManagerInputState(MicrophoneState microphone)
+        public KaraokeRulesetInputManagerInputState(MicrophoneInputState state)
+            : base(state)
         {
-            Microphone = microphone;
+            Microphone = state.Microphone;
         }
     }
 
