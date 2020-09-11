5e96ed12b8b3a3fe7ed5992004a85c4fd5a2e177
andy840119 <andy840119@gmail.com>
2020-09-11 10:50:21 +0900

    Adjust beatmap statistic.

--- osu.Game.Rulesets.Karaoke/Beatmaps/KaraokeBeatmap.cs
+++ osu.Game.Rulesets.Karaoke/Beatmaps/KaraokeBeatmap.cs
@@ -15,29 +15,33 @@ namespace osu.Game.Rulesets.Karaoke.Beatmaps
         {
             int singers = 1;
             int lyrics = HitObjects.Count(s => s is LyricLine);
-            int notes = HitObjects.Count(s => s is Note);
 
-            return new[]
+            var defaultStatistic = new List<BeatmapStatistic>
             {
                 new BeatmapStatistic
                 {
                     Name = @"Singer",
                     Content = singers.ToString(),
-                    Icon = FontAwesome.Solid.User
+                    CreateIcon = () => new SpriteIcon { Icon = FontAwesome.Solid.User }
                 },
                 new BeatmapStatistic
                 {
                     Name = @"Lyric",
                     Content = lyrics.ToString(),
-                    Icon = FontAwesome.Solid.AlignLeft
+                    CreateIcon = () => new SpriteIcon { Icon = FontAwesome.Solid.AlignLeft }
                 },
-                new BeatmapStatistic
+            };
+
+            int notes = HitObjects.Count(s => s is Note note && note.Display);
+            if (notes > 0)
+                defaultStatistic.Add(new BeatmapStatistic
                 {
                     Name = @"Note",
                     Content = notes.ToString(),
-                    Icon = FontAwesome.Solid.Music
-                },
-            };
+                    CreateIcon = () => new SpriteIcon { Icon = FontAwesome.Solid.Music }
+                });
+
+            return defaultStatistic.ToArray();
         }
     }
 }
