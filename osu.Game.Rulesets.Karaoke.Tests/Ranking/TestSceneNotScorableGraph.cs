e094b06171ddd95846e0eac1a030a50e050855ff
andy840119 <andy840119@gmail.com>
2020-10-02 12:39:03 +0900

    Add not scorable graph.

---
+++ osu.Game.Rulesets.Karaoke.Tests/Ranking/TestSceneNotScorableGraph.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Framework.Extensions.Color4Extensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
+using osu.Game.Rulesets.Karaoke.Statistics;
+using osu.Game.Tests.Visual;
+using osuTK;
+
+namespace osu.Game.Rulesets.Karaoke.Tests.Ranking
+{
+    public class TestSceneNotScorableGraph : OsuTestScene
+    {
+        [Test]
+        public void TestBeatmapInfoGraph()
+        {
+            createTest();
+        }
+
+        private void createTest() => AddStep("create test", () =>
+        {
+            Children = new Drawable[]
+            {
+                new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = Color4Extensions.FromHex("#333")
+                },
+                new NotScorableGraph()
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Size = new Vector2(600, 130)
+                }
+            };
+        });
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/Statistics/NotScorableGraph.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Extensions.Color4Extensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
+using osuTK.Graphics;
+
+namespace osu.Game.Rulesets.Karaoke.Statistics
+{
+    public class NotScorableGraph : Container
+    {
+        public NotScorableGraph()
+        {
+            Masking = true;
+            CornerRadius = 5;
+            InternalChildren = new Drawable[]
+            {
+                new Box
+                {
+                    Name = "Background",
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = Color4.Black.Opacity(0.5f),
+                },
+                new OsuSpriteText
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Font = OsuFont.GetFont(weight: FontWeight.Bold, size: 20),
+                    Text = "Sorry, this beatmap is not scorable."
+                }
+            };
+        }
+    }
+}
