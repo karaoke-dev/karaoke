395dc9321261f6ca89b68a0c886c9e75a486e1ae
andy840119 <andy840119@gmail.com>
2020-10-12 22:14:51 +0900

    Add base test case and lyric line class.

---
+++ osu.Game.Rulesets.Karaoke.Tests/Graphics/TestSceneLyricTooltip.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Framework.Graphics;
+using osu.Game.Rulesets.Karaoke.Graphics.Cursor;
+using osu.Game.Rulesets.Karaoke.Objects;
+using osu.Game.Rulesets.Karaoke.Tests.Beatmaps;
+using osu.Game.Tests.Visual;
+using System.Linq;
+
+namespace osu.Game.Rulesets.Karaoke.Tests.Graphics
+{
+    [TestFixture]
+    public class TestSceneLyricTooltip : OsuTestScene
+    {
+        private LyricTooltip toolTip;
+
+        [SetUp]
+        public void SetUp() => Schedule(() =>
+        {
+            Child = toolTip = new LyricTooltip
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre
+            };
+            toolTip.Show();
+        });
+
+        [Test]
+        public void TestDisplayToolTip()
+        {
+            var beatmap = new TestKaraokeBeatmap(null);
+            var lyrics = beatmap.HitObjects.OfType<LyricLine>().ToList();
+            foreach (var lyric in lyrics)
+            {
+                AddStep($"Test lyric: {lyric.Text}", () =>
+                {
+                    toolTip.SetContent(lyric);
+                });
+            }
+        }
+    }
+}
