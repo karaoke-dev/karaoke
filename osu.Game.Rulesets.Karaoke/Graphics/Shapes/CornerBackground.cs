f3b78f1ee306b5491bd3034f0ecd4bc80d1e43b1
andy840119 <andy840119@gmail.com>
2020-10-17 18:35:08 +0900

    Add corner background component.

---
+++ osu.Game.Rulesets.Karaoke/Graphics/Shapes/CornerBackground.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+
+namespace osu.Game.Rulesets.Karaoke.Graphics.Shapes
+{
+    public class CornerBackground : Container
+    {
+        public CornerBackground()
+        {
+            Masking = true;
+            CornerRadius = 5;
+            AddInternal(new Box { RelativeSizeAxes = Axes.Both });
+        }
+    }
+}
