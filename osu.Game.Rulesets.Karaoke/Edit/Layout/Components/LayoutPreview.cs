ca63b0d8958cfa2d020ccdf79f6df22af769f026
andy840119 <andy840119@gmail.com>
2020-10-18 17:51:18 +0900

    Implement basic layout preview area.

--- osu.Game.Rulesets.Karaoke/Edit/Layout/Components/LayoutPreview.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Layout/Components/LayoutPreview.cs
@@ -6,11 +6,18 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Game.Overlays;
+using osu.Game.Rulesets.Karaoke.Graphics.UserInterfaceV2;
+using osu.Game.Rulesets.Karaoke.Objects;
+using osu.Game.Rulesets.Karaoke.Objects.Drawables;
+using osu.Game.Skinning;
+using osuTK;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.Layout.Components
 {
     public class LayoutPreview : Container
     {
+        private const float section_scale = 0.75f;
+
         [BackgroundDependencyLoader]
         private void load(OverlayColourProvider colourProvider)
         {
@@ -38,24 +45,96 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Layout.Components
                     {
                         new Drawable[]
                         {
-                            new Box
+                            new LayoutPreviewArea
                             {
-                                Name = "Ltric preview area",
-                                Colour = colourProvider.Light1,
-                                RelativeSizeAxes = Axes.Both
+                                Name = "Lyric preview area",
                             }
                         },
                         new Drawable[]
                         {
-                            new Container
+                            new GridContainer
                             {
                                 Name = "Controls",
                                 RelativeSizeAxes = Axes.Both,
+                                Anchor = Anchor.Centre,
+                                Origin = Anchor.Centre,
+                                Scale = new Vector2(section_scale),
+                                Size = new Vector2(1 / section_scale),
+                                ColumnDimensions = new []
+                                {
+                                    new Dimension(GridSizeMode.Relative, 0.05f),
+                                    new Dimension(GridSizeMode.Relative, 0.3f),
+                                    new Dimension(GridSizeMode.Relative, 0.3f),
+                                    new Dimension(GridSizeMode.Relative, 0.3f),
+                                },
+                                Content = new []
+                                {
+                                    new Drawable[]
+                                    {
+                                        null,
+                                        new LabelledDropdown<PreviewRatop>
+                                        {
+                                            Label = "Ratio",
+                                            Description = "Adjust to see different preview ratio."
+                                        },
+                                        new LabelledDropdown<PreviewSample>
+                                        {
+                                            Label = "Lyric",
+                                            Description = "Select different lyric to check layout is valid."
+                                        },
+                                        new LabelledDropdown<int>
+                                        {
+                                            Label = "Style",
+                                            Description = "Select different style to check layout is valid."
+                                        },
+                                    }
+                                }
                             }
                         }
                     }
                 }
             };
+        }
+
+        internal class LayoutPreviewArea : Container
+        {
+            private SkinProvidingContainer layoutArea;
+            private DrawableLyricLine drawableLyricLine;
+
+            [BackgroundDependencyLoader]
+            private void load(OverlayColourProvider colourProvider)
+            {
+                Children = new Drawable[]
+                {
+                    new Box
+                    {
+                        Name = "Setting background",
+                        RelativeSizeAxes = Axes.Both,
+                        Colour = colourProvider.Light4
+                    },
+                    layoutArea = new SkinProvidingContainer(new LayoutEditorSkin())
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                    }
+                };
+            }
+
+            public void InitialLyricLine(LyricLine lyricLine)
+                => layoutArea.Child = drawableLyricLine = new DrawableLyricLine(lyricLine);
+        }
+
+        internal enum PreviewRatop
+        {
+            
+        }
+
+        internal enum PreviewSample
+        {
+            SampeSmall,
+
+            SampleMedium,
+
+            SampleLarge
         }
     }
 }
