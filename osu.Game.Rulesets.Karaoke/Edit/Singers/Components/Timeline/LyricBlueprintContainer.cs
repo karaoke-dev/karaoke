1ebcddc8e29d7d6c1ce73cad2def5f8eb119c138
andy840119 <andy840119@gmail.com>
2020-11-07 13:21:21 +0900

    implement lock movement.

--- osu.Game.Rulesets.Karaoke/Edit/Singers/Components/Timeline/LyricBlueprintContainer.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Singers/Components/Timeline/LyricBlueprintContainer.cs
@@ -1,9 +1,11 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Game.Rulesets.Edit;
 using osu.Game.Rulesets.Karaoke.Edit.Components.Timeline;
 using osu.Game.Rulesets.Karaoke.Objects;
 using osu.Game.Rulesets.Objects;
+using osu.Game.Screens.Edit.Compose.Components;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.Singers.Components.Timeline
 {
@@ -17,6 +19,18 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Singers.Components.Timeline
             // todo : check lyric is at the same singer.
 
             base.AddBlueprintFor(hitObject);
+        }
+
+        protected override SelectionHandler CreateSelectionHandler()
+            => new LyricTimelineSelectionHandler();
+
+        protected override SelectionBlueprint CreateBlueprintFor(HitObject hitObject)
+            => new LyricTimelineHitObjectBlueprint(hitObject);
+
+        internal class LyricTimelineSelectionHandler : TimelineSelectionHandler
+        {
+            // does not allow movement
+            public override bool HandleMovement(MoveSelectionEvent moveEvent) => false;
         }
     }
 }
