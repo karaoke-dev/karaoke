f350743198b262be01f5b5e44b5e20cd190b37ca
andy840119 <andy840119@gmail.com>
2020-11-14 12:33:37 +0900

    Technically test and utility looks like this. Need to think about how to implement and other edge case.

---
+++ osu.Game.Rulesets.Karaoke.Tests/Utils/TimeTagsUtilsTest.cs
@@ -0,0 +1,112 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Framework.Graphics.Sprites;
+using System;
+using System.Collections.Generic;
+
+namespace osu.Game.Rulesets.Karaoke.Tests.Utils
+{
+    [TestFixture]
+    public class TimeTagsUtilsTest
+    {
+        [TestCaseSource("ValidTimeTagWithSorted")]
+        [TestCaseSource("ValidTimeTagWithUnsorted")]
+        public void TestSort(IReadOnlyList<Tuple<TimeTagIndex, double>> timetags)
+        {
+            // run all then using time(nullable double) to check.
+        }
+
+        public void TestFindInvalid()
+        {
+            // run all and find error amount and index.
+        }
+
+        public void TestFixInvalid()
+        {
+            // run all valid and check do not fixing
+
+            // run all invalid then check which part is fixed, using list of time to check result.
+        }
+
+        public void TestToDictionary()
+        {
+            // try all empty dictionary included.
+        }
+
+        #region valid source
+
+        public static IReadOnlyList<Tuple<TimeTagIndex, double>> ValidTimeTagWithSorted()
+            => new List<Tuple<TimeTagIndex, double>>
+            {
+                // todo : sorted list
+            };
+
+        public static IReadOnlyList<Tuple<TimeTagIndex, double>> ValidTimeTagWithUnsorted()
+             => new List<Tuple<TimeTagIndex, double>>
+             {
+                 // todo : just not sorted.
+             };
+
+        public static IReadOnlyList<Tuple<TimeTagIndex, double>> ValidTimeTagWithUnsortedAndDuplicatedWithNoValue()
+             => new List<Tuple<TimeTagIndex, double>>
+             {
+                 // not sorted + duliicated time tag(with no value)
+             };
+
+        public static IReadOnlyList<Tuple<TimeTagIndex, double>> ValidTimeTagWithUnsortedAndDuplicatedWithValue()
+             => new List<Tuple<TimeTagIndex, double>>
+             {
+                 // not sorted + duliicated time tag(with value)
+             };
+
+        public static IReadOnlyList<Tuple<TimeTagIndex, double>> ValidTimeTagWithUnsortedAndAllEmpty()
+             => new List<Tuple<TimeTagIndex, double>>
+             {
+                 // all empty
+             };
+
+        #endregion
+
+        #region invalid source
+
+        public static IReadOnlyList<Tuple<TimeTagIndex, double>> InvalidTimeTagWithOneStartLargerThenAllEnd()
+             => new List<Tuple<TimeTagIndex, double>>
+             {
+                 // 1. one start larger then all end.
+             };
+
+        public static IReadOnlyList<Tuple<TimeTagIndex, double>> InvalidTimeTagWithMultiStartLargerThenAllEnd()
+             => new List<Tuple<TimeTagIndex, double>>
+             {
+                 // 2. multi start larger then all end.
+             };
+
+        public static IReadOnlyList<Tuple<TimeTagIndex, double>> InvalidTimeTagWithOneEndLargerThenAllStart()
+             => new List<Tuple<TimeTagIndex, double>>
+             {
+                 // 3. one end larger then all start.
+             };
+
+        public static IReadOnlyList<Tuple<TimeTagIndex, double>> InvalidTimeTagWithMultiEndLargerThenAllStart()
+             => new List<Tuple<TimeTagIndex, double>>
+             {
+                 // 4. multi start larger then all end.
+             };
+
+        public static IReadOnlyList<Tuple<TimeTagIndex, double>> InvalidTimeTagWithSomeStartLargerThenSomeEnd()
+             => new List<Tuple<TimeTagIndex, double>>
+             {
+                 // 5. some start larger then some end.
+             };
+
+        public static IReadOnlyList<Tuple<TimeTagIndex, double>> InvalidTimeTagWithSomeEndLargerThenSomeStart()
+             => new List<Tuple<TimeTagIndex, double>>
+             {
+                 // 6. some end larger then some start.
+             };
+
+        #endregion
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/Utils/TimeTagsUtils.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics.Sprites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace osu.Game.Rulesets.Karaoke.Utils
+{
+    public static class TimeTagsUtils
+    {
+        /// <summary>
+        /// Sort list of time tags by index and time.
+        /// </summary>
+        /// <param name="timeTags">Time tags</param>
+        /// <returns>Sorted time tags</returns>
+        public static List<Tuple<TimeTagIndex, double>> Sort(List<Tuple<TimeTagIndex, double>> timeTags)
+        {
+            return timeTags.OrderBy(x => x.Item1.Index).ThenByDescending(x => x.Item1.State).ThenBy(x => x.Item2).ToList();
+        }
+
+        /// <summary>
+        /// Find invalid time tags.
+        /// </summary>
+        /// <param name="timeTags">Time tags</param>
+        /// <returns>List of invalid time tags</returns>
+        public static List<Tuple<TimeTagIndex, double>> FindInvalid(List<Tuple<TimeTagIndex, double>> timeTags)
+        {
+            var sortedTimeTags = Sort(timeTags);
+
+            // todo : find the time larger then normal time tag.
+            throw new Exception();
+        }
+
+        /// <summary>
+        /// Auto fix invalid time tags.
+        /// </summary>
+        /// <param name="timeTags">Time tags</param>
+        /// <param name="fixWay">Fix way</param>
+        /// <returns>Fixed time tags.</returns>
+        public static List<Tuple<TimeTagIndex, double>> FixInvalid(List<Tuple<TimeTagIndex, double>> timeTags, FixWay fixWay)
+        {
+            var sortedTimeTags = Sort(timeTags);
+            var invalidTimeTags = FindInvalid(timeTags);
+
+            foreach (var timetag in invalidTimeTags)
+            {
+                // todo : delete or delete with merge?
+            }
+
+            return sortedTimeTags;
+        }
+
+        /// <summary>
+        /// Convert list of time tag to index.
+        /// </summary>
+        /// <param name="timeTags">Time tags</param>
+        /// <param name="applyFix">Should auto-fix or not</param>
+        /// <returns>Time tags with dictionary format.</returns>
+        public static IReadOnlyDictionary<TimeTagIndex, double> ToDictionary(List<Tuple<TimeTagIndex, double>> timeTags, bool applyFix = true)
+        {
+            // sorted value
+            var sortedTimeTags = applyFix ? FixInvalid(timeTags, FixWay.Merge) :  Sort(timeTags);
+
+            // todo : convert to dictionary, will get start's smallest time and end's largest time.
+            throw new Exception();
+
+        }
+    }
+
+    public enum FixWay
+    {
+        RemoveSmaller,
+
+        RemoveLarger,
+
+        Merge
+    }
+}
