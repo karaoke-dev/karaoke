252a1418f345b070d0b2f5f0ea7b6cea97b91c79
andy840119 <andy840119@gmail.com>
2020-11-22 18:58:46 +0900

    Move import logic out of dlalog because this doalog only called in DragFileSubScreen So don't need to collect all logic in that place.

--- osu.Game.Rulesets.Karaoke/Edit/ImportLyric/DragFile/DragFileSubScreen.cs
+++ osu.Game.Rulesets.Karaoke/Edit/ImportLyric/DragFile/DragFileSubScreen.cs
@@ -1,11 +1,15 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Sprites;
 using osu.Game.Database;
+using osu.Game.Overlays.Dialog;
 using osu.Game.Rulesets.Karaoke.Edit.ImportLyric.DragFile.Components;
+using osu.Game.Rulesets.Karaoke.Graphics.Overlays.Dialog;
 using osuTK;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -21,6 +25,9 @@ namespace osu.Game.Rulesets.Karaoke.Edit.ImportLyric.DragFile
         public override IconUsage Icon => FontAwesome.Solid.Upload;
 
         public IEnumerable<string> HandledExtensions => ImportLyricManager.LyricFormatExtensions;
+
+        [Resolved]
+        private ImportLyricManager importManager { get; set; }
 
         public DragFileSubScreen()
         {
@@ -43,11 +50,45 @@ namespace osu.Game.Rulesets.Karaoke.Edit.ImportLyric.DragFile
         {
             Schedule(() =>
             {
-                DialogOverlay.Push(new ImportLyricDialog(fileInfo, success =>
+                // Check file is exist
+                if (!fileInfo.Exists)
                 {
-                    if (success)
+                    DialogOverlay.Push(createFileNotFoundDialog());
+                    return;
+                }
+
+                // Check format is match
+                if (!ImportLyricManager.LyricFormatExtensions.Contains(fileInfo.Extension))
+                {
+                    DialogOverlay.Push(createFormatNotMatchDialog());
+                    return;
+                }
+
+                DialogOverlay.Push(new ImportLyricDialog(fileInfo, execute =>
+                {
+                    if (!execute)
+                        return;
+
+                    try
                     {
-                        Complete();
+                        importManager.ImportLrcFile(fileInfo);
+                        DialogOverlay.Push(createCompleteDialog());
+                    }
+                    catch (Exception ex)
+                    {
+                        switch (ex)
+                        {
+                            case FileNotFoundException _:
+                                DialogOverlay.Push(createFileNotFoundDialog());
+                                break;
+
+                            case FileLoadException loadException:
+                                DialogOverlay.Push(createLoadExceptionDialog(loadException));
+                                break;
+                            default:
+                                DialogOverlay.Push(createUnknownExceptionDialog());
+                                break;
+                        }
                     }
                 }));
             });
@@ -57,5 +98,48 @@ namespace osu.Game.Rulesets.Karaoke.Edit.ImportLyric.DragFile
         {
             ScreenStack.Push(ImportLyricStep.AssignLanguage);
         }
+
+        private PopupDialog createFileNotFoundDialog()
+            => new OkPopupDialog
+            {
+                Icon = FontAwesome.Regular.QuestionCircle,
+                HeaderText = "Seems file is not exist",
+                BodyText = $"Drag the file then drop again.",
+            };
+
+        private PopupDialog createFormatNotMatchDialog()
+            => new OkPopupDialog
+            {
+                Icon = FontAwesome.Solid.ExclamationTriangle,
+                HeaderText = "This type of file is not supported",
+                BodyText = $"May sure this type of file is supported.",
+            };
+
+        private PopupDialog createLoadExceptionDialog(FileLoadException loadException)
+            => new OkPopupDialog
+            {
+                Icon = FontAwesome.Solid.Bug,
+                HeaderText = @"File loading error",
+                BodyText = loadException.Message,
+            };
+
+        private PopupDialog createUnknownExceptionDialog()
+            => new OkPopupDialog
+            {
+                Icon = FontAwesome.Solid.Bug,
+                HeaderText = @"Unknown error",
+                BodyText = @"Unknown error QAQa."
+            };
+
+        private PopupDialog createCompleteDialog()
+            => new OkPopupDialog(ok =>
+            {
+                Complete();
+            })
+            {
+                Icon = FontAwesome.Regular.CheckCircle,
+                HeaderText = @"Import success",
+                BodyText = "Lyrics has been imported."
+            };
     }
 }
--- osu.Game.Rulesets.Karaoke/Edit/ImportLyric/DragFile/ImportLyricDialog.cs
+++ osu.Game.Rulesets.Karaoke/Edit/ImportLyric/DragFile/ImportLyricDialog.cs
@@ -15,9 +15,6 @@ namespace osu.Game.Rulesets.Karaoke.Edit.ImportLyric.DragFile
     public class ImportLyricDialog : PopupDialog
     {
         [Resolved]
-        private ImportLyricManager importManager { get; set; }
-
-        [Resolved]
         private DialogOverlay dialogOverlay { get; set; }
 
         public ImportLyricDialog(FileInfo info, Action<bool> resetAction = null)
@@ -31,96 +28,14 @@ namespace osu.Game.Rulesets.Karaoke.Edit.ImportLyric.DragFile
                 new PopupDialogOkButton
                 {
                     Text = @"Yes. Go for it.",
-                    Action = () =>
-                    {
-                        var success = processImport(info);
-                        resetAction?.Invoke(success);
-                    }
+                    Action = () => resetAction?.Invoke(true),
                 },
                 new PopupDialogCancelButton
                 {
                     Text = @"No! Abort mission!",
+                    Action = () => resetAction?.Invoke(false),
                 },
             };
         }
-
-        private bool processImport(FileInfo info)
-        {
-            try
-            {
-                // Check file is exist
-                if (!info.Exists)
-                {
-                    dialogOverlay.Push(createFileNotFoundDialog());
-                    return false;
-                }
-
-                // Check format is match
-                if (!ImportLyricManager.LyricFormatExtensions.Contains(info.Extension))
-                {
-                    dialogOverlay.Push(createFormatNotMatchDialog());
-                    return false;
-                }
-
-                importManager.ImportLrcFile(info);
-                dialogOverlay.Push(new OkPopupDialog
-                {
-                    Icon = FontAwesome.Regular.CheckCircle,
-                    HeaderText = @"Import success",
-                    BodyText = "Lyrics has been imported."
-                });
-                return true;
-            }
-            catch (Exception ex)
-            {
-                switch (ex)
-                {
-                    case FileNotFoundException _:
-                        dialogOverlay.Push(createFileNotFoundDialog());
-                        break;
-
-                    case FileLoadException loadException:
-                        dialogOverlay.Push(createLoadExceptionDialog(loadException));
-                        break;
-                    default:
-                        dialogOverlay.Push(createUnknownExceptionDialog());
-                        break;
-                }
-
-                return false;
-            }
-        }
-
-        private PopupDialog createFileNotFoundDialog()
-            => new OkPopupDialog
-            {
-                Icon = FontAwesome.Regular.QuestionCircle,
-                HeaderText = "Seems file is not exist",
-                BodyText = $"Drag the file then drop again.",
-            };
-
-        private PopupDialog createFormatNotMatchDialog()
-            => new OkPopupDialog
-            {
-                Icon = FontAwesome.Solid.ExclamationTriangle,
-                HeaderText = "This type of file is not supported",
-                BodyText = $"May sure this type of file is supported.",
-            };
-
-        private PopupDialog createLoadExceptionDialog(FileLoadException loadException)
-            => new OkPopupDialog
-            {
-                Icon = FontAwesome.Solid.Bug,
-                HeaderText = @"File loading error",
-                BodyText = loadException.Message,
-            };
-
-        private PopupDialog createUnknownExceptionDialog()
-            => new OkPopupDialog
-            {
-                Icon = FontAwesome.Solid.Bug,
-                HeaderText = @"Unknown error",
-                BodyText = @"Unknown error QAQa."
-            };
     }
 }
