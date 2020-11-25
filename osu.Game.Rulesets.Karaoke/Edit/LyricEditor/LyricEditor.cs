9a7c41f1c58bd5ee29a9af2460f9794dbf878114
andy840119 <andy840119@gmail.com>
2020-11-25 22:18:16 +0900

    Add edit editor's mode.

--- osu.Game.Rulesets.Karaoke/Edit/LyricEditor/LyricEditor.cs
+++ osu.Game.Rulesets.Karaoke/Edit/LyricEditor/LyricEditor.cs
@@ -4,7 +4,6 @@
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Karaoke.Objects;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Screens.Edit;
@@ -80,5 +79,57 @@ namespace osu.Game.Rulesets.Karaoke.Edit.LyricEditor
             get => skin.FontSize;
             set => skin.FontSize = value;
         }
+
+
+        public Mode Mode { get; set; }
+
+
+        public LyricFastEditMode LyricFastEditMode { get; set; }
+    }
+
+    public enum Mode
+    {
+        /// <summary>
+        /// Cannot edit anything except each lyric's left-side part.
+        /// </summary>
+        ViewMode,
+
+        /// <summary>
+        /// Can create/delete/mode/split/combine lyric.
+        /// </summary>
+        EditMode,
+
+        /// <summary>
+        /// Click white-space to set current time into time-tag.
+        /// </summary>
+        RecordMode,
+
+        /// <summary>
+        /// Enable to create/delete and reset time tag.
+        /// </summary>
+        TimeTagEditMode
+    }
+
+    public enum LyricFastEditMode
+    {
+        /// <summary>
+        /// User can only see start and end time.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Can edit each lyric's layout.
+        /// </summary>
+        Layout,
+
+        /// <summary>
+        /// Can edit each lyric's singer.
+        /// </summary>
+        Singer,
+
+        /// <summary>
+        /// Can edit each lyric's language.
+        /// </summary>
+        Language,
     }
 }
