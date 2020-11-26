099487a27ffb85ff54b67e137475758396cc8ebb
andy840119 <andy840119@gmail.com>
2020-11-26 21:51:01 +0900

    Get ready for lyric editor.

--- osu.Game.Rulesets.Karaoke/Edit/ImportLyric/AssignLanguage/AssignLanguageSubScreen.cs
+++ osu.Game.Rulesets.Karaoke/Edit/ImportLyric/AssignLanguage/AssignLanguageSubScreen.cs
@@ -1,7 +1,10 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Sprites;
+using osu.Framework.Timing;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.ImportLyric.AssignLanguage
 {
@@ -14,6 +17,26 @@ namespace osu.Game.Rulesets.Karaoke.Edit.ImportLyric.AssignLanguage
         public override ImportLyricStep Step => ImportLyricStep.AssignLanguage;
 
         public override IconUsage Icon => FontAwesome.Solid.Globe;
+
+        protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent)
+        {
+            var dependencies = new DependencyContainer(base.CreateChildDependencies(parent));
+            var clock = new DecoupleableInterpolatingFramedClock { IsCoupled = false };
+            dependencies.CacheAs<IAdjustableClock>(clock);
+            dependencies.CacheAs<IFrameBasedClock>(clock);
+
+            return dependencies;
+        }
+
+        public AssignLanguageSubScreen()
+        {
+            InternalChild = new LyricEditor.LyricEditor
+            {
+                RelativeSizeAxes = Axes.Both,
+                Mode = LyricEditor.Mode.EditMode,
+                LyricFastEditMode = LyricEditor.LyricFastEditMode.Language,
+            };
+        }
 
         public override void Complete()
         {
