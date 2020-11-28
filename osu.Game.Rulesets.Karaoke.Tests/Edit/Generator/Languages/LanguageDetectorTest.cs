337badac7164936affdd604dac5854761bbe9936
andy840119 <andy840119@gmail.com>
2020-11-28 15:23:33 +0900

    Use another package to detect language because it's simple and much more accurate.

--- osu.Game.Rulesets.Karaoke.Tests/Edit/Generator/Languages/LanguageDetectorTest.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Edit/Generator/Languages/LanguageDetectorTest.cs
@@ -3,16 +3,29 @@
 
 using NUnit.Framework;
 using osu.Game.Rulesets.Karaoke.Edit.Generator.Languages;
+using osu.Game.Rulesets.Karaoke.Objects;
+using System.Globalization;
 
 namespace osu.Game.Rulesets.Karaoke.Tests.Edit.Generator.Languages
 {
     [TestFixture]
     public class LanguageDetectorTest
     {
-        [TestCase("花火大会", "Ja-jp")]
+        [TestCase("花火大会", "zh-CN")]
+        [TestCase("Testing", "en-US")]
         public void TestDetectLanguage(string text, string language)
         {
             var detector = new LanguageDetector();
+            var result = detector.DetectLanguage(new Lyric { Text = text });
+            Assert.AreEqual(result, new CultureInfo(language));
+        }
+
+        //[Ignore("Japanese text not supported now")]
+        [TestCase("ハナビ", "ja-jp")]
+        [TestCase("はなび", "ja-jp")]
+        public void TestJapaneseLanguage(string text, string language)
+        {
+            TestDetectLanguage(text, language);
         }
     }
 }
--- osu.Game.Rulesets.Karaoke/Edit/Generator/Languages/LanguageDetector.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Generator/Languages/LanguageDetector.cs
@@ -1,66 +1,30 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using Lucene.Net.Analysis;
-using Lucene.Net.Analysis.OpenNlp;
-using Lucene.Net.Analysis.Standard;
-using Lucene.Net.Analysis.TokenAttributes;
-using Lucene.Net.Util;
-using System.IO;
+using osu.Game.Rulesets.Karaoke.Objects;
+using System.Globalization;
+using System.Linq;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.Generator.Languages
 {
     public class LanguageDetector
     {
-        public LanguageDetector()
+        public CultureInfo DetectLanguage(Lyric lyric)
         {
-            // this is copied from :
-            // https://fabian-kostadinov.github.io/2018/09/08/introduction-to-lucene-opennlp-part1/
-            var version = LuceneVersion.LUCENE_48;
+            var detector = new LanguageDetection.LanguageDetector();
+            detector.AddAllLanguages();
+            var result = detector.DetectAll(lyric.Text);
+            var languageCode = result.FirstOrDefault()?.Language;
 
-            var text = "The quick brown fox jumped over the lazy dogs.";
-
-            var analyzer = new StandardAnalyzer(version);
-
-            // We're using a string reader to return a token stream. This allows us to observe the tokens
-            // while they are being processed by our analyzer.
-            TokenStream stream = analyzer.GetTokenStream("field", new StringReader(text));
-
-            // CharTermAttribute will contain the actual token word
-            CharTermAttribute termAtt = stream.AddAttribute<CharTermAttribute>();
-
-            // TypeAttribute will contain the OpenNLP POS (Treebank II) tag
-            TypeAttribute typeAtt = stream.AddAttribute<TypeAttribute>();
-
-            try
+            switch (languageCode)
             {
-                stream.Reset();
-
-                // Print all tokens until stream is exhausted
-                while (stream.IncrementToken()) {
-                    var result = termAtt.ToString() + ": " + typeAtt.Type;
-                }
-
-                stream.End();
-
-            }
-            finally
-            {
-                stream.Dispose();
+                case "zho":
+                    return new CultureInfo("zh-CN");
+                case "eng":
+                    return new CultureInfo("en-US");
+                default:
+                    return null;
             }
         }
-
-        /*
-        public class OpenNLPAnalyzer : Analyzer
-        {
-            protected override TokenStreamComponents CreateComponents(string fieldName, TextReader reader)
-            {
-                Tokenizer tokenizer = new OpenNLPTokenizer(reader);
-
-                TokenFilter tokenFilter = new OpenNLPPOSFilter();
-                return new TokenStreamComponents(tokenizer, tokenFilter);
-            }
-        }
-        */
     }
 }
