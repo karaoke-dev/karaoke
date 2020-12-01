9e47586eff83bdd834a7394bb9578d6a61f437d8
andy840119 <andy840119@gmail.com>
2020-12-01 23:38:55 +0900

    implement test case and some check part.

--- osu.Game.Rulesets.Karaoke/Utils/TextTagsUtils.cs
+++ osu.Game.Rulesets.Karaoke/Utils/TextTagsUtils.cs
@@ -4,85 +4,70 @@
 using osu.Game.Rulesets.Karaoke.Objects.Types;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace osu.Game.Rulesets.Karaoke.Utils
 {
     public static class TextTagsUtils
     {
-        public static T[] Sort<T>(T[] textTags) where T : ITextTag
+        public static T[] Sort<T>(T[] textTags, Sorting sorting = Sorting.Asc) where T : ITextTag
         {
-            return textTags?.OrderBy(x => x.StartIndex).ThenBy(x => x.EndIndex).ToArray();
+            switch (sorting)
+            {
+                case Sorting.Asc:
+                    return textTags?.OrderBy(x => x.StartIndex).ThenBy(x => x.EndIndex).ToArray();
+                case Sorting.Desc:
+                    return textTags?.OrderByDescending(x => x.EndIndex).ThenByDescending(x => x.StartIndex).ToArray();
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(sorting));
+            }
         }
 
-        public static T[] FindInvalid<T>(T[] textTags, GroupCheck other = GroupCheck.Asc, SelfCheck self = SelfCheck.BasedOnStart) where T : ITextTag
+        public static T[] FindInvalid<T>(T[] textTags, string lyric, Sorting sorting = Sorting.Asc) where T : ITextTag
         {
-            var sortedTextTags = Sort(textTags);
-            var groupedTextTags = sortedTextTags.GroupBy(x => x.StartIndex);
+            // check is null or empty
+            if (textTags == null || textTags.Length == 0)
+                return new T[] { };
+
+            var sortedTextTags = Sort(textTags, sorting);
 
             var invalidList = new List<T>();
 
-            foreach (var groupedTimeTag in groupedTextTags)
+            // check invalid range
+            invalidList.AddRange(sortedTextTags.Where(x => x.StartIndex < 0 || x.EndIndex > lyric.Length));
+
+            // check end is less or equal to start index
+            invalidList.AddRange(sortedTextTags.Where(x => x.EndIndex <= x.StartIndex));
+
+            // find other is smaller or bigger
+            foreach (var textTag in sortedTextTags)
             {
-                // add invalid group into list.
-                var groupInvalid = findGroupInvalid();
-                if (groupInvalid != null)
-                    invalidList.AddRange(groupInvalid);
-
-                // add invalid self into list.
-                var selfInvalid = findSelfInvalid();
-                if (selfInvalid != null)
-                    invalidList.AddRange(selfInvalid);
-
-                List<T> findGroupInvalid()
+                switch (sorting)
                 {
-                    switch (other)
-                    {
-                        case GroupCheck.Asc:
-                            // mark next is invalid if smaller then self
-                            var groupMaxTime = groupedTimeTag.Max(x => x.Item2);
-                            if (groupMaxTime == null)
-                                return null;
+                    case Sorting.Asc:
 
-                            return groupedTimeTag.Where(x => x.Index > groupedTimeTag.Key && x.Item2 < groupMaxTime).ToList();
+                        break;
 
-                        case GroupCheck.Desc:
-                            // mark previous is invalid if larger then self
-                            var groupMinTime = groupedTimeTag.Min(x => x.Item2);
-                            if (groupMinTime == null)
-                                return null;
+                    case Sorting.Desc:
 
-                            return groupedTimeTag.Where(x => x.Index < groupedTimeTag.Key && x.Item2 > groupMinTime).ToList();
-
-                        default:
-                            return null;
-                    }
-                }
-
-                List<T> findSelfInvalid()
-                {
-                    switch (self)
-                    {
-                        case SelfCheck.BasedOnStart:
-                            var maxStartTime = startTimeGroup.Max(x => x.Item2);
-                            if (maxStartTime == null)
-                                return null;
-
-                            return endTimeGroup.Where(x => x.Item2.Value < maxStartTime.Value).ToList();
-
-                        case SelfCheck.BasedOnEnd:
-                            var minEndTime = endTimeGroup.Min(x => x.Item2);
-                            if (minEndTime == null)
-                                return null;
-
-                            return startTimeGroup.Where(x => x.Item2.Value > minEndTime.Value).ToList();
-
-                        default:
-                            return null;
-                    }
+                        break;
                 }
             }
 
             return Sort(invalidList.Distinct().ToArray());
+        }
+
+        public enum Sorting
+        {
+            /// <summary>
+            /// Mark next time tag is error if conflict.
+            /// </summary>
+            Asc,
+
+            /// <summary>
+            /// Mark previous tag is error if conflict.
+            /// </summary>
+            Desc
         }
     }
 }
