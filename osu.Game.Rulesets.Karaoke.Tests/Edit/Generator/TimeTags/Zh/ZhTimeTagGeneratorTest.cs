c01c0fa864db2cc1c2d4e8d002c5a0dab562c6ff
andy840119 <andy840119@gmail.com>
2020-12-02 11:07:41 +0900

    Creater default class for implementation.

---
+++ osu.Game.Rulesets.Karaoke.Tests/Edit/Generator/TimeTags/Zh/ZhTimeTagGeneratorTest.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Linq;
+using NUnit.Framework;
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Rulesets.Karaoke.Edit.Generator.TimeTags.Zh;
+using osu.Game.Rulesets.Karaoke.Objects;
+
+namespace osu.Game.Rulesets.Karaoke.Tests.Edit.Generator.TimeTags.Zh
+{
+    [TestFixture]
+    public class ZhTimeTagGeneratorTest
+    {
+    }
+}
