0a5339818ae54ee1bbdfb615779acbd1a266b0b0
andy840119 <andy840119@gmail.com>
2020-12-03 21:55:49 +0900

    Add utility to check is chinses character. And it's test.

---
+++ osu.Game.Rulesets.Karaoke/Utils/ZhStringUtils.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Text.Unicode;
+
+namespace osu.Game.Rulesets.Karaoke.Utils
+{
+    public static class ZhStringUtils
+    {
+        public static bool IsChinese(char character)
+        {
+            // From : https://stackoverflow.com/a/61738863/4105113
+            var minValue = UnicodeRanges.CjkUnifiedIdeographs.FirstCodePoint;
+            var maxValue = minValue + UnicodeRanges.CjkUnifiedIdeographs.Length;
+            return character >= minValue && character < maxValue;
+        }
+    }
+}
