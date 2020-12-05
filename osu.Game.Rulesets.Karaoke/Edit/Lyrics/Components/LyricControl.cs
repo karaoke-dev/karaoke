b195ada9b7d706c61f64fc9250fcbcd774d13361
andy840119 <andy840119@gmail.com>
2020-12-05 17:53:47 +0900

    Implement time tag display.

--- osu.Game.Rulesets.Karaoke/Edit/Lyrics/Components/LyricControl.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Components/LyricControl.cs
@@ -4,10 +4,13 @@
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
 using osu.Framework.Timing;
+using osu.Game.Rulesets.Karaoke.Edit.Lyrics.Components.TimeTags;
 using osu.Game.Rulesets.Karaoke.Objects;
 using osu.Game.Rulesets.Karaoke.Objects.Drawables;
 using osu.Game.Rulesets.Karaoke.Skinning.Components;
+using System;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Components
 {
@@ -36,11 +39,39 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Components
 
         public class DrawableEditorLyric : DrawableLyric
         {
+            private readonly Container timeTagContainer;
+
             public DrawableEditorLyric(Lyric lyric)
                 : base(lyric)
             {
+                AddInternal(timeTagContainer = new Container
+                {
+                    RelativeSizeAxes = Axes.Both
+                });
+
                 DisplayRuby = true;
                 DisplayRomaji = true;
+            }
+
+            protected override void LoadComplete()
+            {
+                base.LoadComplete();
+
+                TimeTagsBindable.BindValueChanged(e =>
+                {
+                    UpdateTimeTags();
+                }, true);
+            }
+
+            protected override void ApplyFont(KaraokeFont font)
+            {
+                base.ApplyFont(font);
+
+                if (TimeTagsBindable.Value == null)
+                    return;
+
+                // todo : need to delay until karaoke text has been calculated.
+                UpdateTimeTags();
             }
 
             protected override void ApplyLayout(KaraokeLayout layout)
@@ -64,6 +95,24 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Components
             {
                 get => double.MaxValue;
                 set => base.LifetimeEnd = double.MaxValue;
+            }
+
+            protected void UpdateTimeTags()
+            {
+                timeTagContainer.Clear();
+                foreach (var timeTag in TimeTagsBindable.Value)
+                {
+                    var index = Math.Min(timeTag.Item1.Index, HitObject.Text.Length - 1);
+                    var percentage = timeTag.Item1.State == TimeTagIndex.IndexState.Start ? 0 : 1;
+                    var position = karaokeText.GetPercentageWidth(index, index + 1, percentage);
+
+                    timeTagContainer.Add(new DrawableTimeTag(timeTag)
+                    {
+                        Anchor = Anchor.BottomLeft,
+                        Origin = Anchor.BottomLeft,
+                        X = position
+                    });
+                }
             }
         }
     }
