c45088a860d886f77328a4f7007ab9c1534ce377
andy840119 <andy840119@gmail.com>
2020-12-10 00:16:04 +0900

    Adjust style and now seems better.

---
+++ osu.Game.Rulesets.Karaoke.Tests/Graphics/TestSceneLanguageSelectionDialog.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Testing;
+using osu.Game.Overlays;
+using osu.Game.Rulesets.Karaoke.Beatmaps;
+using osu.Game.Rulesets.Karaoke.Edit.Translate;
+using osu.Game.Rulesets.Karaoke.Graphics.UserInterface;
+using osu.Game.Rulesets.Karaoke.Tests.Beatmaps;
+using osu.Game.Screens.Edit;
+using osu.Game.Tests.Visual;
+namespace osu.Game.Rulesets.Karaoke.Tests.Graphics
+{
+    public class TestSceneLanguageSelectionDialog : OsuManualInputManagerTestScene
+    {
+        protected override Container<Drawable> Content { get; } = new Container { RelativeSizeAxes = Axes.Both };
+
+        private DialogOverlay dialogOverlay;
+        private TranslateManager manager;
+
+        private LanguageSelectionDialog dialog;
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            var beatmap = new TestKaraokeBeatmap(null);
+            var karaokeBeatmap = new KaraokeBeatmapConverter(beatmap, new KaraokeRuleset()).Convert();
+            var editorBeatmap = new EditorBeatmap(karaokeBeatmap);
+            Dependencies.Cache(editorBeatmap);
+
+            base.Content.AddRange(new Drawable[]
+            {
+                Content,
+                dialogOverlay = new DialogOverlay()
+            });
+
+            Dependencies.Cache(dialogOverlay);
+        }
+
+        [SetUp]
+        public void SetUp() => Schedule(() =>
+        {
+            Child = dialog = new LanguageSelectionDialog();
+        });
+
+        [SetUpSteps]
+        public void SetUpSteps()
+        {
+            AddStep("show dialog", () => dialog.Show());
+        }
+    }
+}
--- osu.Game.Rulesets.Karaoke/Graphics/UserInterface/LanguageSelectionDialog.cs
+++ osu.Game.Rulesets.Karaoke/Graphics/UserInterface/LanguageSelectionDialog.cs
@@ -1,14 +1,15 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Game.Graphics;
 using osu.Game.Graphics.Containers;
+using osu.Game.Graphics.UserInterface;
 using osu.Game.Rulesets.Karaoke.Graphics.Containers;
 using osuTK;
+using osuTK.Graphics;
+using System.Collections.Generic;
 using System.Globalization;
 
 namespace osu.Game.Rulesets.Karaoke.Graphics.UserInterface
@@ -17,70 +18,116 @@ namespace osu.Game.Rulesets.Karaoke.Graphics.UserInterface
     {
         protected override string Title => "Select language";
 
+        private TranslateSearchTextBox filter;
+        private DrawableLanguageList languageList;
+
         public LanguageSelectionDialog()
         {
             RelativeSizeAxes = Axes.Both;
             Size = new Vector2(0.5f, 0.8f);
-        }
 
-        [BackgroundDependencyLoader]
-        private void load(OsuColour colours)
-        {
-            var languages = new BindableList<CultureInfo>(CultureInfo.GetCultures(CultureTypes.AllCultures));
-            Children = new Drawable[]
+            var languages = new BindableList<CultureInfo>(CultureInfo.GetCultures(CultureTypes.NeutralCultures));
+
+            Child = new GridContainer
             {
-                new DrawableLanguageList
+                RelativeSizeAxes = Axes.Both,
+                RowDimensions = new[]
                 {
-                    RelativeSizeAxes = Axes.Both,
-                    Items = { BindTarget = languages }
+                    new Dimension(GridSizeMode.Absolute, 50),
+                    new Dimension(GridSizeMode.Distributed)
+                },
+                Content = new[]
+                {
+                    new Drawable[]
+                    {
+                        filter = new TranslateSearchTextBox
+                        {
+                            RelativeSizeAxes = Axes.X,
+                        }
+                    },
+                    new Drawable[]
+                    {
+                        languageList = new DrawableLanguageList
+                        {
+                            RelativeSizeAxes = Axes.Both,
+                            Items = { BindTarget = languages }
+                        }
+                    }
                 }
             };
+
+            filter.Current.BindValueChanged(e => languageList.Filter(e.NewValue));
+        }
+
+        public class TranslateSearchTextBox : SearchTextBox
+        {
+            protected override Color4 SelectionColour => Color4.Gray;
+
+            public TranslateSearchTextBox()
+            {
+                PlaceholderText = @"type in keywords...";
+            }
         }
 
         public class DrawableLanguageList : OsuRearrangeableListContainer<CultureInfo>
         {
-            protected override FillFlowContainer<RearrangeableListItem<CultureInfo>> CreateListFillFlowContainer() => new Flow
+            public void Filter(string text)
             {
-                DragActive = { BindTarget = DragActive }
+                var items = (SearchContainer<RearrangeableListItem<CultureInfo>>)ListContainer;
+                items.SearchTerm = text;
+            }
+
+            protected override FillFlowContainer<RearrangeableListItem<CultureInfo>> CreateListFillFlowContainer() => new SearchContainer<RearrangeableListItem<CultureInfo>>
+            {
+                Spacing = new Vector2(0, 3),
+                LayoutDuration = 200,
+                LayoutEasing = Easing.OutQuint,
             };
 
             protected override OsuRearrangeableListItem<CultureInfo> CreateOsuDrawable(CultureInfo item)
                 => new DrawableLanguageListItem(item);
 
-            /// <summary>
-            /// The flow of <see cref="DrawableLanguageListItem"/>. Disables layout easing unless a drag is in progress.
-            /// </summary>
-            private class Flow : FillFlowContainer<RearrangeableListItem<CultureInfo>>
-            {
-                public readonly IBindable<bool> DragActive = new Bindable<bool>();
-
-                public Flow()
-                {
-                    Spacing = new Vector2(0, 5);
-                    LayoutEasing = Easing.OutQuint;
-                }
-
-                protected override void LoadComplete()
-                {
-                    base.LoadComplete();
-                    DragActive.BindValueChanged(active => LayoutDuration = active.NewValue ? 200 : 0);
-                }
-            }
-
-            public class DrawableLanguageListItem : OsuRearrangeableListItem<CultureInfo>
+            public class DrawableLanguageListItem : OsuRearrangeableListItem<CultureInfo>, IFilterable
             {
                 public DrawableLanguageListItem(CultureInfo item)
                     : base(item)
                 {
                     Padding = new MarginPadding { Left = 5 };
+                    FilterTerms = new List<string>
+                    {
+                        item.Name,
+                        item.DisplayName,
+                        item.EnglishName,
+                        item.NativeName
+                    };
                 }
 
                 protected override Drawable CreateContent() => new OsuTextFlowContainer
                 {
                     RelativeSizeAxes = Axes.X,
                     AutoSizeAxes = Axes.Y,
-                    Text = Model.DisplayName,
+                    Text = Model.EnglishName,
                 };
+                public IEnumerable<string> FilterTerms { get; }
+
+                private bool matchingFilter = true;
+
+                public bool MatchingFilter
+                {
+                    get => matchingFilter;
+                    set
+                    {
+                        if (matchingFilter == value)
+                            return;
+
+                        matchingFilter = value;
+                        updateFilter();
+                    }
+                }
+
+                private void updateFilter() => this.FadeTo(MatchingFilter ? 1 : 0, 200);
+
+                public bool FilteringActive { get; set; }
             }
         }
     }
