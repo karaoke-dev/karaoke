92b23ef55401249e33d656271a5647fdd7ec3ca8
andy840119 <andy840119@gmail.com>
2020-12-20 18:52:59 +0900

    Fix header.

--- osu.Game.Rulesets.Karaoke/Edit/Lyrics/Lyrics/Components/IHasTimeTag.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Lyrics/Components/IHasTimeTag.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
-// See the LICENCE file in the repository root for full licence text.ions.Generic;
+// See the LICENCE file in the repository root for full licence text.
 
 using osu.Game.Rulesets.Karaoke.Objects;
 
