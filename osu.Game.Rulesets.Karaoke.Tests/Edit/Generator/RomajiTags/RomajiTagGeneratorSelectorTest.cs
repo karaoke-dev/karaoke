cf1cd26f5a3a75d85db50a15225d28ef06645ef1
andy840119 <andy840119@gmail.com>
2020-12-26 18:57:28 +0900

    Implement selector test.

---
+++ osu.Game.Rulesets.Karaoke.Tests/Edit/Generator/RomajiTags/RomajiTagGeneratorSelectorTest.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Globalization;
+using NUnit.Framework;
+using osu.Game.Rulesets.Karaoke.Edit.Generator.RomajiTags;
+using osu.Game.Rulesets.Karaoke.Objects;
+using osu.Game.Rulesets.Karaoke.Tests.Helper;
+
+namespace osu.Game.Rulesets.Karaoke.Tests.Edit.Generator.RomajiTags
+{
+    public class RomajiTagGeneratorSelectorTest
+    {
+        [TestCase(17, "花火大会", new[] { "[0,2]:hanabi", "[2,4]:taikai" })] // Japanese
+        [TestCase(1041, "はなび", new[] { "[0,3]:hanabi" })] // Japanese
+        [TestCase(1028, "はなび", null)] // Chinses(should not supported)
+        public void TestCreateRomajiTag(int lcid, string text, string[] actualRomaji)
+        {
+            var lyric = new Lyric
+            {
+                Language = new CultureInfo(lcid),
+                Text = text,
+            };
+            var selector = new RomajiTagGeneratorSelector();
+            var generatedRomaji = selector.GenerateRomajiTags(lyric);
+            Assert.AreEqual(generatedRomaji, TestCaseTagHelper.ParseRomajiTags(actualRomaji));
+        }
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/Edit/Generator/TimeTags/TimeTagGeneratorSelector.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Globalization;
+using osu.Game.Rulesets.Karaoke.Edit.Generator.TimeTags.Ja;
+using osu.Game.Rulesets.Karaoke.Edit.Generator.TimeTags.Zh;
+using osu.Game.Rulesets.Karaoke.Objects;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Generator.TimeTags
+{
+    public class TimeTagGeneratorSelector : GeneratorSelector<TimeTagGenerator, TimeTagGeneratorConfig>
+    {
+        public TimeTagGeneratorSelector()
+        {
+            RegistGenerator<JaTimeTagGenerator, JaTimeTagGeneratorConfig>(new CultureInfo(17));
+            RegistGenerator<JaTimeTagGenerator, JaTimeTagGeneratorConfig>(new CultureInfo(1041));
+            RegistGenerator<ZhTimeTagGenerator, ZhTimeTagGeneratorConfig>(new CultureInfo(1028));
+        }
+
+        public TimeTag[] GenerateTimeTags(Lyric lyric)
+        {
+            if (!Generator.TryGetValue(lyric.Language, out var generator))
+                return null;
+
+            return generator.Value.CreateTimeTags(lyric);
+        }
+    }
+}
