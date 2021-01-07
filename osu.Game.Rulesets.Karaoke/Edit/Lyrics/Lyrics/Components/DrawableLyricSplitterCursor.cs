0398f5fc0c7bc71348ff8af95e2a42377cbe677b
andy840119 <andy840119@gmail.com>
2021-01-07 23:22:54 +0900

    Change split lyirc icon more understandable.

--- osu.Game.Rulesets.Karaoke/Edit/Lyrics/Lyrics/Components/DrawableLyricSplitterCursor.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Lyrics/Components/DrawableLyricSplitterCursor.cs
@@ -5,6 +5,7 @@ using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
 using osu.Game.Graphics;
 using osuTK;
 
@@ -13,44 +14,55 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Lyrics.Components
     public class DrawableLyricSplitterCursor : CompositeDrawable, IDrawableCursor
     {
         private readonly Container splitter;
+        private readonly SpriteIcon splitIcon;
 
         public DrawableLyricSplitterCursor()
         {
             RelativeSizeAxes = Axes.Y;
-            AutoSizeAxes = Axes.X;
-            InternalChild = splitter = new Container
+            Width = 10;
+            InternalChildren = new Drawable[]
             {
-                RelativeSizeAxes = Axes.Y,
-                AutoSizeAxes = Axes.X,
-                Children = new Drawable[]
+                splitIcon = new SpriteIcon
                 {
-                    new Triangle
+                    Anchor = Anchor.TopCentre,
+                    Origin = Anchor.BottomCentre,
+                    Icon = FontAwesome.Solid.HandScissors,
+                    X = 7,
+                    Y = -5,
+                    Size = new Vector2(10),
+                },
+                splitter = new Container
+                {
+                    RelativeSizeAxes = Axes.Y,
+                    AutoSizeAxes = Axes.X,
+                    Children = new Drawable[]
                     {
-                        Anchor = Anchor.TopCentre,
-                        Origin = Anchor.BottomCentre,
-                        Scale = new Vector2(1, -1),
-                        Size = new Vector2(10, 5),
-                    },
-                    new Triangle
-                    {
-                        Anchor = Anchor.BottomCentre,
-                        Origin = Anchor.BottomCentre,
-                        Size = new Vector2(10, 5)
-                    },
-                    new Box
-                    {
-                        Anchor = Anchor.Centre,
-                        Origin = Anchor.Centre,
-                        RelativeSizeAxes = Axes.Y,
-                        Width = 2,
-                        EdgeSmoothness = new Vector2(1, 0)
+                        new Triangle
+                        {
+                            Anchor = Anchor.TopCentre,
+                            Origin = Anchor.BottomCentre,
+                            Scale = new Vector2(1, -1),
+                            Size = new Vector2(10, 5),
+                        },
+                        new Box
+                        {
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre,
+                            RelativeSizeAxes = Axes.Y,
+                            Width = 2,
+                            EdgeSmoothness = new Vector2(1, 0)
+                        }
                     }
                 }
             };
         }
 
         [BackgroundDependencyLoader]
-        private void load(OsuColour colours) => splitter.Colour = colours.Red;
+        private void load(OsuColour colours)
+        {
+            splitter.Colour = colours.Red;
+            splitIcon.Colour = colours.Yellow;
+        }
 
         private bool preview;
 
@@ -61,6 +73,7 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Lyrics.Components
             {
                 preview = value;
                 splitter.Alpha = preview ? 0.5f : 1;
+                splitIcon.Alpha = preview ? 1 : 0;
             }
         }
     }
