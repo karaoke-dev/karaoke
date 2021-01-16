c003d4a1b2c53bee2fd19db03d2ea29f68858da1
andy840119 <andy840119@gmail.com>
2021-01-16 11:34:25 +0900

    Create convertor Because it's for test purpose, so not write test case for now.

---
+++ osu.Game.Rulesets.Karaoke/Beatmaps/Formats/KaraokeJsonBeatmapDecoder.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using osu.Game.Beatmaps;
+using osu.Game.Beatmaps.Formats;
+using osu.Game.IO;
+using osu.Game.IO.Serialization;
+using osu.Game.Rulesets.Karaoke.IO.Serialization.Converters;
+
+namespace osu.Game.Rulesets.Karaoke.Beatmaps.Formats
+{
+    public class KaraokeJsonBeatmapDecoder : JsonBeatmapDecoder
+    {
+        protected override void ParseStreamInto(LineBufferedReader stream, Beatmap output)
+        {
+            var convertor = new List<JsonConverter>
+            {
+                new CultureInfoConverter(),
+                new RomajiTagConverter(),
+                new RubyTagConverter(),
+                new TimeTagConverter(),
+                new ToneConverter(),
+            };
+
+            var globalSetting = JsonSerializableExtensions.CreateGlobalSettings();
+            convertor.AddRange(globalSetting.Converters);
+            globalSetting.Converters = convertor.ToArray();
+
+            // replace string stream.ReadToEnd().DeserializeInto(output);
+            JsonConvert.PopulateObject(stream.ReadToEnd(), output, globalSetting);
+
+            base.ParseStreamInto(stream, output);
+        }
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/Beatmaps/Formats/KaraokeJsonBeatmapEncoder.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using osu.Game.Beatmaps;
+using osu.Game.IO.Serialization;
+using osu.Game.Rulesets.Karaoke.IO.Serialization.Converters;
+
+namespace osu.Game.Rulesets.Karaoke.Beatmaps.Formats
+{
+    public class KaraokeJsonBeatmapEncoder
+    {
+        public string Encode(Beatmap output)
+        {
+            var convertor = new List<JsonConverter>
+            {
+                new CultureInfoConverter(),
+                new RomajiTagConverter(),
+                new RubyTagConverter(),
+                new TimeTagConverter(),
+                new ToneConverter(),
+            };
+
+            var globalSetting = JsonSerializableExtensions.CreateGlobalSettings();
+            convertor.AddRange(globalSetting.Converters);
+            globalSetting.Converters = convertor.ToArray();
+
+            // replace string stream.ReadToEnd().Serialize(output);
+            return JsonConvert.SerializeObject(output, globalSetting);
+        }
+    }
+}
