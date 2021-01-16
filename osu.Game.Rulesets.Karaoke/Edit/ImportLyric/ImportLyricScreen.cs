1f9a91f6f39934e6775022f877865bea68b16917
andy840119 <andy840119@gmail.com>
2021-01-16 17:19:35 +0900

    Implement color convertor.

--- osu.Game.Rulesets.Karaoke/Edit/ImportLyric/ImportLyricScreen.cs
+++ osu.Game.Rulesets.Karaoke/Edit/ImportLyric/ImportLyricScreen.cs
@@ -4,7 +4,6 @@
 using osu.Framework.Allocation;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Game.Graphics.Containers;
 using osu.Game.Rulesets.Karaoke.Edit.Components;
---
+++ osu.Game.Rulesets.Karaoke/IO/Serialization/Converters/ColourConvertor.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using osu.Framework.Extensions.Color4Extensions;
+using osuTK.Graphics;
+
+namespace osu.Game.Rulesets.Karaoke.IO.Serialization.Converters
+{
+    public class ColourConvertor : JsonConverter<Color4>
+    {
+        public override Color4 ReadJson(JsonReader reader, Type objectType, Color4 existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            var obj = JToken.Load(reader);
+            var value = obj.Value<string>();
+
+            if (value == null)
+                return new Color4();
+
+            return Color4Extensions.FromHex(value);
+        }
+
+        public override void WriteJson(JsonWriter writer, Color4 value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value.ToHex());
+        }
+    }
+}
