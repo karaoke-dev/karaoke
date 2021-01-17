7d1ccd20556bbba08120303a884bb0a4d5eaff22
andy840119 <andy840119@gmail.com>
2021-01-17 18:27:56 +0900

    Add circle click button.

--- osu.Game.Rulesets.Karaoke/Edit/Singers/Components/CreateNewLyricPlacementColumn.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Singers/Components/CreateNewLyricPlacementColumn.cs
@@ -1,9 +1,13 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Graphics.UserInterface;
 using osu.Game.Rulesets.Karaoke.Beatmaps.Metadatas;
+using osuTK;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.Singers.Components
 {
@@ -16,7 +20,24 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Singers.Components
 
         protected override float SingerInfoSize => 178;
 
-        // todo : might display add icon
-        protected override Drawable CreateSingerInfo(Singer singer) => new Container();
+        protected override Drawable CreateSingerInfo(Singer singer)
+        {
+            return new Container
+            {
+                Child = new IconButton
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Icon = FontAwesome.Solid.PlusCircle,
+                    Size = new Vector2(32),
+                    TooltipText = "Click to add new singer",
+                    Action = () => OnClick?.Invoke(),
+                }
+            };
+        }
+
+        protected override Drawable CreateTimeLinePart(KaraokeHitObjectComposer composer) => new Container();
+
+        public Action OnClick;
     }
 }
