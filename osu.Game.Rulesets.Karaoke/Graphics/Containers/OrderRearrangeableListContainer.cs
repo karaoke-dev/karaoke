d7fb9bc808ed11aee471de731432e8c7d5dd72b6
andy840119 <andy840119@gmail.com>
2021-01-23 16:42:08 +0900

    Create order raarrangeable list container to get order change event. And notice that Items.CollectionChanged caanot regist directly in singer manager, idk why.

---
+++ osu.Game.Rulesets.Karaoke/Graphics/Containers/OrderRearrangeableListContainer.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Collections.Specialized;
+using osu.Game.Graphics.Containers;
+
+namespace osu.Game.Rulesets.Karaoke.Graphics.Containers
+{
+    public abstract class OrderRearrangeableListContainer<TModel> : OsuRearrangeableListContainer<TModel>
+    {
+        public event Action<TModel, int> OnOrderChanged;
+
+        protected OrderRearrangeableListContainer()
+        {
+            Items.CollectionChanged += collectionChanged;
+        }
+
+        private void collectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                // should get the event if user change the position.
+                case NotifyCollectionChangedAction.Move:
+                    var item = (TModel)e.NewItems[0];
+                    var newInex = e.NewStartingIndex;
+                    OnOrderChanged?.Invoke(item, newInex);
+                    break;
+            }
+        }
+    }
+}
