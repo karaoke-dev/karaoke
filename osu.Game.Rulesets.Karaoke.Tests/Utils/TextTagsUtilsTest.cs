1ce9650b8f8719840dc3764433fdfd067f1523f6
andy840119 <andy840119@gmail.com>
2021-02-07 00:25:13 +0900

    Remove unused namespace.

--- osu.Game.Rulesets.Karaoke.Tests/Utils/TextTagsUtilsTest.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Utils/TextTagsUtilsTest.cs
@@ -1,11 +1,7 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
-using System.Linq;
-using Microsoft.EntityFrameworkCore.Internal;
 using NUnit.Framework;
-using osu.Game.Rulesets.Karaoke.Objects;
 using osu.Game.Rulesets.Karaoke.Tests.Helper;
 using osu.Game.Rulesets.Karaoke.Utils;
 
