57c9be717ce0b2a88bc00229ae931acc0b44829a
andy840119 <andy840119@gmail.com>
2021-02-15 16:22:22 +0900

    Add comment for text tag. Note that time-tag and text-tag's index means two different things.

--- osu.Game.Rulesets.Karaoke/Objects/Types/ITextTag.cs
+++ osu.Game.Rulesets.Karaoke/Objects/Types/ITextTag.cs
@@ -5,8 +5,14 @@ namespace osu.Game.Rulesets.Karaoke.Objects.Types
 {
     public interface ITextTag : IHasText
     {
+        /// <summary>
+        /// Start index, this index means gap index between characters.
+        /// </summary>
         int StartIndex { get; set; }
 
+        /// <summary>
+        /// End index, this index means gap index between characters.
+        /// </summary>
         int EndIndex { get; set; }
     }
 }
