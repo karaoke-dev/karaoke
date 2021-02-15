1fa24f8674ff6ad95350d5c88f8b058f556c315c
andy840119 <andy840119@gmail.com>
2021-02-15 16:46:52 +0900

    Update missing file.

---
+++ osu.Game.Rulesets.Karaoke/Configuration/KaraokeRulesetEditCheckerConfigManager.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Configuration;
+using osu.Game.Rulesets.Karaoke.Edit.Checker.Lyrics;
+using osu.Game.Rulesets.Karaoke.Edit.Checker.Types;
+
+namespace osu.Game.Rulesets.Karaoke.Configuration
+{
+    public class KaraokeRulesetEditCheckerConfigManager : InMemoryConfigManager<KaraokeRulesetEditCheckerSetting>
+    {
+        protected override void InitialiseDefaults()
+        {
+            base.InitialiseDefaults();
+
+            // Lyric
+            Set(KaraokeRulesetEditCheckerSetting.Lyric, CreateDefaultConfig<LyricCheckerConfig>());
+        }
+
+        protected static T CreateDefaultConfig<T>() where T : IHasConfig<T>, new()
+            => new T().CreateDefaultConfig();
+    }
+
+    public enum KaraokeRulesetEditCheckerSetting
+    {
+        Lyric
+    }
+}
