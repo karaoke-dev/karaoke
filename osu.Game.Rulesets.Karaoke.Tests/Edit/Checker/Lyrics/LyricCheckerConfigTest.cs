b7a130aba3f72258f17302b9464f2aaec79596fe
andy840119 <andy840119@gmail.com>
2021-02-16 16:23:21 +0900

    Clean-up some code.

--- osu.Game.Rulesets.Karaoke.Tests/Edit/Checker/Lyrics/LyricCheckerConfigTest.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Edit/Checker/Lyrics/LyricCheckerConfigTest.cs
@@ -16,8 +16,8 @@ namespace osu.Game.Rulesets.Karaoke.Tests.Edit.Checker.Lyrics
     public class LyricCheckerConfigTest
     {
         [TestCase("[1000,3000]:カラオケ", new[] { "[0,start]:1000", "[3,end]:3000" }, new TimeInvalid[] { })]
-        [TestCase("[3000,1000]:カラオケ", new string[] { }, new [] { TimeInvalid.Overlapping })]
-        [TestCase("[2000,3000]:カラオケ", new[] { "[0,start]:1000", "[3,end]:3000" }, new [] { TimeInvalid.StartTimeInvalid })]
+        [TestCase("[3000,1000]:カラオケ", new string[] { }, new[] { TimeInvalid.Overlapping })]
+        [TestCase("[2000,3000]:カラオケ", new[] { "[0,start]:1000", "[3,end]:3000" }, new[] { TimeInvalid.StartTimeInvalid })]
         [TestCase("[1000,2000]:カラオケ", new[] { "[0,start]:1000", "[3,end]:3000" }, new[] { TimeInvalid.EndTimeInvalid })]
         public void TestCheckInvalidLyricTime(string lyricText, string[] timeTags, TimeInvalid[] invalid)
         {
@@ -31,9 +31,9 @@ namespace osu.Game.Rulesets.Karaoke.Tests.Edit.Checker.Lyrics
 
         [TestCase("カラオケ", new[] { "[0,start]:1000", "[1,start]:2000", "[2,start]:3000", "[3,start]:4000", "[3,end]:5000" }, new TimeTagInvalid[] { })]
         [TestCase("カラオケ", new[] { "[0,start]:1000", "[3,end]:5000" }, new TimeTagInvalid[] { })]
-        [TestCase("カラオケ", new[] { "[-1,start]:1000" }, new [] { TimeTagInvalid.OutOfRange })]
+        [TestCase("カラオケ", new[] { "[-1,start]:1000" }, new[] { TimeTagInvalid.OutOfRange })]
         [TestCase("カラオケ", new[] { "[4,start]:4000" }, new[] { TimeTagInvalid.OutOfRange })]
-        [TestCase("カラオケ", new[] { "[0,start]:5000", "[3,end]:1000" }, new [] { TimeTagInvalid.Overlapping })]
+        [TestCase("カラオケ", new[] { "[0,start]:5000", "[3,end]:1000" }, new[] { TimeTagInvalid.Overlapping })]
         public void TestCheckInvalidTimeTags(string text, string[] timeTags, TimeTagInvalid[] invalids)
         {
             var lyric = new Lyric
@@ -68,7 +68,7 @@ namespace osu.Game.Rulesets.Karaoke.Tests.Edit.Checker.Lyrics
 
         [TestCase("karaoke", new[] { "[0,2]:ka", "[2,4]:ra", "[4,5]:o", "[5,7]:ke" }, new RomajiTagInvalid[] { })]
         [TestCase("karaoke", new[] { "[0,7]:karaoke" }, new RomajiTagInvalid[] { })]
-        [TestCase("karaoke", new[] { "[-1,2]:ka" }, new [] { RomajiTagInvalid.OutOfRange })]
+        [TestCase("karaoke", new[] { "[-1,2]:ka" }, new[] { RomajiTagInvalid.OutOfRange })]
         [TestCase("karaoke", new[] { "[7,8]:ke" }, new[] { RomajiTagInvalid.OutOfRange })]
         [TestCase("karaoke", new[] { "[0,2]:ka", "[1,3]:ra" }, new[] { RomajiTagInvalid.Overlapping })]
         [TestCase("karaoke", new[] { "[0,3]:ka", "[1,2]:ra" }, new[] { RomajiTagInvalid.Overlapping })]
@@ -88,6 +88,6 @@ namespace osu.Game.Rulesets.Karaoke.Tests.Edit.Checker.Lyrics
         // create checker with default config.
         // config change is not test scope so use default it ok.
         private static LyricChecker createChecker()
-            => new LyricChecker(new LyricCheckerConfig().CreateDefaultConfig()); 
+            => new LyricChecker(new LyricCheckerConfig().CreateDefaultConfig());
     }
 }
