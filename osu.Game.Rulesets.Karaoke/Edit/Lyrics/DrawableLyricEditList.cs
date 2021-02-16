0e400be0ed1d10ca30944e0955fcc1fb0184603a
andy840119 <andy840119@gmail.com>
2021-02-16 14:46:34 +0900

    Remoe unused usnig.

--- osu.Game.Rulesets.Karaoke/Edit/Lyrics/DrawableLyricEditList.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/DrawableLyricEditList.cs
@@ -6,7 +6,6 @@ using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Graphics.Containers;
-using osu.Game.Rulesets.Karaoke.Configuration;
 using osu.Game.Rulesets.Karaoke.Edit.Lyrics.Components;
 using osu.Game.Rulesets.Karaoke.Graphics.Containers;
 using osu.Game.Rulesets.Karaoke.Objects;
