0c40adce10a9f702f9600d8c7fb43012de632324
andy840119 <andy840119@gmail.com>
2021-03-23 21:51:12 +0900

    Add initial dialog and its test case.

---
+++ osu.Game.Rulesets.Karaoke/Graphics/UserInterface/FontSelectionDialog.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Bindables;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.UserInterface;
+using osu.Game.Rulesets.Karaoke.Graphics.Containers;
+
+namespace osu.Game.Rulesets.Karaoke.Graphics.UserInterface
+{
+    public class FontSelectionDialog : TitleFocusedOverlayContainer, IHasCurrentValue<FontUsage>
+    {
+        protected override string Title => "Select font";
+
+        private readonly BindableWithCurrent<FontUsage> current = new BindableWithCurrent<FontUsage>();
+
+        public Bindable<FontUsage> Current
+        {
+            get => current.Current;
+            set => current.Current = value;
+        }
+    }
+}
