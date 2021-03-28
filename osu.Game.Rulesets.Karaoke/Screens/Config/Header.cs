14953391cbc60ec9b4e02d4fa701547c80498c0a
andy840119 <andy840119@gmail.com>
2021-03-28 15:52:39 +0900

    Implement hello config screen and its visual test case.

---
+++ osu.Game.Rulesets.Karaoke/Screens/Config/Header.cs
@@ -0,0 +1,126 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Extensions.Color4Extensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Graphics.UserInterface;
+using osu.Game.Overlays;
+using osu.Game.Screens;
+using osuTK;
+
+namespace osu.Game.Rulesets.Karaoke.Screens.Config
+{
+    public class Header : Container
+    {
+        public const float HEIGHT = 80;
+
+        public Header()
+        {
+            RelativeSizeAxes = Axes.X;
+            Height = HEIGHT;
+
+            KaraokeConfigHeaderTitle title;
+            PageTabControl<KaraokeConfigCategory> tabs;
+
+            Children = new Drawable[]
+            {
+                new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = Color4Extensions.FromHex(@"#1f1921"),
+                },
+                new Container
+                {
+                    Anchor = Anchor.CentreLeft,
+                    Origin = Anchor.CentreLeft,
+                    RelativeSizeAxes = Axes.Both,
+                    Padding = new MarginPadding { Left = WaveOverlayContainer.WIDTH_PADDING + OsuScreen.HORIZONTAL_OVERFLOW_PADDING },
+                    Children = new Drawable[]
+                    {
+                        title = new KaraokeConfigHeaderTitle
+                        {
+                            Anchor = Anchor.CentreLeft,
+                            Origin = Anchor.BottomLeft,
+                        },
+                        tabs = new PageTabControl<KaraokeConfigCategory>
+                        {
+                            Anchor = Anchor.BottomLeft,
+                            Origin = Anchor.BottomLeft,
+                            RelativeSizeAxes = Axes.X,
+                        },
+                    },
+                },
+            };
+
+            tabs.Current.Value = KaraokeConfigCategory.Config;
+            tabs.Current.BindValueChanged(x =>
+            {
+                // todo : might apply translate in here.
+                title.PageTitle = x.NewValue.ToString();
+            }, true);
+            
+        }
+
+        private class KaraokeConfigHeaderTitle : CompositeDrawable
+        {
+            private const float spacing = 6;
+
+            private readonly OsuSpriteText dot;
+            private readonly OsuSpriteText pageTitle;
+
+            public string PageTitle
+            {
+                set => pageTitle.Text = value;
+            }
+
+            public KaraokeConfigHeaderTitle()
+            {
+                AutoSizeAxes = Axes.Both;
+
+                InternalChildren = new Drawable[]
+                {
+                    new FillFlowContainer
+                    {
+                        AutoSizeAxes = Axes.Both,
+                        Spacing = new Vector2(spacing, 0),
+                        Direction = FillDirection.Horizontal,
+                        Children = new Drawable[]
+                        {
+                            new OsuSpriteText
+                            {
+                                Anchor = Anchor.CentreLeft,
+                                Origin = Anchor.CentreLeft,
+                                Font = OsuFont.GetFont(size: 24),
+                                Text = "Karaoke"
+                            },
+                            dot = new OsuSpriteText
+                            {
+                                Anchor = Anchor.CentreLeft,
+                                Origin = Anchor.CentreLeft,
+                                Font = OsuFont.GetFont(size: 24),
+                                Text = "·"
+                            },
+                            pageTitle = new OsuSpriteText
+                            {
+                                Anchor = Anchor.CentreLeft,
+                                Origin = Anchor.CentreLeft,
+                                Font = OsuFont.GetFont(size: 24),
+                            }
+                        }
+                    },
+                };
+            }
+
+            [BackgroundDependencyLoader]
+            private void load(OsuColour colours)
+            {
+                pageTitle.Colour = dot.Colour = colours.Yellow;
+            }
+        }
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/Screens/Config/KaraokeConfigScreen.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Extensions.Color4Extensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
+using osu.Game.Graphics.Containers;
+using osu.Game.Screens;
+
+namespace osu.Game.Rulesets.Karaoke.Screens.Config
+{
+    public class KaraokeConfigScreen : OsuScreen
+    {
+        private readonly KaraokeConfigWaveContainer waves;
+
+        public KaraokeConfigScreen()
+        {
+            var backgroundColour = Color4Extensions.FromHex(@"3e3a44");
+
+            InternalChild = waves = new KaraokeConfigWaveContainer
+            {
+                RelativeSizeAxes = Axes.Both,
+                Children = new Drawable[]
+                {
+                    new Box
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Colour = backgroundColour,
+                    },
+                    new KaraokeEditInputManager(new KaraokeRuleset().RulesetInfo)
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Padding = new MarginPadding { Top = Header.HEIGHT },
+                    },
+                    new Header(),
+                }
+            };
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            waves.Show();
+        }
+
+        private class KaraokeConfigWaveContainer : WaveContainer
+        {
+            protected override bool StartHidden => true;
+
+            public KaraokeConfigWaveContainer()
+            {
+                FirstWaveColour = Color4Extensions.FromHex(@"654d8c");
+                SecondWaveColour = Color4Extensions.FromHex(@"554075");
+                ThirdWaveColour = Color4Extensions.FromHex(@"44325e");
+                FourthWaveColour = Color4Extensions.FromHex(@"392850");
+            }
+        }
+    }
+}
