ce1632600c82b3b99518b6c08343ffbd4a7a5a77
andy840119 <andy840119@gmail.com>
2021-03-28 17:41:53 +0900

    Create first setting section and setting subscription class.

---
+++ osu.Game.Rulesets.Karaoke/Screens/Config/KaraokeSettingsSubsection.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Game.Overlays.Settings;
+using osu.Game.Rulesets.Karaoke.Configuration;
+
+namespace osu.Game.Rulesets.Karaoke.Screens.Config
+{
+    public abstract class KaraokeSettingsSubsection : SettingsSubsection
+    {
+        [Resolved]
+        protected KaraokeRulesetConfigManager Config { get; set; }
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/Screens/Config/Sections/ScoringSection.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Overlays.Settings;
+using osu.Game.Rulesets.Karaoke.Screens.Config.Sections.Input;
+
+namespace osu.Game.Rulesets.Karaoke.Screens.Config.Sections
+{
+    public class ScoringSection : SettingsSection
+    {
+        public override string Header => "Scoring";
+
+        public override Drawable CreateIcon() => new SpriteIcon
+        {
+            Icon = FontAwesome.Solid.Gamepad
+        };
+
+        public ScoringSection()
+        {
+            Children = new Drawable[]
+            {
+                new MicrophoneSettings(),
+            };
+        }
+    }
+}
