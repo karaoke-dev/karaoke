baa13d192aee776b963a45b877d10a5f52d6fb1c
andy840119 <andy840119@gmail.com>
2021-04-10 13:10:26 +0900

    Font usage is able to get target size range.

--- osu.Game.Rulesets.Karaoke/Utils/FontUtils.cs
+++ osu.Game.Rulesets.Karaoke/Utils/FontUtils.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
+
 namespace osu.Game.Rulesets.Karaoke.Utils
 {
     public static class FontUtils
@@ -11,6 +13,18 @@ namespace osu.Game.Rulesets.Karaoke.Utils
         /// <returns></returns>
         public static float[] DefaultFontSize()
             => new float[] { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+
+        public static float[] DefaultFontSize(float? minSize = null, float? maxSize = null)
+            => DefaultFontSize().Where(x =>
+            {
+                if (minSize.HasValue && x < minSize.Value)
+                    return false;
+
+                if (maxSize.HasValue && x > maxSize.Value)
+                    return false;
+
+                return true;
+            }).ToArray();
 
         /// <summary>
         /// For selecting preview size in editor.
