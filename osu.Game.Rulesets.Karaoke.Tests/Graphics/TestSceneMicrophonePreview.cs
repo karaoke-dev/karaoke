fe734c9a579fecd5e7ff8ea8434551321b4d5ad7
andy840119 <andy840119@gmail.com>
2021-04-11 15:17:32 +0900

    Update package and implement base preview component.

---
+++ osu.Game.Rulesets.Karaoke.Tests/Graphics/TestSceneMicrophonePreview.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Framework.Graphics;
+using osu.Framework.Input;
+using osu.Game.Rulesets.Karaoke.Graphics.UserInterface;
+using osu.Game.Tests.Visual;
+
+namespace osu.Game.Rulesets.Karaoke.Tests.Graphics
+{
+    [TestFixture]
+    public class TestSceneMicrophonePreview : OsuTestScene
+    {
+        private MicrophoneSoundVisualizer preview;
+
+        [SetUp]
+        public void SetUp() => Schedule(() =>
+        {
+            Child = new MicrophoneInputManager(-1)
+            {
+                Child = preview = new MicrophoneSoundVisualizer
+                {
+                    Width = 300,
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre
+                }
+            };
+        });
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/Graphics/UserInterface/MicrophoneSoundVisualizer.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Input.Events;
+
+namespace osu.Game.Rulesets.Karaoke.Graphics.UserInterface
+{
+    public class MicrophoneSoundVisualizer : CompositeDrawable
+    {
+        protected override bool Handle(UIEvent e)
+        {
+            switch (e)
+            {
+                case MicrophoneStartPitchingEvent microphoneStartPitching:
+                    return OnMicrophoneStartSinging(microphoneStartPitching);
+
+                case MicrophoneEndPitchingEvent microphoneEndPitching:
+                    return OnMicrophoneEndSinging(microphoneEndPitching);
+
+                case MicrophonePitchingEvent microphonePitching:
+                    return OnMicrophoneSinging(microphonePitching);
+
+                default:
+                    return base.Handle(e);
+            }
+        }
+
+        protected virtual bool OnMicrophoneStartSinging(MicrophoneStartPitchingEvent e)
+        {
+            return false;
+        }
+
+        protected virtual bool OnMicrophoneEndSinging(MicrophoneEndPitchingEvent e)
+        {
+            return false;
+        }
+
+        protected virtual bool OnMicrophoneSinging(MicrophonePitchingEvent e)
+        {
+            var loudness = e.CurrentState.Microphone.Loudness;
+            var pitch = e.CurrentState.Microphone.Pitch;
+
+            // todo : draw the pitch position and loudness into this composite drawable.
+
+            return false;
+        }
+
+        internal class LoudnessVisualizer : CompositeDrawable
+        {
+            public LoudnessVisualizer()
+            {
+                // todo : see how osu-logo edge do
+            }
+        }
+
+        internal class PitchVisualier : CompositeDrawable
+        {
+            public PitchVisualier()
+            {
+                // todo : draw that stupid shapes with progressive background color.
+            }
+        }
+    }
+}
