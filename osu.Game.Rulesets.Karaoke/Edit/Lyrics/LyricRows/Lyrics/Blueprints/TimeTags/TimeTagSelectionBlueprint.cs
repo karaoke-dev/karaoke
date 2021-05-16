1ed96309db1dddfdf678116070a70cc8815540b5
andy840119 <andy840119@gmail.com>
2021-05-16 17:46:19 +0900

    Add time-tag blueprint container and it's blueprint.

---
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/LyricRows/Lyrics/Blueprints/TimeTags/TimeTagSelectionBlueprint.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Game.Rulesets.Karaoke.Objects;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.LyricRows.Lyrics.Blueprints.TimeTags
+{
+    public class TimeTagSelectionBlueprint : TagSelectionBlueprint<TimeTag>
+    {
+        [Resolved]
+        private EditorLyricPiece editorLyricPiece { get; set; }
+
+        public TimeTagSelectionBlueprint(TimeTag item)
+            : base(item)
+        {
+            updatePosition();
+        }
+
+        private void updatePosition()
+        {
+            // todo : should be able to get time-tag position.
+            var position = editorLyricPiece.GetTimeTagPosition(Item);
+            UpdatePositionAndSize(position);
+        }
+    }
+}
