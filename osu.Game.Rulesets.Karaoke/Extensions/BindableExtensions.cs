da901aced9a9a75d795dc114ba08538c65b49e07
andy840119 <andy840119@gmail.com>
2021-05-20 21:14:55 +0900

    Add bindable extension for manage add / reomve objects in bindable<array[]>

---
+++ osu.Game.Rulesets.Karaoke/Extensions/BindableExtensions.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Linq;
+using osu.Framework.Bindables;
+
+namespace osu.Game.Rulesets.Karaoke.Extensions
+{
+    public static class BindableExtensions
+    {
+        public static void BindArrayChanged<T>(this Bindable<T[]> bindable, Action<T[]> add, Action<T[]> remove, bool runOnceImmediately = false)
+        {
+            // Add time-tag into blueprint container
+            bindable.BindValueChanged(e =>
+            {
+                // remove old item.
+                var removedItems = e.OldValue?.Except(e.NewValue).ToArray() ?? new T[] {};
+                remove?.Invoke(removedItems);
+
+                // add new time-tags
+                var newItems = e.NewValue ?? new T[] { };
+                add?.Invoke(newItems);
+            }, runOnceImmediately);
+        }
+    }
+}
