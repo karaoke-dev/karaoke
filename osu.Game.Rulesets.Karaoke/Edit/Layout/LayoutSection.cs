9b0a2ee35aeca99749672aa2465a47006eaa3db6
andy840119 <andy840119@gmail.com>
2021-05-23 16:02:10 +0900

    Create base section class for collecting all relative logic.

--- osu.Game.Rulesets.Karaoke/Edit/Layout/LayoutSection.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Layout/LayoutSection.cs
@@ -1,54 +1,11 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Allocation;
-using osu.Framework.Bindables;
-using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
-using osu.Game.Beatmaps;
-using osu.Game.Graphics;
-using osu.Game.Graphics.Sprites;
-using osuTK;
+using osu.Game.Rulesets.Karaoke.Edit.Components.Containers;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.Layout
 {
-    internal abstract class LayoutSection : Container
+    internal abstract class LayoutSection : Section
     {
-        private readonly FillFlowContainer flow;
-
-        [Resolved]
-        protected OsuColour Colours { get; private set; }
-
-        [Resolved]
-        protected IBindable<WorkingBeatmap> Beatmap { get; private set; }
-
-        protected override Container<Drawable> Content => flow;
-
-        protected abstract string Title { get; }
-
-        protected LayoutSection()
-        {
-            RelativeSizeAxes = Axes.X;
-            AutoSizeAxes = Axes.Y;
-
-            Padding = new MarginPadding(10);
-
-            InternalChildren = new Drawable[]
-            {
-                new OsuSpriteText
-                {
-                    Font = OsuFont.GetFont(weight: FontWeight.Bold, size: 18),
-                    Text = Title,
-                },
-                flow = new FillFlowContainer
-                {
-                    RelativeSizeAxes = Axes.X,
-                    AutoSizeAxes = Axes.Y,
-                    Spacing = new Vector2(10),
-                    Direction = FillDirection.Vertical,
-                    Margin = new MarginPadding { Top = 30 }
-                }
-            };
-        }
     }
 }
