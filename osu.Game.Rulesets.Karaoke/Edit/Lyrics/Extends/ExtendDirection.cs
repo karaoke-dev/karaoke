e988d15b5efa9dbe3cbb6824fa9d75c8dfb7d82a
andy840119 <andy840119@gmail.com>
2021-05-23 15:27:03 +0900

    implement extend area for some edit mode have extra spacing in the left or right side.

---
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Extends/ExtendDirection.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends
+{
+    public enum ExtendDirection
+    {
+        /// <summary>
+        /// At the right side of the main lyric editor.
+        /// </summary>
+        Left,
+
+        /// <summary>
+        /// At the right side of the main lyric editor.
+        /// </summary>
+        Right
+    }
+}
