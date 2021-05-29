43c72da9100be2c3609440974bcffb376738ebd1
andy840119 <andy840119@gmail.com>
2021-05-29 21:23:51 +0900

    Let config logic become more simple.

--- osu.Game.Rulesets.Karaoke/Screens/Config/Previews/Gameplay/NotePlayfieldPreview.cs
+++ osu.Game.Rulesets.Karaoke/Screens/Config/Previews/Gameplay/NotePlayfieldPreview.cs
@@ -12,13 +12,11 @@ using osu.Game.Beatmaps;
 using osu.Game.Beatmaps.ControlPoints;
 using osu.Game.Rulesets.Karaoke.Configuration;
 using osu.Game.Rulesets.Karaoke.Objects;
-using osu.Game.Rulesets.Karaoke.Objects.Drawables;
 using osu.Game.Rulesets.Karaoke.Scoring;
 using osu.Game.Rulesets.Karaoke.UI;
 using osu.Game.Rulesets.Karaoke.UI.Position;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Timing;
-using osu.Game.Rulesets.UI;
 using osu.Game.Rulesets.UI.Scrolling;
 using osu.Game.Rulesets.UI.Scrolling.Algorithms;
 using osuTK;
@@ -29,47 +27,11 @@ namespace osu.Game.Rulesets.Karaoke.Screens.Config.Previews.Gameplay
     {
         private const int row_amount = 9;
 
-        /// <summary>
-        /// The default span of time visible by the length of the scrolling axes.
-        /// This is clamped between <see cref="time_span_min"/> and <see cref="time_span_max"/>.
-        /// </summary>
-        private const double time_span_default = 1500;
-
-        /// <summary>
-        /// The minimum span of time that may be visible by the length of the scrolling axes.
-        /// </summary>
-        private const double time_span_min = 50;
-
-        /// <summary>
-        /// The maximum span of time that may be visible by the length of the scrolling axes.
-        /// </summary>
-        private const double time_span_max = 10000;
-
         [Cached(Type = typeof(IScrollingInfo))]
         private readonly LocalScrollingInfo scrollingInfo;
 
         [Cached(Type = typeof(IPositionCalculator))]
         private readonly PositionCalculator positionCalculator;
-
-        /// <summary>
-        /// The <see cref="MultiplierControlPoint"/>s that adjust the scrolling rate of <see cref="HitObject"/>s inside this <see cref="DrawableRuleset"/>.
-        /// </summary>
-        protected readonly SortedList<MultiplierControlPoint> ControlPoints = new SortedList<MultiplierControlPoint>(Comparer<MultiplierControlPoint>.Default);
-
-        protected readonly Bindable<ScrollingDirection> Direction = new Bindable<ScrollingDirection>();
-
-        private readonly Bindable<KaraokeScrollingDirection> configDirection = new Bindable<KaraokeScrollingDirection>();
-
-        /// <summary>
-        /// The span of time that is visible by the length of the scrolling axes.
-        /// For example, only hit objects with start time less than or equal to 1000 will be visible with <see cref="TimeRange"/> = 1000.
-        /// </summary>
-        protected readonly BindableDouble TimeRange = new BindableDouble(time_span_default)
-        {
-            Default = time_span_default,
-            MinValue = time_span_min,
-            MaxValue = time_span_max
-        };
 
         private readonly NotePlayfield notePlayfield;
 
@@ -78,9 +40,7 @@ namespace osu.Game.Rulesets.Karaoke.Screens.Config.Previews.Gameplay
             Size = new Vector2(0.7f, 0.5f);
 
             positionCalculator = new PositionCalculator(row_amount);
-            scrollingInfo = new LocalScrollingInfo { Algorithm = new SequentialScrollAlgorithm(ControlPoints) };
-            scrollingInfo.Direction.BindTo(Direction);
-            scrollingInfo.TimeRange.BindTo(TimeRange);
+            scrollingInfo = new LocalScrollingInfo();
 
             Child = new Container
             {
@@ -121,13 +81,19 @@ namespace osu.Game.Rulesets.Karaoke.Screens.Config.Previews.Gameplay
             }
         }
 
+        private readonly Bindable<KaraokeScrollingDirection> configDirection = new Bindable<KaraokeScrollingDirection>();
+
         [BackgroundDependencyLoader]
         private void load()
         {
             Config.BindWith(KaraokeRulesetSetting.ScrollDirection, configDirection);
-            configDirection.BindValueChanged(direction => Direction.Value = (ScrollingDirection)direction.NewValue, true);
+            configDirection.BindValueChanged(direction =>
+            {
+                if (scrollingInfo.Direction is Bindable<ScrollingDirection> bindableScrollingDirection)
+                    bindableScrollingDirection.Value = (ScrollingDirection)direction.NewValue;
+            }, true);
 
-            Config.BindWith(KaraokeRulesetSetting.ScrollTime, TimeRange);
+            Config.BindWith(KaraokeRulesetSetting.ScrollTime, scrollingInfo.TimeRange as BindableDouble);
         }
 
         private IBeatmap createSampleBeatmap()
@@ -154,9 +120,9 @@ namespace osu.Game.Rulesets.Karaoke.Screens.Config.Previews.Gameplay
         {
             public IBindable<ScrollingDirection> Direction { get; } = new Bindable<ScrollingDirection>();
 
-            public IBindable<double> TimeRange { get; } = new BindableDouble();
+            public IBindable<double> TimeRange { get; } = new BindableDouble(1500);
 
-            public IScrollAlgorithm Algorithm { get; set; }
+            public IScrollAlgorithm Algorithm { get; } = new SequentialScrollAlgorithm(new SortedList<MultiplierControlPoint>(Comparer<MultiplierControlPoint>.Default));
         }
     }
 }
