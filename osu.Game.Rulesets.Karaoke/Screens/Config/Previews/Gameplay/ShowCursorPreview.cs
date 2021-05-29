9b32b1d9ac05f59eb554daf6b4d728f979530897
andy840119 <andy840119@gmail.com>
2021-05-29 21:56:00 +0900

    Implement show cursor preview.

---
+++ osu.Game.Rulesets.Karaoke/Screens/Config/Previews/Gameplay/ShowCursorPreview.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Game.Graphics.Cursor;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Rulesets.Karaoke.Configuration;
+using osuTK;
+
+namespace osu.Game.Rulesets.Karaoke.Screens.Config.Previews.Gameplay
+{
+    public class ShowCursorPreview : SettingsSubsectionPreview
+    {
+        private readonly MenuCursor.Cursor cursor;
+
+        public ShowCursorPreview()
+        {
+            Size = new Vector2(0.3f);
+            Children = new Drawable[]
+            {
+                cursor = new MenuCursor.Cursor
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre
+                },
+                new OsuSpriteText
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Y = 30,
+                    Text = "Wanna show this while gameplay?"
+                }
+            };
+        }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            var showCursorBindable = Config.GetBindable<bool>(KaraokeRulesetSetting.ShowCursor);
+            showCursorBindable.BindValueChanged(e =>
+            {
+                var showCursor = e.NewValue;
+
+                if (showCursor)
+                {
+                    cursor.FadeTo(1, 200);
+                }
+                else
+                {
+                    cursor.FadeTo(0.5f, 200);
+                }
+            }, true);
+        }
+    }
+}
