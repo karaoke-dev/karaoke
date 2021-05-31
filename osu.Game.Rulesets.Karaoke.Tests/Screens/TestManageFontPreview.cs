c71c8be1097e4eb2bde719c75b968993cfb234d5
andy840119 <andy840119@gmail.com>
2021-06-01 00:29:31 +0900

    will create egg swing at the corner if user click target text.

--- osu.Game.Rulesets.Karaoke.Tests/Screens/TestManageFontPreview.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Screens/TestManageFontPreview.cs
@@ -1,7 +1,11 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Textures;
+using osu.Framework.IO.Stores;
+using osu.Framework.Platform;
 using osu.Framework.Timing;
 using osu.Game.Overlays;
 using osu.Game.Rulesets.Karaoke.Screens.Config.Previews.Graphics;
@@ -18,18 +22,24 @@ namespace osu.Game.Rulesets.Karaoke.Tests.Screens
             Clock = new FramedClock();
             Clock.ProcessFrame();
 
-            AddRange(new Drawable[]
+            Add(np = new NowPlayingOverlay
             {
-                new ManageFontPreview
-                {
-                    Anchor = Anchor.Centre,
-                    Origin = Anchor.Centre,
-                },
-                np = new NowPlayingOverlay
-                {
-                    Origin = Anchor.TopRight,
-                    Anchor = Anchor.TopRight,
-                }
+                Origin = Anchor.TopRight,
+                Anchor = Anchor.TopRight,
+            });
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(GameHost host)
+        {
+            var resources = new KaraokeRuleset().CreateResourceStore();
+            var textureStore = new TextureStore(host.CreateTextureLoaderStore(new NamespacedResourceStore<byte[]>(resources, @"Textures")));
+            Dependencies.CacheAs(textureStore);
+
+            Add(new ManageFontPreview
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
             });
         }
 
