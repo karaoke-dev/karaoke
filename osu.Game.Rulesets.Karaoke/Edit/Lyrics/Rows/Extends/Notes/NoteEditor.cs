dd797234520e4e10cfca9760361d84d4f0061a29
andy840119 <andy840119@gmail.com>
2021-06-03 01:25:22 +0900

    Base step of converting ComposeBlueprintContainer into more generic blueprint container.

---
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Rows/Extends/Notes/NoteEditor.cs
@@ -0,0 +1,76 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using System.Linq;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Rulesets.Karaoke.Objects;
+using osu.Game.Rulesets.Karaoke.Timing;
+using osu.Game.Rulesets.Timing;
+using osu.Game.Rulesets.UI.Scrolling;
+using osu.Game.Rulesets.UI.Scrolling.Algorithms;
+using osu.Game.Screens.Edit;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Rows.Extends.Notes
+{
+    [Cached]
+    public class NoteEditor : Container
+    {
+        private const int row_amount = 9;
+
+        [Cached(Type = typeof(IScrollingInfo))]
+        private readonly LocalScrollingInfo scrollingInfo;
+
+        private readonly Lyric lyric;
+
+        public BindableList<Note> SelectedNotes { get; } = new BindableList<Note>();
+
+        public EditorNotePlayfield Playfield { get; }
+
+        public NoteEditor(Lyric lyric)
+        {
+            this.lyric = lyric;
+            scrollingInfo = new LocalScrollingInfo();
+
+            InternalChild = new Container
+            {
+                Name = "Content",
+                RelativeSizeAxes = Axes.Both,
+                Children = new Drawable[]
+                {
+                    // layers below playfield
+                    Playfield = new EditorNotePlayfield(row_amount),
+                    // layers above playfield
+                    new EditNoteBlueprintContainer(lyric),
+                }
+            };
+
+            // set stop clock and navigation to target time.
+            Playfield.Clock = new StopClock(lyric.LyricStartTime);
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(EditorBeatmap beatmap)
+        {
+            // add all matched notes into playfield
+            var notes = beatmap.HitObjects.OfType<Note>().Where(x => x.ParentLyric == lyric).ToList();
+
+            foreach (var note in notes)
+            {
+                Playfield.Add(note);
+            }
+        }
+
+        private class LocalScrollingInfo : IScrollingInfo
+        {
+            public IBindable<ScrollingDirection> Direction { get; } = new Bindable<ScrollingDirection>(ScrollingDirection.Left);
+
+            public IBindable<double> TimeRange { get; } = new BindableDouble(5000);
+
+            public IScrollAlgorithm Algorithm { get; set; } = new SequentialScrollAlgorithm(new List<MultiplierControlPoint>());
+        }
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Rows/Extends/Notes/NoteEditorHitObjectBlueprint.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.UserInterface;
+using osu.Game.Graphics.UserInterface;
+using osu.Game.Rulesets.Edit;
+using osu.Game.Rulesets.Karaoke.Edit.Blueprints.Notes.Components;
+using osu.Game.Rulesets.Karaoke.Edit.Notes;
+using osu.Game.Rulesets.Karaoke.Objects;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Rows.Extends.Notes
+{
+    public class NoteEditorHitObjectBlueprint : SelectionBlueprint<Note>
+    {
+        [Resolved]
+        private NoteManager noteManager { get; set; }
+
+        public NoteEditorHitObjectBlueprint(Note item)
+            : base(item)
+        {
+            AddInternal(new EditBodyPiece
+            {
+                RelativeSizeAxes = Axes.Both
+            });
+        }
+
+        public override MenuItem[] ContextMenuItems => new MenuItem[]
+        {
+            new OsuMenuItem(Item.Display ? "Hide" : "Show", Item.Display ? MenuItemType.Destructive : MenuItemType.Standard, () => noteManager.ChangeDisplay(Item, !Item.Display)),
+            new OsuMenuItem("Split", MenuItemType.Destructive, () => noteManager.SplitNote(Item)),
+        };
+    }
+}
