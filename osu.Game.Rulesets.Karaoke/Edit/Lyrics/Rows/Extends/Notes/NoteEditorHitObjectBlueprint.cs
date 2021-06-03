2011f1cc927066a49c8486deb80e6c40f171fbb6
andy840119 <andy840119@gmail.com>
2021-06-03 22:12:38 +0900

    write some sample code.

--- osu.Game.Rulesets.Karaoke/Edit/Lyrics/Rows/Extends/Notes/NoteEditorHitObjectBlueprint.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Rows/Extends/Notes/NoteEditorHitObjectBlueprint.cs
@@ -1,30 +1,73 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using JetBrains.Annotations;
 using osu.Framework.Allocation;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Primitives;
 using osu.Framework.Graphics.UserInterface;
 using osu.Game.Graphics.UserInterface;
 using osu.Game.Rulesets.Edit;
 using osu.Game.Rulesets.Karaoke.Edit.Blueprints.Notes.Components;
 using osu.Game.Rulesets.Karaoke.Edit.Notes;
 using osu.Game.Rulesets.Karaoke.Objects;
+using osu.Game.Rulesets.Karaoke.UI.Components;
+using osu.Game.Rulesets.Karaoke.UI.Position;
+using osuTK;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Rows.Extends.Notes
 {
     public class NoteEditorHitObjectBlueprint : SelectionBlueprint<Note>
     {
+        [UsedImplicitly]
+        private readonly Bindable<double> startTime;
+
+        [UsedImplicitly]
+        private readonly Bindable<Tone> tone;
+
+        private readonly EditBodyPiece editBodyPiece;
+
         [Resolved]
         private NoteManager noteManager { get; set; }
 
         public NoteEditorHitObjectBlueprint(Note item)
             : base(item)
         {
-            AddInternal(new EditBodyPiece
+            startTime = item.StartTimeBindable.GetBoundCopy();
+            tone = item.ToneBindable.GetBoundCopy();
+
+            AddInternal(editBodyPiece = new EditBodyPiece
             {
-                RelativeSizeAxes = Axes.Both
+                Anchor = Anchor.CentreLeft,
+                Origin = Anchor.CentreLeft,
+                Width = 100
             });
         }
+
+        [BackgroundDependencyLoader]
+        private void load(IPositionCalculator positionCalculator)
+        {
+            editBodyPiece.Height = DefaultColumnBackground.COLUMN_HEIGHT;
+
+            startTime.BindValueChanged(e =>
+            {
+                // todo : adjust x position
+            });
+
+            tone.BindValueChanged(e =>
+            {
+                // todo : adjust y position
+                editBodyPiece.Y = positionCalculator.YPositionAt(e.NewValue);
+            }, true);
+        }
+
+        public override bool ReceivePositionalInputAt(Vector2 screenSpacePos)
+            => editBodyPiece.ReceivePositionalInputAt(screenSpacePos);
+
+        public override Vector2 ScreenSpaceSelectionPoint => editBodyPiece.ScreenSpaceDrawQuad.Centre;
+
+        public override Quad SelectionQuad => editBodyPiece.ScreenSpaceDrawQuad;
 
         public override MenuItem[] ContextMenuItems => new MenuItem[]
         {
