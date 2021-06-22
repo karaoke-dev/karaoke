63067f66f370e4c0e460b14dcd1af65ce45fb33b
andy840119 <andy840119@gmail.com>
2021-06-22 22:08:07 +0900

    Move auto complete button into individual place.

---
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Extends/Components/AutoGenerateButton.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+using osu.Game.Graphics.UserInterface;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends.Components
+{
+    public class AutoGenerateButton : OsuButton
+    {
+        public AutoGenerateButton()
+        {
+            RelativeSizeAxes = Axes.X;
+            Content.CornerRadius = 15;
+        }
+    }
+}
