6bd925d486865ce9bd7c07b23ba965148a27d79e
andy840119 <andy840119@gmail.com>
2021-06-23 23:34:13 +0900

    update nuget and use new ruleset api.

--- osu.Game.Rulesets.Karaoke.Tests/Skinning/TestSceneLyric.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Skinning/TestSceneLyric.cs
@@ -82,8 +82,8 @@ namespace osu.Game.Rulesets.Karaoke.Tests.Skinning
             var drawable = CreateDrawableLyric(lyric, auto);
             drawable.DisplayTranslateLanguage = defaultLanguage;
 
-            foreach (var mod in SelectedMods.Value.OfType<IApplicableToDrawableHitObjects>())
-                mod.ApplyToDrawableHitObjects(new[] { drawable });
+            foreach (var mod in SelectedMods.Value.OfType<IApplicableToDrawableHitObject>())
+                mod.ApplyToDrawableHitObject(drawable);
 
             return drawable;
         }
