81281c155436167764adfe7461a9745f2dc0f073
andy840119 <andy840119@gmail.com>
2021-07-04 11:43:39 +0900

    add base auto generate section and let other class to inherit

---
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Extends/Components/AutoGenerateSection.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using System.Linq;
+using osu.Framework.Allocation;
+using osu.Game.Rulesets.Karaoke.Edit.Components.Containers;
+using osu.Game.Rulesets.Karaoke.Edit.Lyrics.States;
+using osu.Game.Rulesets.Karaoke.Objects;
+using osu.Game.Screens.Edit;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends.Components
+{
+    public abstract class AutoGenerateSection : Section
+    {
+        protected sealed override string Title => "Auto generate";
+
+        [BackgroundDependencyLoader]
+        private void load(EditorBeatmap beatmap, LyricSelectionState lyricSelectionState)
+        {
+            Children = new[]
+            {
+                new AutoGenerateButton
+                {
+                    StartSelecting = () =>
+                        GetDisableSelectingLyrics(beatmap.HitObjects.OfType<Lyric>().ToArray())
+                },
+            };
+
+            lyricSelectionState.Action = e =>
+            {
+                if (e != LyricEditorSelectingAction.Apply)
+                    return;
+
+                var selectedLyrics = lyricSelectionState.SelectedLyrics.ToArray();
+                Apply(selectedLyrics);
+            };
+        }
+
+        protected abstract Dictionary<Lyric, string> GetDisableSelectingLyrics(Lyric[] lyrics);
+
+        protected abstract void Apply(Lyric[] lyrics);
+
+        private class AutoGenerateButton : SelectLyricButton
+        {
+            protected override string StandardText => "Generate";
+
+            protected override string SelectingText => "Cancel generate";
+        }
+    }
+}
--- osu.Game.Rulesets.Karaoke/Edit/Lyrics/Extends/Languages/LanguageAutoGenerateSection.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Extends/Languages/LanguageAutoGenerateSection.cs
@@ -1,41 +1,24 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using System.Linq;
 using osu.Framework.Allocation;
-using osu.Game.Rulesets.Karaoke.Edit.Components.Containers;
 using osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends.Components;
-using osu.Game.Rulesets.Karaoke.Edit.Lyrics.States;
 using osu.Game.Rulesets.Karaoke.Objects;
-using osu.Game.Screens.Edit;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends.Languages
 {
-    public class LanguageAutoGenerateSection : Section
+    public class LanguageAutoGenerateSection : AutoGenerateSection
     {
-        protected override string Title => "Auto generate";
+        [Resolved]
+        private LyricManager lyricManager { get; set; }
 
-        [BackgroundDependencyLoader]
-        private void load(EditorBeatmap beatmap, LyricManager lyricManager, LyricSelectionState lyricSelectionState)
-        {
-            Children = new[]
-            {
-                new AutoGenerateButton
-                {
-                    StartSelecting = () =>
-                        beatmap.HitObjects.OfType<Lyric>().Where(x => string.IsNullOrEmpty(x.Text))
-                               .ToDictionary(k => k, i => "Should have text in lyric.")
-                },
-            };
+        protected override Dictionary<Lyric, string> GetDisableSelectingLyrics(Lyric[] lyrics)
+            => lyrics.Where(x => string.IsNullOrEmpty(x.Text))
+                     .ToDictionary(k => k, i => "Should have text in lyric.");
 
-            lyricSelectionState.Action = e =>
-            {
-                if (e != LyricEditorSelectingAction.Apply)
-                    return;
-
-                var selectedLyric = lyricSelectionState.SelectedLyrics.ToList();
-                lyricManager.AutoDetectLyricLanguage(selectedLyric);
-            };
-        }
+        protected override void Apply(Lyric[] lyrics)
+            => lyricManager.AutoDetectLyricLanguage(lyrics);
     }
 }
