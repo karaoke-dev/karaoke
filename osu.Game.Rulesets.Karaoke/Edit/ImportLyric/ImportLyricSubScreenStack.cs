c71ccdebf871ae2b85bf0b67dd44b7a194484772
andy840119 <andy840119@gmail.com>
2021-07-05 23:19:42 +0900

    implement pop feature.

--- osu.Game.Rulesets.Karaoke/Edit/ImportLyric/ImportLyricSubScreenStack.cs
+++ osu.Game.Rulesets.Karaoke/Edit/ImportLyric/ImportLyricSubScreenStack.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
+using System.Linq;
+using osu.Framework.Screens;
 using osu.Game.Rulesets.Karaoke.Edit.ImportLyric.AssignLanguage;
 using osu.Game.Rulesets.Karaoke.Edit.ImportLyric.DragFile;
 using osu.Game.Rulesets.Karaoke.Edit.ImportLyric.EditLyric;
@@ -13,6 +16,8 @@ namespace osu.Game.Rulesets.Karaoke.Edit.ImportLyric
 {
     public class ImportLyricSubScreenStack : OsuScreenStack
     {
+        private readonly Stack<IImportLyricSubScreen> stack = new Stack<IImportLyricSubScreen>();
+
         public void Push(ImportLyricStep step)
         {
             if (CurrentScreen is IImportLyricSubScreen lyricSubScreen)
@@ -58,9 +63,25 @@ namespace osu.Game.Rulesets.Karaoke.Edit.ImportLyric
             }
         }
 
+        public void Push(IImportLyricSubScreen screen)
+        {
+            stack.Push(screen);
+            Push(screen as IScreen);
+        }
+
         public void Pop(ImportLyricStep step)
         {
-            // todo : implement
+            var screen = stack.FirstOrDefault(x => x.Step == step);
+            if (screen == null)
+                throw new ScreenNotCurrentException("Screen is not in the lyric import step.");
+
+            screen.MakeCurrent();
+
+            // pop to target stack.
+            while (stack.Peek().Step != step)
+            {
+                stack.Pop();
+            }
         }
     }
 }
