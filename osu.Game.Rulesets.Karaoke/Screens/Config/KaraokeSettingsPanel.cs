6d9d900e42684aadbe027d0c3d20170c003a2881
andy840119 <andy840119@gmail.com>
2021-07-18 11:44:54 +0900

    should clear selected sub-screen if change search text.

--- osu.Game.Rulesets.Karaoke/Screens/Config/KaraokeSettingsPanel.cs
+++ osu.Game.Rulesets.Karaoke/Screens/Config/KaraokeSettingsPanel.cs
@@ -8,6 +8,7 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Game.Graphics.Containers;
+using osu.Game.Graphics.UserInterface;
 using osu.Game.Overlays.Settings;
 using osu.Game.Rulesets.Karaoke.Screens.Config.Sections;
 
@@ -32,13 +33,22 @@ namespace osu.Game.Rulesets.Karaoke.Screens.Config
         };
 
         [BackgroundDependencyLoader]
-        private void load(ConfigColourProvider colourProvider, Bindable<SettingsSection> selectedSection)
+        private void load(ConfigColourProvider colourProvider, Bindable<SettingsSection> selectedSection, Bindable<SettingsSubsection> selectedSubsection)
         {
             selectedSection.BindValueChanged(x =>
             {
                 var colour = colourProvider.GetBackground3Colour(x.NewValue);
                 Background.Delay(200).Then().FadeColour(colour, 500);
             });
+
+            if (SectionsContainer.FixedHeader is SeekLimitedSearchTextBox searchTextBox)
+            {
+                searchTextBox.Current.ValueChanged += (term) =>
+                {
+                    // should clear selected sub-section if change search text.
+                    selectedSubsection.Value = null;
+                };
+            }
         }
 
         public class KaraokeSettingsSectionsContainer : SettingsSectionsContainer
@@ -57,17 +67,24 @@ namespace osu.Game.Rulesets.Karaoke.Screens.Config
                 {
                     RelativeSizeAxes = Axes.X,
                     Depth = 1,
-                    Alpha = 0.6f
                 });
 
+                // change background color if section changed.
                 selectedSection.BindValueChanged(x =>
                 {
                     var colour = colourProvider.GetBackgroundColour(x.NewValue);
                     background.Delay(200).Then().FadeColour(colour, 500);
                 });
 
+                // move background to target sub-section if user hover to it.
                 selectedSubsection.BindValueChanged(x =>
                 {
+                    var alpha = x.NewValue != null ? 0.6f : 0f;
+                    background.FadeTo(alpha, 200);
+
+                    if (x.NewValue == null)
+                        return;
+
                     const int offset = 20;
                     var position = scrollContainer.GetChildPosInContent(x.NewValue);
                     background.MoveToY(position + offset, 50);
