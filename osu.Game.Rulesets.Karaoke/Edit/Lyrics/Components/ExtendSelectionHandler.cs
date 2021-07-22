65acb03e51160801ea07215c06ddbf82c6ee3d3e
andy840119 <andy840119@gmail.com>
2021-07-23 00:45:53 +0900

    it will be better to use internal method.

--- osu.Game.Rulesets.Karaoke/Edit/Lyrics/Components/ExtendSelectionHandler.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Components/ExtendSelectionHandler.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Linq;
-using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Game.Screens.Edit.Compose.Components;
 
@@ -10,16 +9,11 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Components
 {
     public abstract class ExtendSelectionHandler<T> : SelectionHandler<T>
     {
-        [BackgroundDependencyLoader]
-        private void load()
+        protected override void OnSelectionChanged()
         {
-            // because it will still show selection box even all selected items is not in current handler.
-            // so should hide the selection box by hand.
-            // todo : this is the temp way, might remove after official fix that.
-            SelectedItems.CollectionChanged += (sender, args) =>
-            {
-                Scheduler.AddOnce(updateVisibility);
-            };
+            base.OnSelectionChanged();
+
+            updateVisibility();
         }
 
         /// <summary>
