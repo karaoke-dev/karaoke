29cbe17819624bcd8d0bb288aea568182e6a4bf8
andy840119 <andy840119@gmail.com>
2021-08-29 14:39:45 +0900

    remove unused namespace.

--- osu.Game.Rulesets.Karaoke/Edit/ImportLyric/EditLyric/EditLyricSubScreen.cs
+++ osu.Game.Rulesets.Karaoke/Edit/ImportLyric/EditLyric/EditLyricSubScreen.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using System.ComponentModel;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Sprites;
--- osu.Game.Rulesets.Karaoke/Skinning/KaraokeSkinLookup.cs
+++ osu.Game.Rulesets.Karaoke/Skinning/KaraokeSkinLookup.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.ComponentModel;
-using System.IO;
 using osu.Game.Rulesets.Karaoke.Utils;
 
 namespace osu.Game.Rulesets.Karaoke.Skinning
