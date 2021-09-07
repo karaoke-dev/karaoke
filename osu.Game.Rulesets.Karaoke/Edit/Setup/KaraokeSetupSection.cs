0986894507885f223a3af103a66feb8282d8b04f
andy840119 <andy840119@gmail.com>
2021-09-07 23:36:23 +0900

    Implement first field for demo.

--- osu.Game.Rulesets.Karaoke/Edit/Setup/KaraokeSetupSection.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Setup/KaraokeSetupSection.cs
@@ -1,15 +1,50 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Game.Graphics.UserInterfaceV2;
+using osu.Game.Rulesets.Karaoke.Beatmaps;
 using osu.Game.Screens.Edit.Setup;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.Setup
 {
     public class KaraokeSetupSection : RulesetSetupSection
     {
+        private KaraokeBeatmap karaokeBeatmap => Beatmap.PlayableBeatmap as KaraokeBeatmap;
+
+        private LabelledSwitchButton saitenable;
+
         public KaraokeSetupSection()
             : base(new KaraokeRuleset().RulesetInfo)
         {
+        }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            Children = new Drawable[]
+            {
+                saitenable = new LabelledSwitchButton
+                {
+                    Label = "Saitenable",
+                    Description = "Will not show saiten playfield in uncheck this.",
+                    Current = { Value = true }
+                }
+            };
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            saitenable.Current.BindValueChanged(_ => updateBeatmap());
+        }
+
+        private void updateBeatmap()
+        {
+            // todo: update the value.
+            // karaokeBeatmap.Saitenable = saitenable.Current.Value;
         }
     }
 }
