53947e66836460e4f5e41ea2d05edf1fb3759a6c
andy840119 <andy840119@gmail.com>
2021-09-07 23:05:46 +0900

    Implement base set-up section.

---
+++ osu.Game.Rulesets.Karaoke/Edit/Setup/KaraokeSetupSection.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Screens.Edit.Setup;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Setup
+{
+    public class KaraokeSetupSection : RulesetSetupSection
+    {
+        public KaraokeSetupSection()
+            : base(new KaraokeRuleset().RulesetInfo)
+        {
+        }
+    }
+}
