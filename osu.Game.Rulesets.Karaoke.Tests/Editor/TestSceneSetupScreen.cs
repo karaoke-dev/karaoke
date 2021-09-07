6512e03d3a493eae660776611e95187e4ab00e8f
andy840119 <andy840119@gmail.com>
2021-09-07 23:11:34 +0900

    Add test case for set-up screen.

---
+++ osu.Game.Rulesets.Karaoke.Tests/Editor/TestSceneSetupScreen.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Rulesets.Edit;
+using osu.Game.Rulesets.Karaoke.Beatmaps;
+using osu.Game.Screens.Edit;
+using osu.Game.Screens.Edit.Setup;
+using osu.Game.Tests.Visual;
+
+namespace osu.Game.Rulesets.Karaoke.Tests.Editor
+{
+    [TestFixture]
+    public class TestSceneSetupScreen : EditorClockTestScene
+    {
+        [Cached(typeof(EditorBeatmap))]
+        [Cached(typeof(IBeatSnapProvider))]
+        private readonly EditorBeatmap editorBeatmap;
+
+        public TestSceneSetupScreen()
+        {
+            editorBeatmap = new EditorBeatmap(new KaraokeBeatmap());
+        }
+
+        [Test]
+        public void TestKaraoke() => runForRuleset(new KaraokeRuleset().RulesetInfo);
+
+        private void runForRuleset(RulesetInfo rulesetInfo)
+        {
+            AddStep("create screen", () =>
+            {
+                editorBeatmap.BeatmapInfo.Ruleset = rulesetInfo;
+
+                Beatmap.Value = CreateWorkingBeatmap(editorBeatmap.PlayableBeatmap);
+
+                Child = new SetupScreen
+                {
+                    State = { Value = Visibility.Visible },
+                };
+            });
+        }
+    }
+}
