84286e8776e5a31d18f6e58637eef9dc30c6479b
andy840119 <andy840119@gmail.com>
2021-09-07 22:48:19 +0900

    Not use EditorScreen now because it already have ColourProvider and set default color as blue.

--- osu.Game.Rulesets.Karaoke/Edit/Components/EditorSubScreen.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Components/EditorSubScreen.cs
@@ -1,18 +1,28 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Cursor;
 using osu.Game.Screens.Edit;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.Components
 {
-    public abstract class EditorSubScreen : EditorScreen
+    /// <summary>
+    /// todo : will inherit <see cref="EditorScreen"/> eventually
+    /// </summary>
+    public abstract class EditorSubScreen : Container
     {
+        protected override Container<Drawable> Content => content;
+        private readonly Container content;
+
         protected EditorSubScreen()
-            : base(EditorScreenMode.Compose)
         {
-            // todo : this is a temp fix and might be broken in next release.
-            // See : https://github.com/ppy/osu/pull/14554/files
-            Show();
+            Anchor = Anchor.Centre;
+            Origin = Anchor.Centre;
+            RelativeSizeAxes = Axes.Both;
+
+            InternalChild = content = new PopoverContainer { RelativeSizeAxes = Axes.Both };
         }
     }
 }
