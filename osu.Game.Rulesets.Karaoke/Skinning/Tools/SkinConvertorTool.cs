ab24c4fa15274c81ac4dd98175b86e4848377f6a
andy840119 <andy840119@gmail.com>
2021-09-30 02:11:32 +0900

    use temp way to apply conversion skin to list of shaders. also adjust colour because got no idea why default colour is black and white.

---
+++ osu.Game.Rulesets.Karaoke/Skinning/Tools/SkinConvertorTool.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics.Shaders;
+using osu.Game.Rulesets.Karaoke.Extensions;
+using osu.Game.Rulesets.Karaoke.Graphics.Shaders;
+using osu.Game.Rulesets.Karaoke.Skinning.Metadatas.Fonts;
+using osuTK.Graphics;
+
+namespace osu.Game.Rulesets.Karaoke.Skinning.Tools
+{
+    // it's the temp logic to collect logic.
+    public static class SkinConvertorTool
+    {
+        public static IShader[] ConvertLeftSideShader(LocalShaderManager shaderManager, LyricFont lyricFont)
+        {
+            if (shaderManager == null)
+                return null;
+
+            var edgeSize = (int)lyricFont.LyricTextFontInfo.EdgeSize;
+            var frontColor = Color4.Blue; // todo: use real colour.
+            return new IShader[]
+            {
+                shaderManager.LocalCustomizedShader<OutlineShader>()
+                             .With(x =>
+                             {
+                                 x.Radius = edgeSize;
+                                 x.OutlineColour = frontColor;
+                             })
+            };
+        }
+
+        public static IShader[] ConvertRightSideShader(LocalShaderManager shaderManager, LyricFont lyricFont)
+        {
+            if (shaderManager == null)
+                return null;
+
+            var edgeSize = (int)lyricFont.LyricTextFontInfo.EdgeSize;
+            var backColor = Color4.DarkGreen; // todo: use real colour.
+            return new IShader[]
+            {
+                shaderManager.LocalCustomizedShader<OutlineShader>()
+                             .With(x =>
+                             {
+                                 x.Radius = edgeSize;
+                                 x.OutlineColour = backColor;
+                             })
+            };
+        }
+    }
+}
