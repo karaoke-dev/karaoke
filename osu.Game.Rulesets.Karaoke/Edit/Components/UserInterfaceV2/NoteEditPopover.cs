a015cf569554a65724c165e25accae92ea1d20f0
andy840119 <andy840119@gmail.com>
2021-10-31 20:59:24 +0800

    Implement base note pop-over

---
+++ osu.Game.Rulesets.Karaoke/Edit/Components/UserInterfaceV2/NoteEditPopover.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Graphics.UserInterfaceV2;
+using osu.Game.Rulesets.Karaoke.Objects;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Components.UserInterfaceV2
+{
+    public class NoteEditPopover : OsuPopover
+    {
+        public NoteEditPopover(Note note)
+        {
+            Children = new Drawable[]
+            {
+                new FillFlowContainer
+                {
+                    Width = 200,
+                    Direction = FillDirection.Vertical,
+                    AutoSizeAxes = Axes.Y,
+                    Children = new Drawable[]
+                    {
+                        new LabelledTextBox
+                        {
+                            Label = "Display text",
+                            Current = note.AlternativeTextBindable
+                        },
+                        new LabelledSwitchButton
+                        {
+                            Label = "Display",
+                            Current = note.DisplayBindable,
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
