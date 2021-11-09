dcb9f0e70b978053cdfb998864184bd177b441ef
為什麼 <andy840119@gmail.com>
2021-11-09 23:36:04 +0800

    create default ctor.

--- osu.Game.Rulesets.Karaoke/Skinning/DefaultKaraokeSkin.cs
+++ osu.Game.Rulesets.Karaoke/Skinning/DefaultKaraokeSkin.cs
@@ -1,10 +1,17 @@
 // Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.IO;
+using osu.Game.IO;
+using osu.Game.Skinning;
+
 namespace osu.Game.Rulesets.Karaoke.Skinning
 {
     public class DefaultKaraokeSkin : KaraokeSkin
     {
-
+        public DefaultKaraokeSkin(SkinInfo skin, IStorageResourceProvider resources, Stream configurationStream = null)
+            : base(skin, resources, configurationStream)
+        {
+        }
     }
 }
