72f5555fd0bee2eef2407f26b8f43046136a2f63
為什麼 <andy840119@gmail.com>
2021-11-15 22:40:14 +0800

    object assert should consider the case that should only convert properties that has setter.

--- osu.Game.Rulesets.Karaoke.Tests/Asserts/ObjectAssert.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Asserts/ObjectAssert.cs
@@ -1,8 +1,13 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using NUnit.Framework;
+using osu.Game.IO.Serialization;
 
 namespace osu.Game.Rulesets.Karaoke.Tests.Asserts
 {
@@ -10,7 +15,23 @@ namespace osu.Game.Rulesets.Karaoke.Tests.Asserts
     {
         public static void ArePropertyEqual<T>(T expect, T actual) where T : class
         {
-            AreEqual(JsonConvert.SerializeObject(expect), JsonConvert.SerializeObject(actual));
+            var settings = JsonSerializableExtensions.CreateGlobalSettings();
+            settings.ContractResolver = new WritablePropertiesOnlyResolver();
+
+            var expectJsonString = JsonConvert.SerializeObject(expect, settings);
+            var actualJsonString = JsonConvert.SerializeObject(actual, settings);
+
+            AreEqual(expectJsonString, actualJsonString);
+        }
+
+        private class WritablePropertiesOnlyResolver : DefaultContractResolver
+        {
+            // we only wants to save properties that only writable.
+            protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+            {
+                IList<JsonProperty> props = base.CreateProperties(type, memberSerialization);
+                return props.Where(p => p.Writable).ToList();
+            }
         }
     }
 }
