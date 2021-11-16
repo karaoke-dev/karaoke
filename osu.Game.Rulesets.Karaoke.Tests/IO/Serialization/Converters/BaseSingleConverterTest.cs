dd0382c8d0fb4984ffe10d2368e3eec03c3d8ee2
andy840119 <andy840119@gmail.com>
2021-11-16 20:16:37 +0800

    Fix the case in convertor test.

--- osu.Game.Rulesets.Karaoke.Tests/IO/Serialization/Converters/BaseSingleConverterTest.cs
+++ osu.Game.Rulesets.Karaoke.Tests/IO/Serialization/Converters/BaseSingleConverterTest.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Linq;
+using System;
 using Newtonsoft.Json;
 using osu.Game.IO.Serialization;
 
@@ -13,13 +13,18 @@ namespace osu.Game.Rulesets.Karaoke.Tests.IO.Serialization.Converters
         {
             var globalSetting = JsonSerializableExtensions.CreateGlobalSettings();
             globalSetting.Formatting = Formatting.None; // do not change new line in testing.
-            globalSetting.Converters = globalSetting.Converters.Concat(CreateDefaultConverts()).ToArray();
+            globalSetting.Converters.Add(new TConverter());
+
+            var extraConvertors = CreateExtraConverts() ?? Array.Empty<JsonConverter>();
+
+            foreach (var extraConvertor in extraConvertors)
+            {
+                globalSetting.Converters.Add(extraConvertor);
+            }
+
             return globalSetting;
         }
 
-        protected virtual JsonConverter[] CreateDefaultConverts() => new JsonConverter[]
-        {
-            new TConverter(),
-        };
+        protected virtual JsonConverter[] CreateExtraConverts() => null;
     }
 }
