f99ea9252bf13c9011bb4f72f7ce1ccc24c67202
andy840119 <andy840119@gmail.com>
2021-11-17 20:10:09 +0800

    it's time to move the property.

--- osu.Game.Rulesets.Karaoke/Skinning/Metadatas/Layouts/LyricLayout.cs
+++ osu.Game.Rulesets.Karaoke/Skinning/Metadatas/Layouts/LyricLayout.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Sprites;
 
 namespace osu.Game.Rulesets.Karaoke.Skinning.Metadatas.Layouts
 {
@@ -42,53 +41,5 @@ namespace osu.Game.Rulesets.Karaoke.Skinning.Metadatas.Layouts
         /// ???
         /// </summary>
         public bool Continuous { get; set; }
-
-        /// <summary>
-        /// ???
-        /// todo: should be moved into config.
-        /// </summary>
-        public KaraokeTextSmartHorizon SmartHorizon { get; set; } = KaraokeTextSmartHorizon.None;
-
-        /// <summary>
-        /// Interval between lyric texts
-        /// todo: should be moved into config.
-        /// </summary>
-        public int LyricsInterval { get; set; }
-
-        /// <summary>
-        /// Interval between lyric rubies
-        /// todo: should be moved into config.
-        /// </summary>
-        public int RubyInterval { get; set; }
-
-        /// <summary>
-        /// Interval between lyric romaji
-        /// todo: should be moved into config.
-        /// </summary>
-        public int RomajiInterval { get; set; }
-
-        /// <summary>
-        /// Ruby position alignment
-        /// todo: should be moved into config.
-        /// </summary>
-        public LyricTextAlignment RubyAlignment { get; set; } = LyricTextAlignment.Auto;
-
-        /// <summary>
-        /// Ruby position alignment
-        /// todo: should be moved into config.
-        /// </summary>
-        public LyricTextAlignment RomajiAlignment { get; set; } = LyricTextAlignment.Auto;
-
-        /// <summary>
-        /// Interval between lyric text and ruby
-        /// todo: should be moved into config.
-        /// </summary>
-        public int RubyMargin { get; set; }
-
-        /// <summary>
-        /// (Additional) Interval between lyric text and romaji
-        /// todo: should be moved into config.
-        /// </summary>
-        public int RomajiMargin { get; set; }
     }
 }
