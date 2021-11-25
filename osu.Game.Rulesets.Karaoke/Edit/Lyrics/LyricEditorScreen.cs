58ec43fcdf874b234be8196df9cd8bd6027f9e30
為什麼 <andy840119@gmail.com>
2021-11-26 00:42:45 +0800

    Implement lyric editor screen for able to user lyric editor in karaoke editor.

---
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/LyricEditorScreen.cs
@@ -0,0 +1,40 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Game.Rulesets.Karaoke.Configuration;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics
+{
+    public class LyricEditorScreen : KaraokeEditorScreen
+    {
+        private readonly Bindable<LyricEditorMode> bindableLyricEditorMode = new();
+
+        public LyricEditorScreen()
+            : base(KaraokeEditorScreenMode.Lyric)
+        {
+            LyricEditor lyricEditor;
+            AddInternal(new KaraokeEditInputManager(new KaraokeRuleset().RulesetInfo)
+            {
+                RelativeSizeAxes = Axes.Both,
+                Padding = new MarginPadding(10),
+                Child = lyricEditor = new LyricEditor
+                {
+                    RelativeSizeAxes = Axes.Both,
+                }
+            });
+            bindableLyricEditorMode.BindValueChanged(e =>
+            {
+                lyricEditor.Mode = e.NewValue;
+            });
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(KaraokeRulesetLyricEditorConfigManager lyricEditorConfigManager)
+        {
+            lyricEditorConfigManager.BindWith(KaraokeRulesetLyricEditorSetting.LyricEditorMode, bindableLyricEditorMode);
+        }
+    }
+}
