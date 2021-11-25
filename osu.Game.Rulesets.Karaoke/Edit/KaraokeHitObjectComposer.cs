cb8f0e11cdb7891b26ef922a960ddd6c9ffd9daa
為什麼 <andy840119@gmail.com>
2021-11-26 01:34:43 +0800

    Remove the lyric editor from editor.

--- osu.Game.Rulesets.Karaoke/Edit/KaraokeHitObjectComposer.cs
+++ osu.Game.Rulesets.Karaoke/Edit/KaraokeHitObjectComposer.cs
@@ -5,29 +5,24 @@ using System;
 using System.Collections.Generic;
 using System.Reflection;
 using osu.Framework.Allocation;
-using osu.Framework.Bindables;
-using osu.Framework.Graphics;
 using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Screens;
 using osu.Game.Beatmaps;
 using osu.Game.Graphics.UserInterface;
 using osu.Game.Rulesets.Edit;
 using osu.Game.Rulesets.Edit.Tools;
 using osu.Game.Rulesets.Karaoke.Configuration;
-using osu.Game.Rulesets.Karaoke.Edit.Checker;
 using osu.Game.Rulesets.Karaoke.Edit.Components.Menu;
-using osu.Game.Rulesets.Karaoke.Edit.Export;
 using osu.Game.Rulesets.Karaoke.Edit.Lyrics;
 using osu.Game.Rulesets.Karaoke.Edit.Notes;
 using osu.Game.Rulesets.Karaoke.Edit.RubyRomaji;
 using osu.Game.Rulesets.Karaoke.Edit.Singers;
-using osu.Game.Rulesets.Karaoke.Graphics.UserInterface;
 using osu.Game.Rulesets.Karaoke.Objects;
 using osu.Game.Rulesets.Karaoke.Skinning.Fonts;
 using osu.Game.Rulesets.Karaoke.UI;
 using osu.Game.Rulesets.Karaoke.UI.Position;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.UI;
-using osu.Game.Rulesets.UI.Scrolling;
 using osu.Game.Screens.Edit;
 using osu.Game.Screens.Edit.Components.Menus;
 using osu.Game.Screens.Edit.Components.TernaryButtons;
@@ -38,30 +33,16 @@ namespace osu.Game.Rulesets.Karaoke.Edit
 {
     public class KaraokeHitObjectComposer : HitObjectComposer<KaraokeHitObject>
     {
-        private readonly Bindable<EditMode> bindableEditMode = new();
-
         private DrawableKaraokeEditorRuleset drawableRuleset;
+
+        [Cached]
+        private readonly KaraokeRulesetEditConfigManager editConfigManager;
 
         [Cached(Type = typeof(INotePositionInfo))]
         private readonly NotePositionInfo notePositionInfo;
 
         [Cached]
         private readonly FontManager fontManager;
-
-        [Cached]
-        private readonly KaraokeRulesetEditConfigManager editConfigManager;
-
-        [Cached]
-        private readonly KaraokeRulesetLyricEditorConfigManager lyricEditorConfigManager;
-
-        [Cached]
-        private readonly KaraokeRulesetEditGeneratorConfigManager generatorConfigManager;
-
-        [Cached]
-        private readonly KaraokeRulesetEditCheckerConfigManager checkerConfigManager;
-
-        [Cached]
-        private readonly ExportLyricManager exportLyricManager;
 
         [Cached]
         private readonly NoteManager noteManager;
@@ -73,13 +54,7 @@ namespace osu.Game.Rulesets.Karaoke.Edit
         private readonly RubyRomajiManager rubyRomajiManager;
 
         [Cached]
-        private readonly LyricCheckerManager lyricCheckerManager;
-
-        [Cached]
         private readonly SingerManager singerManager;
-
-        [Cached]
-        private LanguageSelectionDialog languageSelectionDialog;
 
         [Resolved]
         private Editor editor { get; set; }
@@ -88,48 +63,21 @@ namespace osu.Game.Rulesets.Karaoke.Edit
             : base(ruleset)
         {
             editConfigManager = new KaraokeRulesetEditConfigManager();
-            lyricEditorConfigManager = new KaraokeRulesetLyricEditorConfigManager();
-            generatorConfigManager = new KaraokeRulesetEditGeneratorConfigManager();
-            checkerConfigManager = new KaraokeRulesetEditCheckerConfigManager();
 
             // Duplicated registration because selection handler need to use it.
             AddInternal(notePositionInfo = new NotePositionInfo());
             AddInternal(fontManager = new FontManager());
 
-            AddInternal(exportLyricManager = new ExportLyricManager());
             AddInternal(noteManager = new NoteManager());
             AddInternal(lyricManager = new LyricManager());
             AddInternal(rubyRomajiManager = new RubyRomajiManager());
-            AddInternal(lyricCheckerManager = new LyricCheckerManager());
             AddInternal(singerManager = new SingerManager());
-            LayerBelowRuleset.Add(languageSelectionDialog = new LanguageSelectionDialog());
         }
 
         [BackgroundDependencyLoader]
         private void load()
         {
             CreateMenuBar();
-            AddInternal(new KaraokeLyricEditor(Ruleset)
-            {
-                RelativeSizeAxes = Axes.Both
-            });
-
-            bindableEditMode.BindValueChanged(e =>
-            {
-                if (e.NewValue == EditMode.LyricEditor)
-                {
-                    InternalChildren[0].Hide(); // hide content
-                    InternalChildren[1].Hide(); // hide left-side toggle.
-                    InternalChildren[2].Show(); // show lyric editor.
-                }
-                else
-                {
-                    InternalChildren[0].Show();
-                    InternalChildren[1].Show();
-                    InternalChildren[2].Hide();
-                }
-            }, true);
-            editConfigManager.BindWith(KaraokeRulesetEditSetting.EditMode, bindableEditMode);
         }
 
         private DependencyContainer dependencies;
@@ -138,8 +86,6 @@ namespace osu.Game.Rulesets.Karaoke.Edit
             => dependencies = new DependencyContainer(base.CreateChildDependencies(parent));
 
         public new KaraokePlayfield Playfield => drawableRuleset.Playfield;
-
-        public IScrollingInfo ScrollingInfo => drawableRuleset.ScrollingInfo;
 
         protected override Playfield PlayfieldAtScreenSpacePosition(Vector2 screenSpacePosition)
         {
@@ -186,50 +132,20 @@ namespace osu.Game.Rulesets.Karaoke.Edit
 
             Schedule(() =>
             {
-                menuBar.Items = new[]
+                menuBar.Items = new List<MenuItem>(menuBar.Items)
                 {
-                    new MenuItem("File")
-                    {
-                        Items = new MenuItem[]
-                        {
-                            new ImportLyricMenu(editor, "Import from text"),
-                            new ImportLyricMenu(editor, "Import from .lrc file"),
-                            new EditorMenuItemSpacer(),
-                            new EditorMenuItem("Export to .lrc", MenuItemType.Standard, () => exportLyricManager.ExportToLrc()),
-                            new EditorMenuItem("Export to text", MenuItemType.Standard, () => exportLyricManager.ExportToText()),
-                            new EditorMenuItem("Export to json", MenuItemType.Destructive, () => exportLyricManager.ExportToJson()),
-                        }
-                    },
-                    new MenuItem("View")
-                    {
-                        Items = new MenuItem[]
-                        {
-                            new EditModeMenu(editConfigManager, "Edit mode"),
-                            new EditorMenuItemSpacer(),
-                            new LyricEditorModeMenu(lyricEditorConfigManager, "Lyric editor mode"),
-                            new LyricEditorTextSizeMenu(lyricEditorConfigManager, "Text size"),
-                            new AutoFocusToEditLyricMenu(lyricEditorConfigManager, "Auto focus to edit lyric"),
-                            new EditorMenuItemSpacer(),
-                            new NoteEditorPreviewMenu(editConfigManager, "Note editor"),
-                        }
-                    },
-                    new MenuItem("Tools")
-                    {
-                        Items = new MenuItem[]
-                        {
-                            new ManagerMenu(editor, "Manage"),
-                            new GeneratorMenu("Generator"),
-                        }
-                    },
-                    new MenuItem("Config")
+                    new("Config")
                     {
                         Items = new MenuItem[]
                         {
                             new EditorMenuItem("Lyric editor"),
                             new GeneratorConfigMenu("Generator"),
-                            new LockStateMenu(lyricEditorConfigManager, "Lock"),
+                            new EditorMenuItem("open Karaoke editor", MenuItemType.Standard, () =>
+                            {
+                                editor.Push(new KaraokeEditor());
+                            }),
                         }
-                    }
+                    },
                 };
             });
         }
