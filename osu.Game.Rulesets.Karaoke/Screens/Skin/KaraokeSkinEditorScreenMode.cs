a5b6f57782fde11f9435c233daf38c9d7c9ad0b2
為什麼 <andy840119@gmail.com>
2021-11-26 23:10:04 +0800

    implement karaoke skin and mode.

---
+++ osu.Game.Rulesets.Karaoke/Screens/Skin/KaraokeSkinEditorScreenMode.cs
@@ -0,0 +1,19 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.ComponentModel;
+
+namespace osu.Game.Rulesets.Karaoke.Screens.Skin
+{
+    public enum KaraokeSkinEditorScreenMode
+    {
+        [Description("Config")]
+        Config,
+
+        [Description("Style")]
+        Style,
+
+        [Description("Layout")]
+        Layout,
+    }
+}
