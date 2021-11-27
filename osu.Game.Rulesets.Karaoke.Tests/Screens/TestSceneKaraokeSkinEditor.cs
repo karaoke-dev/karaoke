154e8b767ba09a6dd8b53169e06d27e6234e1c44
為什麼 <andy840119@gmail.com>
2021-11-27 12:28:32 +0800

    implement karaoke skin editor.

---
+++ osu.Game.Rulesets.Karaoke.Tests/Screens/TestSceneKaraokeSkinEditor.cs
@@ -0,0 +1,27 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Game.Rulesets.Karaoke.Beatmaps;
+using osu.Game.Rulesets.Karaoke.Screens.Skin;
+using osu.Game.Rulesets.Karaoke.Tests.Editor;
+using osu.Game.Screens.Edit;
+
+namespace osu.Game.Rulesets.Karaoke.Tests.Screens
+{
+    public class TestSceneKaraokeSkinEditor : EditorSubScreenTestScene<KaraokeSkinEditor>
+    {
+        // todo: karaoke skin editor might not need editor beatmap, or at least it will be optional.
+        [Cached(typeof(EditorBeatmap))]
+        private readonly EditorBeatmap editorBeatmap = new(new KaraokeBeatmap());
+
+        protected override KaraokeSkinEditor CreateEditor() => new();
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            // todo: karaoke skin editor might not need editor click eventually?
+            Dependencies.Cache(new EditorClock());
+        }
+    }
+}
