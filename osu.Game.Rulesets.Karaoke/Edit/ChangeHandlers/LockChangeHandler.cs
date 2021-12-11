2deb6e38964d874a419fcb114a4c990c2c5320ce
andy840119 <andy840119@gmail.com>
2021-12-11 12:33:17 +0900

    Implement lock change handler for able to lock any hitobjects that able to be locked.

---
+++ osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/LockChangeHandler.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Karaoke.Objects;
+using osu.Game.Rulesets.Karaoke.Objects.Types;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers
+{
+    public class LockChangeHandler : HitObjectChangeHandler<KaraokeHitObject>, ILockChangeHandler
+    {
+        public void Lock(LockState lockState)
+        {
+            PerformOnSelection(h =>
+            {
+                if (h is IHasLock hasLock)
+                    hasLock.Lock = lockState;
+            });
+        }
+
+        public void Unlock()
+        {
+            Lock(LockState.None);
+        }
+    }
+}
