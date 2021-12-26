4a60e1cae564167f28d8b5e7949eee41b37f975e
andy840119 <andy840119@gmail.com>
2021-12-26 11:39:23 +0800

    create those pop-overs.

---
+++ osu.Game.Rulesets.Karaoke/Edit/Configs/Generator/GeneratorConfigPopover.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Graphics.Containers;
+using osu.Game.Graphics.UserInterfaceV2;
+using osu.Game.Rulesets.Karaoke.Configuration;
+using osu.Game.Rulesets.Karaoke.Edit.Generator.Types;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Configs.Generator
+{
+    public abstract class GeneratorConfigPopover<TConfig> : OsuPopover where TConfig : IHasConfig<TConfig>, new()
+    {
+        private readonly Bindable<TConfig> bindableConfig = new();
+
+        protected GeneratorConfigPopover()
+        {
+            Child = new OsuScrollContainer
+            {
+                Height = 500,
+                Width = 300,
+                Child = new FillFlowContainer<GeneratorConfigSection>
+                {
+                    Direction = FillDirection.Vertical,
+                    RelativeSizeAxes = Axes.X,
+                    AutoSizeAxes = Axes.Y,
+                    Children = CreateConfigSection(bindableConfig) ?? Array.Empty<GeneratorConfigSection>()
+                }
+            };
+        }
+
+        protected abstract KaraokeRulesetEditGeneratorSetting Config { get; }
+
+        protected abstract GeneratorConfigSection[] CreateConfigSection(Bindable<TConfig> current);
+
+        [BackgroundDependencyLoader]
+        private void load(KaraokeRulesetEditGeneratorConfigManager config)
+        {
+            bindableConfig.BindTo(config.GetBindable<TConfig>(Config));
+        }
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/Edit/Configs/Generator/Notes/NoteGeneratorConfigPopover.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Bindables;
+using osu.Game.Rulesets.Karaoke.Configuration;
+using osu.Game.Rulesets.Karaoke.Edit.Generator.Notes;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Configs.Generator.Notes
+{
+    public class NoteGeneratorConfigPopover : GeneratorConfigPopover<NoteGeneratorConfig>
+    {
+        protected override KaraokeRulesetEditGeneratorSetting Config => KaraokeRulesetEditGeneratorSetting.NoteGeneratorConfig;
+
+        protected override GeneratorConfigSection[] CreateConfigSection(Bindable<NoteGeneratorConfig> current)
+        {
+            return null;
+        }
+    }
+}
