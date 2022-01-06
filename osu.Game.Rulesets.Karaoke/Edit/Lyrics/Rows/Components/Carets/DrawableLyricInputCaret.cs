c093b89d5a1448e12843ee05456c9c1c21c01927
為什麼 <andy840119@gmail.com>
2022-01-06 20:48:24 +0800

    should be able to have pending text in text caret. also implement: - insert text into lyric. - delete the text from lyric.

--- osu.Game.Rulesets.Karaoke/Edit/Lyrics/Rows/Components/Carets/DrawableLyricInputCaret.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Rows/Components/Carets/DrawableLyricInputCaret.cs
@@ -1,10 +1,16 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Input;
+using osu.Framework.Input.Events;
+using osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers.Lyrics;
 using osu.Game.Rulesets.Karaoke.Edit.Lyrics.CaretPosition;
+using osu.Game.Rulesets.Karaoke.Edit.Lyrics.States;
 using osuTK;
 using osuTK.Graphics;
 
@@ -18,26 +24,83 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Rows.Components.Carets
         [Resolved]
         private EditorLyricPiece lyricPiece { get; set; }
 
+        [Resolved]
+        private ILyricTextChangeHandler lyricTextChangeHandler { get; set; }
+
+        [Resolved]
+        private ILyricCaretState lyricCaretState { get; set; }
+
+        private readonly Box drawableCaret;
+        private readonly InputCaretTextBox inputCaretTextBox;
+
+        private TextCaretPosition caretPosition;
+
         public DrawableLyricInputCaret(bool preview)
             : base(preview)
         {
             Width = caret_width;
-            Colour = Color4.Transparent;
-            Masking = true;
-            CornerRadius = 1;
 
-            InternalChild = new Box
+            InternalChild = drawableCaret = new Box
             {
                 RelativeSizeAxes = Axes.Both,
                 Colour = Color4.White,
-                Alpha = preview ? 0.5f : 1
+                Alpha = preview ? 0.5f : 1,
             };
+
+            if (!preview)
+            {
+                AddInternal(inputCaretTextBox = new InputCaretTextBox
+                {
+                    Anchor = Anchor.TopRight,
+                    Origin = Anchor.TopLeft,
+                    Width = 50,
+                    Height = 20,
+                    ReleaseFocusOnCommit = false,
+                    NewCommitText = (text) =>
+                    {
+                        if (caretPosition == null)
+                            throw new ArgumentNullException(nameof(caretPosition));
+
+                        int index = caretPosition.Index;
+                        lyricTextChangeHandler.InsertText(index, text);
+
+                        moveCaret(text.Length);
+                    },
+                    DeleteText = () =>
+                    {
+                        if (caretPosition == null)
+                            throw new ArgumentNullException(nameof(caretPosition));
+
+                        int index = caretPosition.Index;
+                        if (index == 0)
+                            return;
+
+                        lyricTextChangeHandler.DeleteLyricText(index);
+
+                        moveCaret(-1);
+                    }
+                });
+            }
+
+            void moveCaret(int offset)
+            {
+                if (caretPosition == null)
+                    throw new ArgumentNullException(nameof(caretPosition));
+
+                // calculate new caret position.
+                var lyric = caretPosition.Lyric;
+                int index = caretPosition.Index + offset;
+                caretPosition = new TextCaretPosition(lyric, index);
+                lyricCaretState.MoveCaretToTargetPosition(caretPosition);
+            }
         }
 
         public override void Hide() => this.FadeOut(200);
 
         protected override void Apply(TextCaretPosition caret)
         {
+            caretPosition = caret;
+
             Height = lyricPiece.LineBaseHeight;
             var position = GetPosition(caret);
 
@@ -46,9 +109,70 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Rows.Components.Carets
 
             this.MoveTo(new Vector2(position.X - caret_width / 2, position.Y), time, Easing.Out);
             this.ResizeWidthTo(caret_width, caret_move_time, Easing.Out);
-            this
+
+            drawableCaret
                 .FadeColour(Color4.White, 200, Easing.Out)
                 .Loop(c => c.FadeTo(0.7f).FadeTo(0.4f, 500, Easing.InOutSine));
+
+            if (inputCaretTextBox == null)
+                return;
+
+            // should focus the caret if change the state.
+            Schedule(() =>
+            {
+                inputCaretTextBox.Text = string.Empty;
+                GetContainingInputManager().ChangeFocus(inputCaretTextBox);
+            });
+        }
+
+        private class InputCaretTextBox : BasicTextBox
+        {
+            public Action<string> NewCommitText;
+
+            public Action DeleteText;
+
+            // should not accept tab event because all focus/unfocus should be controlled by caret.
+            public override bool CanBeTabbedTo => false;
+
+            // should not allow cursor index change because press left/right event is handled by parent caret.
+            protected override bool AllowWordNavigation => false;
+
+            public InputCaretTextBox()
+            {
+                OnCommit += submitMessage;
+
+                void submitMessage(TextBox textBox, bool newText)
+                {
+                    string text = textBox.Text.Trim();
+
+                    if (string.IsNullOrWhiteSpace(text))
+                        return;
+
+                    NewCommitText?.Invoke(text);
+
+                    textBox.Text = string.Empty;
+                }
+            }
+
+            public override bool OnPressed(KeyBindingPressEvent<PlatformAction> e)
+            {
+                bool triggerDeleteText = processTriggerDeleteText(e.Action);
+
+                if (!triggerDeleteText || !string.IsNullOrEmpty(Text))
+                    return base.OnPressed(e);
+
+                // should trigger delete the main text in lyric if there's not pending text.
+                DeleteText?.Invoke();
+                return true;
+
+                static bool processTriggerDeleteText(PlatformAction action) =>
+                    action switch
+                    {
+                        // Deletion
+                        PlatformAction.DeleteBackwardChar => true,
+                        _ => false
+                    };
+            }
         }
     }
 }
