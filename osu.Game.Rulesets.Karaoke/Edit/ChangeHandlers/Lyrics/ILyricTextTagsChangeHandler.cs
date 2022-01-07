b263c16fd7b4b014f438634b7149179cb0ba6afb
為什麼 <andy840119@gmail.com>
2022-01-07 20:42:05 +0800

    add lyric text tag change handler interface.

---
+++ osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/Lyrics/ILyricTextTagsChangeHandler.cs
@@ -0,0 +1,18 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Karaoke.Objects.Types;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers.Lyrics
+{
+    public interface ILyricTextTagsChangeHandler<in TTextTag> where TTextTag : ITextTag
+    {
+        void Add(TTextTag textTag);
+
+        void Remove(TTextTag textTag);
+
+        void SetPosition(TTextTag textTag, int? startIndex, int? endIndex);
+
+        void SetText(TTextTag textTag, string text);
+    }
+}
