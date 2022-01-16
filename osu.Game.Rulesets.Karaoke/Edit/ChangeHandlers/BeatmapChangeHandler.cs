ca483d7aae059bb6f34bd4c38ef9f03f04f6f3f6
為什麼 <andy840119@gmail.com>
2022-01-16 14:59:30 +0800

    refactor the beatmap change handler and its relative class. the logic about sync data between karaoke beatmap and change handler should be written in the base class.

--- osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/BeatmapChangeHandler.cs
+++ osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/BeatmapChangeHandler.cs
@@ -2,9 +2,13 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using osu.Framework.Allocation;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Game.Rulesets.Karaoke.Beatmaps;
+using osu.Game.Rulesets.Karaoke.Objects;
 using osu.Game.Screens.Edit;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers
@@ -14,20 +18,73 @@ namespace osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers
         [Resolved]
         private EditorBeatmap beatmap { get; set; }
 
-        protected KaraokeBeatmap Beatmap => beatmap.PlayableBeatmap as KaraokeBeatmap;
+        private KaraokeBeatmap karaokeBeatmap => beatmap.PlayableBeatmap as KaraokeBeatmap;
 
-        [Resolved(CanBeNull = true)]
-        private IEditorChangeHandler changeHandler { get; set; }
+        protected IEnumerable<Lyric> Lyrics => karaokeBeatmap.HitObjects.OfType<Lyric>();
+
+        // todo: should be interface.
+        protected BindableList<TItem> Items = new();
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            Items.AddRange(GetItemsFromBeatmap(karaokeBeatmap));
+
+            // todo: find a better way to handle only beatmap property changed.
+            beatmap.TransactionEnded += syncItemsFromBeatmap;
+
+            syncItemsFromBeatmap();
+
+            void syncItemsFromBeatmap()
+            {
+                var items = GetItemsFromBeatmap(karaokeBeatmap);
+
+                if (Items.SequenceEqual(items))
+                    return;
+
+                Items.AddRange(items.Except(Items));
+                Items.RemoveAll(x => !items.Contains(x));
+            }
+        }
 
         protected void PerformObjectChanged(TItem item, Action<TItem> action)
         {
-            changeHandler?.BeginChange();
+            // should call change from editor beatmap because there's only way to trigger transaction ended.
+            beatmap?.BeginChange();
             action?.Invoke(item);
-            changeHandler?.EndChange();
+            beatmap?.EndChange();
         }
 
-        public abstract void Add(TItem item);
+        public abstract List<TItem> GetItemsFromBeatmap(KaraokeBeatmap beatmap);
 
-        public abstract void Remove(TItem item);
+        public void Add(TItem item)
+        {
+            var items = GetItemsFromBeatmap(karaokeBeatmap);
+            if (items.Contains(item))
+                throw new InvalidOperationException(nameof(item));
+
+            PerformObjectChanged(item, i =>
+            {
+                items.Add(i);
+                OnItemAdded(i);
+            });
+        }
+
+        public void Remove(TItem item)
+        {
+            var items = GetItemsFromBeatmap(karaokeBeatmap);
+            if (!items.Contains(item))
+                throw new InvalidOperationException($"{nameof(item)} is not in the list");
+
+            PerformObjectChanged(item, i =>
+            {
+                items.Remove(i);
+                OnItemRemoved(i);
+            });
+        }
+
+        protected abstract void OnItemAdded(TItem item);
+
+        protected abstract void OnItemRemoved(TItem item);
     }
 }
--- osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/Languages/LanguagesChangeHandler.cs
+++ osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/Languages/LanguagesChangeHandler.cs
@@ -1,61 +1,36 @@
 // Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using osu.Framework.Allocation;
 using osu.Framework.Bindables;
-using osu.Game.Rulesets.Karaoke.Objects;
+using osu.Game.Rulesets.Karaoke.Beatmaps;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers.Languages
 {
     public class LanguagesChangeHandler : BeatmapChangeHandler<CultureInfo>, ILanguagesChangeHandler
     {
-        private readonly BindableList<CultureInfo> bindableLanguages = new();
+        public IBindableList<CultureInfo> Languages => Items;
 
-        public IBindableList<CultureInfo> Languages => bindableLanguages;
+        public override List<CultureInfo> GetItemsFromBeatmap(KaraokeBeatmap beatmap)
+            => beatmap.AvailableTranslates;
 
-        private IEnumerable<Lyric> lyrics => Beatmap.HitObjects.OfType<Lyric>();
-
-        [BackgroundDependencyLoader]
-        private void load()
+        protected override void OnItemAdded(CultureInfo item)
         {
-            bindableLanguages.AddRange(Beatmap.AvailableTranslates);
-            bindableLanguages.BindCollectionChanged((_, _) => { Beatmap.AvailableTranslates = Languages.ToList(); });
+            // there's no need to do anything.
         }
 
-        public override void Add(CultureInfo item)
+        protected override void OnItemRemoved(CultureInfo item)
         {
-            if (bindableLanguages.Contains(item))
-                return;
-
-            PerformObjectChanged(item, cultureInfo =>
+            // Delete from lyric also.
+            foreach (var lyric in Lyrics.Where(lyric => lyric.Translates.ContainsKey(item)))
             {
-                bindableLanguages.Add(cultureInfo);
-            });
-        }
-
-        public override void Remove(CultureInfo item)
-        {
-            if (!bindableLanguages.Contains(item))
-                throw new InvalidOperationException($"{nameof(item)} is not in the list");
-
-            PerformObjectChanged(item, cultureInfo =>
-            {
-                // Delete from list.
-                bindableLanguages.Remove(cultureInfo);
-
-                // Delete from lyric also.
-                foreach (var lyric in lyrics.Where(lyric => lyric.Translates.ContainsKey(cultureInfo)))
-                {
-                    lyric.Translates.Remove(cultureInfo);
-                }
-            });
+                lyric.Translates.Remove(item);
+            }
         }
 
         public bool IsLanguageContainsTranslate(CultureInfo cultureInfo)
-            => lyrics.Any(x => x.Translates.ContainsKey(cultureInfo));
+            => Lyrics.Any(x => x.Translates.ContainsKey(cultureInfo));
     }
 }
