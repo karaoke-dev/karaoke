c57c24adff417ae0c4caeca0a4ce5ac5b17972d1
為什麼 <andy840119@gmail.com>
2022-01-17 21:45:57 +0800

    remove default singer display if there's no singer in the lyric.

--- osu.Game.Rulesets.Karaoke/Edit/Lyrics/Rows/Components/SubInfo/SingerInfo.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Rows/Components/SubInfo/SingerInfo.cs
@@ -1,14 +1,12 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Collections.Generic;
 using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Rulesets.Karaoke.Beatmaps;
-using osu.Game.Rulesets.Karaoke.Beatmaps.Metadatas;
 using osu.Game.Rulesets.Karaoke.Graphics.UserInterface;
 using osu.Game.Rulesets.Karaoke.Objects;
 using osu.Game.Rulesets.Karaoke.Utils;
@@ -47,21 +45,7 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Rows.Components.SubInfo
                         return;
 
                     var singers = karaokeBeatmap.Singers?.Where(singer => LyricUtils.ContainsSinger(lyric, singer)).ToList();
-
-                    if (singers?.Any() ?? false)
-                    {
-                        singerDisplay.Current.Value = singers;
-                    }
-                    else
-                    {
-                        singerDisplay.Current.Value = new List<Singer>
-                        {
-                            new()
-                            {
-                                Name = "No singer"
-                            }
-                        };
-                    }
+                    singerDisplay.Current.Value = singers;
                 });
             }, true);
         }
