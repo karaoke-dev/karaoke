b96949bd0ab246202577e7ef787e6d13ae6f7583
為什麼 <andy840119@gmail.com>
2022-01-22 16:38:54 +0800

    apply change handler in the note edit popover.

--- osu.Game.Rulesets.Karaoke/Edit/Components/UserInterfaceV2/NoteEditPopover.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Components/UserInterfaceV2/NoteEditPopover.cs
@@ -1,17 +1,27 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Graphics.UserInterfaceV2;
+using osu.Game.Rulesets.Edit;
+using osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers.Notes;
 using osu.Game.Rulesets.Karaoke.Objects;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.Components.UserInterfaceV2
 {
     public class NoteEditPopover : OsuPopover
     {
+        [Resolved(canBeNull: true)]
+        private INotePropertyChangeHandler notePropertyChangeHandler { get; set; }
+
         public NoteEditPopover(Note note)
         {
+            LabelledTextBox text;
+            LabelledTextBox rubyText;
+            LabelledSwitchButton display;
+
             Children = new Drawable[]
             {
                 new FillFlowContainer
@@ -21,27 +31,55 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Components.UserInterfaceV2
                     AutoSizeAxes = Axes.Y,
                     Children = new Drawable[]
                     {
-                        new LabelledTextBox
+                        text = new LabelledTextBox
                         {
                             Label = "Text",
                             Description = "The text display on the note.",
                             Current = note.TextBindable
                         },
-                        new LabelledTextBox
+                        rubyText = new LabelledTextBox
                         {
                             Label = "Ruby text",
-                            Description = "Should placing something like ruby, 拼音 or ふりがな.",
+                            Description = "Should place something like ruby, 拼音 or ふりがな.",
                             Current = note.RubyTextBindable
                         },
-                        new LabelledSwitchButton
+                        display = new LabelledSwitchButton
                         {
                             Label = "Display",
                             Description = "This note will be hidden and not scorable if not display.",
-                            Current = note.DisplayBindable,
+                            Current = note.DisplayBindable
                         }
                     }
                 }
             };
+
+            text.OnCommit += (sender, newText) =>
+            {
+                string text = sender.Text.Trim();
+                notePropertyChangeHandler?.ChangeText(text);
+            };
+
+            rubyText.OnCommit += (sender, newText) =>
+            {
+                string text = sender.Text.Trim();
+                notePropertyChangeHandler?.ChangeRubyText(text);
+            };
+
+            display.Current.BindValueChanged(v =>
+            {
+                notePropertyChangeHandler?.ChangeDisplayState(v.NewValue);
+            });
+        }
+
+        [BackgroundDependencyLoader(true)]
+        private void load(HitObjectComposer composer)
+        {
+            if (notePropertyChangeHandler != null || composer == null)
+                return;
+
+            // todo: not a good way to get change handler, might remove or found another way eventually.
+            // cannot get change handler directly in editor screen, so should trying to get from karaoke hit object composer.
+            notePropertyChangeHandler = composer.Dependencies.Get<INotePropertyChangeHandler>();
         }
     }
 }
