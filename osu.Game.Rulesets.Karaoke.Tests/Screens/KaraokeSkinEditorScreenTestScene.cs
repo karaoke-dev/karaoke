cdde6ce0121e7197106211a9bbc5dbb033eb8bb8
andy840119 <andy840119@gmail.com>
2022-02-02 23:23:03 +0800

    remove the ugly way to load the skin.

--- osu.Game.Rulesets.Karaoke.Tests/Screens/KaraokeSkinEditorScreenTestScene.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Screens/KaraokeSkinEditorScreenTestScene.cs
@@ -1,17 +1,13 @@
 // Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Linq;
-using System.Reflection;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Game.IO;
 using osu.Game.Overlays;
-using osu.Game.Rulesets.Karaoke.Beatmaps.Formats;
 using osu.Game.Rulesets.Karaoke.Screens.Skin;
 using osu.Game.Rulesets.Karaoke.Skinning;
-using osu.Game.Rulesets.Karaoke.Skinning.Elements;
+using osu.Game.Rulesets.Karaoke.Tests.Resources;
 using osu.Game.Skinning;
 using osu.Game.Tests.Visual;
 
@@ -41,40 +37,8 @@ namespace osu.Game.Rulesets.Karaoke.Tests.Screens
         protected class TestKaraokeBeatmapSkin : KaraokeBeatmapSkin
         {
             public TestKaraokeBeatmapSkin()
-                : base(new SkinInfo(), null)
+                : base(new SkinInfo(), TestResources.CreateSkinStorageResourceProvider())
             {
-                // TODO : need a better way to load resource
-                var assembly = Assembly.GetExecutingAssembly();
-                const string resource_name = @"osu.Game.Rulesets.Karaoke.Tests.Resources.Testing.Skin.default.skin";
-
-                using (var stream = assembly.GetManifestResourceStream(resource_name))
-                using (var reader = new LineBufferedReader(stream))
-                {
-                    var karaokeSkin = new KaraokeSkinDecoder().Decode(reader);
-
-                    // Default values
-                    DefaultElement[ElementType.LyricConfig] = karaokeSkin.DefaultLyricConfig;
-                    DefaultElement[ElementType.LyricStyle] = karaokeSkin.DefaultLyricStyle;
-                    DefaultElement[ElementType.NoteStyle] = karaokeSkin.DefaultNoteStyle;
-
-                    // Create bindable
-                    foreach (LyricLayout t in karaokeSkin.Layouts)
-                        Elements[ElementType.LyricLayout].Add(t);
-
-                    foreach (LyricStyle t in karaokeSkin.LyricStyles)
-                        Elements[ElementType.LyricStyle].Add(t);
-
-                    foreach (NoteStyle t in karaokeSkin.NoteStyles)
-                        Elements[ElementType.NoteStyle].Add(t);
-
-                    // before apply new logic, add fake lyric layout for prevent test failed.
-                    Elements[ElementType.LyricConfig].Add(LyricConfig.DEFAULT);
-
-                    // Create lookups
-                    BindableFontsLookup.Value = karaokeSkin.LyricStyles.ToDictionary(k => karaokeSkin.LyricStyles.IndexOf(k), y => y.Name);
-                    BindableLayoutsLookup.Value = karaokeSkin.Layouts.ToDictionary(k => karaokeSkin.Layouts.IndexOf(k), y => y.Name);
-                    BindableNotesLookup.Value = karaokeSkin.NoteStyles.ToDictionary(k => karaokeSkin.NoteStyles.IndexOf(k), y => y.Name);
-                }
             }
         }
     }
