b09d68f3c285e6d70dbb70e8c47e8eff81e542dc
andy840119 <andy840119@gmail.com>
2022-02-12 16:56:27 +0800

    implement IHasEditModeState for define the state has mode to switch.

---
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/States/Modes/IHasEditModeState.cs
@@ -0,0 +1,17 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using osu.Framework.Bindables;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.States.Modes
+{
+    public interface IHasEditModeState<T> where T : Enum
+    {
+        IBindable<T> BindableEditMode { get; }
+
+        T EditMode => BindableEditMode.Value;
+
+        void ChangeEditMode(T mode);
+    }
+}
