7e5e49488f9aacb4350c3fb938b5c577c29dfc05
andy840119 <andy840119@gmail.com>
2022-02-23 23:37:13 +0800

    adjust json beatmap serializer/deserializer. also register in the constructor, like how legacy beatmap decoder does.

--- osu.Game.Rulesets.Karaoke/Beatmaps/Formats/KaraokeJsonBeatmapDecoder.cs
+++ osu.Game.Rulesets.Karaoke/Beatmaps/Formats/KaraokeJsonBeatmapDecoder.cs
@@ -1,17 +1,28 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using System.Linq;
 using Newtonsoft.Json;
 using osu.Game.Beatmaps;
 using osu.Game.Beatmaps.Formats;
 using osu.Game.IO;
 using osu.Game.IO.Serialization;
 using osu.Game.Rulesets.Karaoke.IO.Serialization.Converters;
+using osu.Game.Rulesets.Karaoke.Objects;
 
 namespace osu.Game.Rulesets.Karaoke.Beatmaps.Formats
 {
     public class KaraokeJsonBeatmapDecoder : JsonBeatmapDecoder
     {
+        public new static void Register()
+        {
+            AddDecoder<Beatmap>("// karaoke json file format v", m => new KaraokeJsonBeatmapDecoder());
+
+            // use this weird way to let all the fall-back beatmap(include karaoke beatmap) become karaoke beatmap.
+            SetFallbackDecoder<Beatmap>(() => new KaraokeJsonBeatmapDecoder());
+        }
+
         protected override void ParseStreamInto(LineBufferedReader stream, Beatmap output)
         {
             var globalSetting = JsonSerializableExtensions.CreateGlobalSettings();
@@ -27,10 +38,32 @@ namespace osu.Game.Rulesets.Karaoke.Beatmaps.Formats
             // create id if object is by reference.
             globalSetting.PreserveReferencesHandling = PreserveReferencesHandling.Objects;
 
-            // replace string stream.ReadToEnd().DeserializeInto(output);
-            JsonConvert.PopulateObject(stream.ReadToEnd(), output, globalSetting);
+            // should not let json decoder to read this line.
+            if (stream.PeekLine().Contains("// karaoke json file format v"))
+            {
+                stream.ReadLine();
+            }
 
-            base.ParseStreamInto(stream, output);
+            // equal to stream.ReadToEnd().DeserializeInto(output); in the base class.
+            JsonConvert.PopulateObject(stream.ReadToEnd(), output, globalSetting);
+            var lyrics = output.HitObjects.OfType<Lyric>().ToArray();
+
+            foreach (var hitObject in output.HitObjects)
+            {
+                if (hitObject is Note note)
+                {
+                    // because of json serializer contains object reference issue with serialize/deserialize the beatmap.
+                    // so should re-assign the lyric instance.
+                    note.ParentLyric = lyrics.FirstOrDefault(x => x.ID == note.ParentLyric.ID);
+
+                    if (note.ParentLyric == null)
+                        throw new InvalidOperationException();
+                }
+
+                hitObject.ApplyDefaults(output.ControlPointInfo, output.Difficulty);
+            }
+
+            var notes = output.HitObjects.OfType<Note>();
         }
     }
 }
--- osu.Game.Rulesets.Karaoke/Beatmaps/Formats/KaraokeJsonBeatmapEncoder.cs
+++ osu.Game.Rulesets.Karaoke/Beatmaps/Formats/KaraokeJsonBeatmapEncoder.cs
@@ -1,10 +1,13 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using System.Linq;
 using Newtonsoft.Json;
 using osu.Game.Beatmaps;
 using osu.Game.IO.Serialization;
 using osu.Game.Rulesets.Karaoke.IO.Serialization.Converters;
+using osu.Game.Rulesets.Karaoke.Objects;
 
 namespace osu.Game.Rulesets.Karaoke.Beatmaps.Formats
 {
@@ -22,8 +25,19 @@ namespace osu.Game.Rulesets.Karaoke.Beatmaps.Formats
             globalSetting.Converters.Add(new TimeTagsConverter());
             globalSetting.Converters.Add(new ToneConverter());
 
+            // should check that every parent in the note contains in the hit objects list.
+            var lyrics = output.HitObjects.OfType<Lyric>();
+            var notes = output.HitObjects.OfType<Note>();
+
+            // make sure that every note has parent lyric.
+            if (notes.Any(note => note.ParentLyric == null || !lyrics.Contains(note.ParentLyric)))
+            {
+                throw new InvalidOperationException();
+            }
+
             // replace string stream.ReadToEnd().Serialize(output);
-            return JsonConvert.SerializeObject(output, globalSetting);
+            string json = JsonConvert.SerializeObject(output, globalSetting);
+            return "// karaoke json file format v1" + '\n' + json;
         }
     }
 }
