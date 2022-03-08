97c81e6f6fd95a52c81d77ee32c855ba92334c5d
andy840119 <andy840119@gmail.com>
2022-03-08 21:57:20 +0800

    force use the list as return type. for prevent return the enumerator because it will cause error in the json decoder.

--- osu.Game.Rulesets.Karaoke/IO/Serialization/Converters/RubyTagsConverter.cs
+++ osu.Game.Rulesets.Karaoke/IO/Serialization/Converters/RubyTagsConverter.cs
@@ -9,7 +9,7 @@ namespace osu.Game.Rulesets.Karaoke.IO.Serialization.Converters
 {
     public class RubyTagsConverter : SortableJsonConvertor<RubyTag>
     {
-        protected override IEnumerable<RubyTag> GetSortedValue(IEnumerable<RubyTag> objects)
+        protected override IList<RubyTag> GetSortedValue(IEnumerable<RubyTag> objects)
             => TextTagsUtils.Sort(objects);
     }
 }
