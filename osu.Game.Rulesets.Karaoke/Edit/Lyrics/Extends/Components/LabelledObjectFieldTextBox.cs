7ba6e9e0cfe01474f4f14a0a8dfa22e3aa300239
andy840119 <andy840119@gmail.com>
2022-03-12 11:07:05 +0800

    got a nice way to force submit the text if caret position changed.

--- osu.Game.Rulesets.Karaoke/Edit/Lyrics/Extends/Components/LabelledObjectFieldTextBox.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Extends/Components/LabelledObjectFieldTextBox.cs
@@ -10,6 +10,7 @@ using osu.Framework.Input.Events;
 using osu.Game.Graphics;
 using osu.Game.Graphics.UserInterface;
 using osu.Game.Graphics.UserInterfaceV2;
+using osu.Game.Rulesets.Karaoke.Edit.Lyrics.CaretPosition;
 using osu.Game.Rulesets.Karaoke.Edit.Lyrics.States;
 using osu.Game.Rulesets.Karaoke.Objects;
 using osuTK.Graphics;
@@ -22,8 +23,7 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends.Components
 
         protected new ObjectFieldTextBox Component => (ObjectFieldTextBox)base.Component;
 
-        [Resolved]
-        private ILyricCaretState lyricCaretState { get; set; }
+        private readonly IBindable<ICaretPosition> bindableCaretPosition = new Bindable<ICaretPosition>();
 
         private readonly T item;
 
@@ -34,13 +34,21 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends.Components
             // apply current text from text-tag.
             Component.Text = GetFieldValue(item);
 
+            // should commit the editing text if caret lyric changed.
+            bindableCaretPosition.BindValueChanged(e =>
+            {
+                if (!Component.HasFocus || e.NewValue.Lyric == lyric)
+                    return;
+
+                ApplyValue(item, Component.Text);
+            });
+
             // should change preview text box if selected ruby/romaji changed.
             OnCommit += (sender, _) =>
             {
-                // because selected lyric might changed if user click another lyric before on commit triggered.
-                // so should force to select the lyric back.
-                if (lyricCaretState.BindableCaretPosition.Value.Lyric != lyric)
-                    lyricCaretState.MoveCaretToTargetPosition(lyric);
+                // prevent duplicated submit after lyric position changed.
+                if (bindableCaretPosition.Value.Lyric != lyric)
+                    return;
 
                 ApplyValue(item, sender.Text);
             };
@@ -93,6 +101,12 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends.Components
                 }
             }
         };
+
+        [BackgroundDependencyLoader]
+        private void load(ILyricCaretState lyricCaretState)
+        {
+            bindableCaretPosition.BindTo(lyricCaretState.BindableCaretPosition);
+        }
 
         protected class ObjectFieldTextBox : OsuTextBox
         {
