be8b76d9ad113923c34371386eefa9f6307017d4
andy840119 <andy840119@gmail.com>
2022-03-13 16:00:03 +0800

    implement delete lyric sub-section.

---
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Extends/Manage/ManageDeleteSubsection.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers.Lyrics;
+using osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends.Components;
+using osu.Game.Rulesets.Karaoke.Edit.Lyrics.States;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends.Manage
+{
+    public class ManageDeleteSubsection : SelectLyricButton
+    {
+        protected override string StandardText => "Delete";
+
+        protected override string SelectingText => "Cancel delete";
+
+        [BackgroundDependencyLoader]
+        private void load(ILyricSelectionState lyricSelectionState, ILyricsChangeHandler lyricsChangeHandler)
+        {
+            lyricSelectionState.Action = e =>
+            {
+                if (e != LyricEditorSelectingAction.Apply)
+                    return;
+
+                lyricsChangeHandler.Remove();
+            };
+        }
+    }
+}
