0a7528b0c521726edd7d50ae411bc0f2d3f23fbd
andy840119 <andy840119@gmail.com>
2022-03-15 21:35:25 +0800

    feat(extend-area): add description in the ruby/romaji edit area.

--- osu.Game.Rulesets.Karaoke/Edit/Lyrics/Extends/RubyRomaji/RubyTagEditModeSection.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Extends/RubyRomaji/RubyTagEditModeSection.cs
@@ -3,6 +3,7 @@
 
 using System.Collections.Generic;
 using osu.Framework.Allocation;
+using osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends.Components.Description;
 using osu.Game.Rulesets.Karaoke.Edit.Lyrics.States.Modes;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends.RubyRomaji
@@ -22,7 +23,41 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends.RubyRomaji
                     TextTagEditMode.Generate, new EditModeSelectionItem("Generate", "Auto-generate rubies in the lyric.")
                 },
                 {
-                    TextTagEditMode.Edit, new EditModeSelectionItem("Edit", "Create / delete and edit lyric rubies in here.")
+                    TextTagEditMode.Edit,
+                    new EditModeSelectionItem("Edit", new DescriptionFormat
+                    {
+                        Text = "Create / delete and edit lyric rubies in here.\n"
+                               + "Click [key](directions) to select the target lyric.\n"
+                               + "Press `Tab` to switch between the ruby tags.\n"
+                               + "Than, press [key](adjust_text_tag_index) or button to adjust ruby index after hover to edit index area.",
+                        Keys = new Dictionary<string, InputKey>
+                        {
+                            {
+                                "directions", new InputKey
+                                {
+                                    Text = "Up or down",
+                                    AdjustableActions = new List<KaraokeEditAction>
+                                    {
+                                        KaraokeEditAction.Up,
+                                        KaraokeEditAction.Down
+                                    }
+                                }
+                            },
+                            {
+                                "adjust_text_tag_index", new InputKey
+                                {
+                                    Text = "Keys",
+                                    AdjustableActions = new List<KaraokeEditAction>
+                                    {
+                                        KaraokeEditAction.EditTextTagReduceStartIndex,
+                                        KaraokeEditAction.EditTextTagIncreaseStartIndex,
+                                        KaraokeEditAction.EditTextTagReduceEndIndex,
+                                        KaraokeEditAction.EditTextTagIncreaseEndIndex,
+                                    }
+                                }
+                            }
+                        }
+                    })
                 },
                 {
                     TextTagEditMode.Verify, new EditModeSelectionItem("Verify", "Check invalid rubies in here.")
