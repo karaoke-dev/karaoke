d49318587f4c94564f985a47a1021f7b1d462de4
andy840119 <andy840119@gmail.com>
2022-03-18 20:57:17 +0800

    Add more toasts.

--- osu.Game.Rulesets.Karaoke/Configuration/KaraokeRulesetConfigManager.cs
+++ osu.Game.Rulesets.Karaoke/Configuration/KaraokeRulesetConfigManager.cs
@@ -113,7 +113,14 @@ namespace osu.Game.Rulesets.Karaoke.Configuration
 
         public override TrackedSettings CreateTrackedSettings() => new()
         {
-            new TrackedSetting<double>(KaraokeRulesetSetting.ScrollTime, v => new SettingDescription(v, "Scroll Time", $"{v}ms"))
+            new TrackedSetting<double>(KaraokeRulesetSetting.ScrollTime, v => new SettingDescription(v, "Scroll Time", $"{v}ms")),
+            new TrackedSetting<bool>(KaraokeRulesetSetting.DisplayNoteRubyText, b => new SettingDescription(b, "Toggle display", b ? "Show" : "Hide")),
+            new TrackedSetting<bool>(KaraokeRulesetSetting.ShowCursor, b => new SettingDescription(b, "Cursor display", b ? "Show" : "Hide")),
+            new TrackedSetting<bool>(KaraokeRulesetSetting.UseTranslate, b => new SettingDescription(b, "Display translate", b ? "Show" : "Hide")),
+            new TrackedSetting<CultureInfo>(KaraokeRulesetSetting.PreferLanguage, c => new SettingDescription(c, "Translate language", c.DisplayName)),
+            new TrackedSetting<bool>(KaraokeRulesetSetting.DisplayRuby, b => new SettingDescription(b, "Display ruby", b ? "Show" : "Hide")),
+            new TrackedSetting<bool>(KaraokeRulesetSetting.DisplayRomaji, b => new SettingDescription(b, "Display romaji", b ? "Show" : "Hide")),
+            new TrackedSetting<string>(KaraokeRulesetSetting.MicrophoneDevice, d => new SettingDescription(d, "Change to the new microphone device", d)),
         };
     }
 
