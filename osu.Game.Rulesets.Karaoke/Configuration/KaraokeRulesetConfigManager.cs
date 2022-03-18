a8d39310114669b40fc476654b0790196566f1c7
andy840119 <andy840119@gmail.com>
2022-03-18 20:55:47 +0800

    Display ruby text -> Display note ruby text for prevent confusing.

--- osu.Game.Rulesets.Karaoke/Configuration/KaraokeRulesetConfigManager.cs
+++ osu.Game.Rulesets.Karaoke/Configuration/KaraokeRulesetConfigManager.cs
@@ -28,7 +28,7 @@ namespace osu.Game.Rulesets.Karaoke.Configuration
             // Visual
             SetDefault(KaraokeRulesetSetting.ScrollTime, 5000.0, 1000.0, 10000.0, 100.0);
             SetDefault(KaraokeRulesetSetting.ScrollDirection, KaraokeScrollingDirection.Left);
-            SetDefault(KaraokeRulesetSetting.DisplayRubyText, false);
+            SetDefault(KaraokeRulesetSetting.DisplayNoteRubyText, false);
             SetDefault(KaraokeRulesetSetting.ShowCursor, false);
             SetDefault(KaraokeRulesetSetting.NoteAlpha, 1, 0.2, 1, 0.01);
             SetDefault(KaraokeRulesetSetting.LyricAlpha, 1, 0.2, 1, 0.01);
@@ -122,7 +122,7 @@ namespace osu.Game.Rulesets.Karaoke.Configuration
         // Visual
         ScrollTime,
         ScrollDirection,
-        DisplayRubyText,
+        DisplayNoteRubyText,
         ShowCursor,
         NoteAlpha,
         LyricAlpha,
