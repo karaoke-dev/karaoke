53735023ee27bbfbf5cb869e00230fba13113246
andy840119 <andy840119@gmail.com>
2022-03-19 20:39:01 +0800

    fix performance calculate api breaking change. all the code is copied from mania.

--- osu.Game.Rulesets.Karaoke/Difficulty/KaraokeDifficultyAttributes.cs
+++ osu.Game.Rulesets.Karaoke/Difficulty/KaraokeDifficultyAttributes.cs
@@ -1,12 +1,47 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
+using Newtonsoft.Json;
 using osu.Game.Rulesets.Difficulty;
 
 namespace osu.Game.Rulesets.Karaoke.Difficulty
 {
     public class KaraokeDifficultyAttributes : DifficultyAttributes
     {
+        /// <summary>
+        /// The hit window for a GREAT hit inclusive of rate-adjusting mods (DT/HT/etc).
+        /// </summary>
+        /// <remarks>
+        /// Rate-adjusting mods do not affect the hit window at all in osu-stable.
+        /// </remarks>
+        [JsonProperty("great_hit_window")]
         public double GreatHitWindow { get; set; }
+
+        /// <summary>
+        /// The score multiplier applied via score-reducing mods.
+        /// </summary>
+        [JsonProperty("score_multiplier")]
+        public double ScoreMultiplier { get; set; }
+
+        public override IEnumerable<(int attributeId, object value)> ToDatabaseAttributes()
+        {
+            foreach (var v in base.ToDatabaseAttributes())
+                yield return v;
+
+            // Todo: osu!mania doesn't output MaxCombo attribute for some reason.
+            yield return (ATTRIB_ID_DIFFICULTY, StarRating);
+            yield return (ATTRIB_ID_GREAT_HIT_WINDOW, GreatHitWindow);
+            yield return (ATTRIB_ID_SCORE_MULTIPLIER, ScoreMultiplier);
+        }
+
+        public override void FromDatabaseAttributes(IReadOnlyDictionary<int, double> values)
+        {
+            base.FromDatabaseAttributes(values);
+
+            StarRating = values[ATTRIB_ID_DIFFICULTY];
+            GreatHitWindow = values[ATTRIB_ID_GREAT_HIT_WINDOW];
+            ScoreMultiplier = values[ATTRIB_ID_SCORE_MULTIPLIER];
+        }
     }
 }
--- osu.Game.Rulesets.Karaoke/Difficulty/KaraokePerformanceAttributes.cs
+++ osu.Game.Rulesets.Karaoke/Difficulty/KaraokePerformanceAttributes.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using osu.Game.Rulesets.Difficulty;
 
@@ -8,10 +9,22 @@ namespace osu.Game.Rulesets.Karaoke.Difficulty
 {
     public class KaraokePerformanceAttributes : PerformanceAttributes
     {
+        [JsonProperty("difficulty")]
+        public double Difficulty { get; set; }
+
         [JsonProperty("accuracy")]
         public double Accuracy { get; set; }
 
         [JsonProperty("scaled_score")]
         public double ScaledScore { get; set; }
+
+        public override IEnumerable<PerformanceDisplayAttribute> GetAttributesForDisplay()
+        {
+            foreach (var attribute in base.GetAttributesForDisplay())
+                yield return attribute;
+
+            yield return new PerformanceDisplayAttribute(nameof(Difficulty), "Difficulty", Difficulty);
+            yield return new PerformanceDisplayAttribute(nameof(Accuracy), "Accuracy", Accuracy);
+        }
     }
 }
