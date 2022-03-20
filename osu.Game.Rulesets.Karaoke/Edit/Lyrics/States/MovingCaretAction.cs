23cc3516828e5fd28f955f2787aa5b2a1129c684
andy840119 <andy840119@gmail.com>
2022-03-20 14:19:39 +0800

    mopving MovingCaretAction to the state namespace.

--- osu.Game.Rulesets.Karaoke/Edit/Lyrics/States/MovingCaretAction.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/States/MovingCaretAction.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics
+namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.States
 {
     public enum MovingCaretAction
     {
