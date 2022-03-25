5c3995c4eee73223698d11edce31cfae3162a876
andy840119 <andy840119@gmail.com>
2022-03-25 21:10:01 +0800

    Add the basic utils.

---
+++ osu.Game.Rulesets.Karaoke/Utils/CultureInfoUtils.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Globalization;
+
+namespace osu.Game.Rulesets.Karaoke.Utils
+{
+    public static class CultureInfoUtils
+    {
+        /// <summary>
+        /// Get all the languages that are not related to the country
+        /// </summary>
+        /// <returns></returns>
+        public static CultureInfo[] GetAvailableLanguages()
+            => CultureInfo.GetCultures(CultureTypes.NeutralCultures);
+
+        public static bool IsLanguage(CultureInfo cultureInfo)
+            => (cultureInfo.CultureTypes & CultureTypes.NeutralCultures) != 0;
+
+        public static string GetLanguageDisplayText(CultureInfo cultureInfo)
+            => cultureInfo.NativeName;
+    }
+}
