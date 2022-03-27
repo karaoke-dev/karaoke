239c98aa69858e6a91fd473e50486abf0b78d10a
andy840119 <andy840119@gmail.com>
2022-03-27 14:23:36 +0800

    Because selected hit objects was cleared after end of selection. So should re-add them into the selection list again.

--- osu.Game.Rulesets.Karaoke/Edit/Lyrics/Extends/Languages/AssignLanguageSubsection.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Extends/Languages/AssignLanguageSubsection.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using System.Globalization;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
@@ -11,6 +12,8 @@ using osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers.Lyrics;
 using osu.Game.Rulesets.Karaoke.Edit.Components.UserInterfaceV2;
 using osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends.Components;
 using osu.Game.Rulesets.Karaoke.Edit.Lyrics.States;
+using osu.Game.Rulesets.Objects;
+using osu.Game.Screens.Edit;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends.Languages
 {
@@ -22,13 +25,18 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends.Languages
 
         private readonly Bindable<CultureInfo> bindableLanguage = new();
 
+        private readonly List<HitObject> selectedLyrics = new();
+
         [BackgroundDependencyLoader]
-        private void load(ILyricSelectionState lyricSelectionState, ILyricLanguageChangeHandler lyricLanguageChangeHandler)
+        private void load(ILyricSelectionState lyricSelectionState, ILyricLanguageChangeHandler lyricLanguageChangeHandler, ILyricCaretState lyricCaretState, EditorBeatmap beatmap)
         {
             lyricSelectionState.Action = e =>
             {
                 if (e != LyricEditorSelectingAction.Apply)
                     return;
+
+                selectedLyrics.Clear();
+                selectedLyrics.AddRange(beatmap.SelectedHitObjects);
 
                 this.ShowPopover();
             };
@@ -39,10 +47,16 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends.Languages
                 if (language == null)
                     return;
 
+                // Note: because selected hit objects was cleared after end of selection.
+                // So should re-add them into the selection list again.
+                beatmap.SelectedHitObjects.AddRange(selectedLyrics);
                 lyricLanguageChangeHandler.SetLanguage(language);
 
                 this.HidePopover();
                 bindableLanguage.Value = null;
+
+                // after apply the language, should make sure that should sync the selected hit object again.
+                lyricCaretState.SyncSelectedHitObjectWithCaret();
             });
         }
 
