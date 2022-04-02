a94f359d7bbf82a146fd64e0d7c11f653ebdc408
andy840119 <andy840119@gmail.com>
2022-04-02 13:08:18 +0800

    hide the back button because it's not possible to show the back button only in the drag lyric step.

--- osu.Game.Rulesets.Karaoke/Edit/ImportLyric/LyricImporter.cs
+++ osu.Game.Rulesets.Karaoke/Edit/ImportLyric/LyricImporter.cs
@@ -7,9 +7,12 @@ using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Cursor;
 using osu.Framework.Graphics.Shapes;
+using osu.Framework.Input.Bindings;
+using osu.Framework.Input.Events;
 using osu.Framework.Screens;
 using osu.Game.Beatmaps;
 using osu.Game.Graphics.Containers;
+using osu.Game.Input.Bindings;
 using osu.Game.Rulesets.Karaoke.Beatmaps;
 using osu.Game.Rulesets.Karaoke.Configuration;
 using osu.Game.Rulesets.Karaoke.Edit.Checker;
@@ -19,7 +22,7 @@ using osu.Game.Screens.Play;
 namespace osu.Game.Rulesets.Karaoke.Edit.ImportLyric
 {
     [Cached(typeof(IImportStateResolver))]
-    public class LyricImporter : ScreenWithBeatmapBackground, IImportStateResolver
+    public class LyricImporter : ScreenWithBeatmapBackground, IImportStateResolver, IKeyBindingHandler<GlobalAction>
     {
         private readonly LyricImporterWaveContainer waves;
 
@@ -40,6 +43,9 @@ namespace osu.Game.Rulesets.Karaoke.Edit.ImportLyric
 
         protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent)
             => dependencies = new DependencyContainer(base.CreateChildDependencies(parent));
+
+        // Hide the back button because we cannot show it only in the first step.
+        public override bool AllowBackButton => false;
 
         public event Action<IBeatmap> OnImportFinished;
 
@@ -119,6 +125,41 @@ namespace osu.Game.Rulesets.Karaoke.Edit.ImportLyric
         {
             this.Exit();
             OnImportFinished?.Invoke(editorBeatmap);
+        }
+
+        public bool OnPressed(KeyBindingPressEvent<GlobalAction> e)
+        {
+            if (e.Repeat)
+                return false;
+
+            switch (e.Action)
+            {
+                case GlobalAction.Back:
+                    // as we don't want to display the back button, manual handling of exit action is required.
+                    // follow how editor.cs does.
+                    if (ScreenStack.CurrentScreen is not ILyricImporterStepScreen screen)
+                        throw new InvalidOperationException("Screen stack should only contains step screen");
+
+                    if (screen.Step != LyricImporterStep.ImportLyric)
+                    {
+                        // the better UX behavior should be move to the previous step.
+                        // But it will not asking.
+                        return false;
+
+                        // todo: implement.
+                        // ScreenStack.Exit();
+                    }
+
+                    this.Exit();
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+
+        public void OnReleased(KeyBindingReleaseEvent<GlobalAction> e)
+        {
         }
 
         private class LyricImporterWaveContainer : WaveContainer
