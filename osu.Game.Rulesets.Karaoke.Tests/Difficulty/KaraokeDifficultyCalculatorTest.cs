702749a56c54c8bf0493bc74299a9800892b2a03
andy840119 <andy840119@gmail.com>
2022-04-05 22:10:59 +0800

    Add tests for diffcalc max combo

--- osu.Game.Rulesets.Karaoke.Tests/Difficulty/KaraokeDifficultyCalculatorTest.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Difficulty/KaraokeDifficultyCalculatorTest.cs
@@ -21,10 +21,10 @@ namespace osu.Game.Rulesets.Karaoke.Tests.Difficulty
 
         protected override string ResourceAssembly => "osu.Game.Rulesets.Karaoke.Tests";
 
-        [TestCase(1.7028276483104616d, "karaoke-file-samples")]
-        [TestCase(1.6935488434919981d, "karaoke-file-samples-without-note")]
-        public void Test(double expected, string name)
-            => base.Test(expected, name);
+        [TestCase(1.7028276483104616d, 936, "karaoke-file-samples")]
+        [TestCase(1.6935488434919981d, 924, "karaoke-file-samples-without-note")]
+        public void Test(double expectedStarRating, int expectedMaxCombo, string name)
+            => base.Test(expectedStarRating, expectedMaxCombo, name);
 
         protected override DifficultyCalculator CreateDifficultyCalculator(IWorkingBeatmap beatmap) => new KaraokeDifficultyCalculator(new KaraokeRuleset().RulesetInfo, beatmap);
 
