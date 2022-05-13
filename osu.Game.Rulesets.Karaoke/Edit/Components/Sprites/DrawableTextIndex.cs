043edcf6f7488cc643589f9d3478cd1ad63fc6c7
andy840119 <andy840119@gmail.com>
2022-05-13 23:13:09 +0800

    Implement drawable text index.

---
+++ osu.Game.Rulesets.Karaoke/Edit/Components/Sprites/DrawableTextIndex.cs
@@ -0,0 +1,30 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Rulesets.Karaoke.Graphics.Shapes;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Components.Sprites
+{
+    public class DrawableTextIndex : RightTriangle
+    {
+        private TextIndex.IndexState state;
+
+        public TextIndex.IndexState State
+        {
+            get => state;
+            set
+            {
+                state = value;
+
+                RightAngleDirection = state switch
+                {
+                    TextIndex.IndexState.Start => TriangleRightAngleDirection.BottomLeft,
+                    TextIndex.IndexState.End => TriangleRightAngleDirection.BottomRight,
+                    _ => throw new ArgumentOutOfRangeException(nameof(value))
+                };
+            }
+        }
+    }
+}
