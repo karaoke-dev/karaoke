604f4569602583a68d1a92f3e872d673e4989a11
andy840119 <andy840119@gmail.com>
2022-05-14 11:13:09 +0800

    Use the enum type directly in the enum utils.

--- osu.Game.Rulesets.Karaoke/Utils/EnumUtils.cs
+++ osu.Game.Rulesets.Karaoke/Utils/EnumUtils.cs
@@ -9,13 +9,13 @@ namespace osu.Game.Rulesets.Karaoke.Utils
 {
     public static class EnumUtils
     {
-        public static T[] GetValues<T>() where T : struct
+        public static T[] GetValues<T>() where T : Enum
             => (T[])Enum.GetValues(typeof(T));
 
-        public static T GetPreviousValue<T>(T v) where T : struct
+        public static T GetPreviousValue<T>(T v) where T : Enum
             => GetValues<T>().Concat(new[] { default(T) }).Reverse().SkipWhile(e => !EqualityComparer<T>.Default.Equals(v, e)).Skip(1).First();
 
-        public static T GetNextValue<T>(T v) where T : struct
+        public static T GetNextValue<T>(T v) where T : Enum
             => GetValues<T>().Concat(new[] { default(T) }).SkipWhile(e => !EqualityComparer<T>.Default.Equals(v, e)).Skip(1).First();
     }
 }
