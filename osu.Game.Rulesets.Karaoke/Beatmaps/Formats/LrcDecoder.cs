35575cee5dcea2e967a0b6af2b38cad2127bb88a
andy840119 <andy840119@gmail.com>
2022-05-27 00:07:58 +0800

    Fix the compile error in the decoder and encoder. Use the new logic to conversion the class between the parse and object in this ruleset.

--- osu.Game.Rulesets.Karaoke/Beatmaps/Formats/LrcDecoder.cs
+++ osu.Game.Rulesets.Karaoke/Beatmaps/Formats/LrcDecoder.cs
@@ -1,16 +1,16 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using LyricMaker.Extensions;
-using LyricMaker.Parser;
-using osu.Framework.Graphics.Sprites;
+using LrcParser.Model;
 using osu.Game.Beatmaps;
 using osu.Game.Beatmaps.Formats;
 using osu.Game.IO;
 using osu.Game.Rulesets.Karaoke.Objects;
+using Lyric = osu.Game.Rulesets.Karaoke.Objects.Lyric;
+using RubyTag = osu.Game.Rulesets.Karaoke.Objects.RubyTag;
+using TextIndex = osu.Framework.Graphics.Sprites.TextIndex;
 
 namespace osu.Game.Rulesets.Karaoke.Beatmaps.Formats
 {
@@ -28,67 +28,50 @@ namespace osu.Game.Rulesets.Karaoke.Beatmaps.Formats
             output.HitObjects.Clear();
 
             string lyricText = stream.ReadToEnd();
-            var result = new LrcParser().Decode(lyricText);
+            var result = new LrcParser.Parser.Lrc.LrcParser().Decode(lyricText);
 
-            // Convert line
-            for (int i = 0; i < result.Lines.Length; i++)
+            foreach (var lrcLyric in result.Lyrics)
             {
-                // Empty line should not be imported
-                var line = result.Lines[i];
-                if (string.IsNullOrEmpty(line.Text))
-                    continue;
+                var lrcTimeTags = lrcLyric.TimeTags.Select(convertTimeTag).ToList();
+                var lrcRubies = lrcLyric.RubyTags.Select(convertRubyTag).ToArray();
 
-                try
+                double? startTime = lrcTimeTags.Select(x => x.Time).Min();
+                double? endTime = lrcTimeTags.Select(x => x.Time).Max();
+                double? duration = endTime - startTime;
+
+                var lyric = new Lyric
                 {
-                    // todo : check list ls sorted by time.
-                    var lrcTimeTag = line.TimeTags;
-                    var timeTags = line.TimeTags.Where(x => x.Check).ToDictionary(k =>
-                    {
-                        int index = (Array.IndexOf(lrcTimeTag, k) - 1) / 2;
-                        var state = (Array.IndexOf(lrcTimeTag, k) - 1) % 2 == 0 ? TextIndex.IndexState.Start : TextIndex.IndexState.End;
-                        return new TextIndex(index, state);
-                    }, v => (double)v.Time);
-
-                    double startTime = timeTags.FirstOrDefault(x => x.Value > 0).Value;
-                    double duration = timeTags.LastOrDefault(x => x.Value > 0).Value - startTime;
-
-                    var lyric = new Lyric
-                    {
-                        ID = output.HitObjects.Count, // id is star from zero.
-                        Order = output.HitObjects.Count + 1, // should create default order.
-                        Text = line.Text,
-                        // Start time and end time should be re-assigned
-                        StartTime = startTime,
-                        Duration = duration,
-                        TimeTags = ToTimeTagList(timeTags),
-                        RubyTags = result.QueryRubies(line.Text).Select(ruby => new RubyTag
-                        {
-                            Text = ruby.Ruby.Ruby,
-                            StartIndex = ruby.StartIndex,
-                            EndIndex = ruby.EndIndex
-                        }).ToArray()
-                    };
-                    lyric.InitialWorkingTime();
-                    output.HitObjects.Add(lyric);
-                }
-                catch (Exception ex)
-                {
-                    string message = $"Parsing lyric '{line.Text}' got error in line:{i}" +
-                                     "Please check time tag should be ordered and not duplicated." +
-                                     "Then re-import again.";
-                    throw new FormatException(message, ex);
-                }
+                    ID = output.HitObjects.Count, // id is star from zero.
+                    Order = output.HitObjects.Count + 1, // should create default order.
+                    Text = lrcLyric.Text,
+                    // Start time and end time should be re-assigned
+                    StartTime = startTime ?? 0,
+                    Duration = duration ?? 0,
+                    TimeTags = lrcTimeTags,
+                    RubyTags = lrcRubies
+                };
+                lyric.InitialWorkingTime();
+                output.HitObjects.Add(lyric);
             }
-        }
 
-        /// <summary>
-        /// Convert dictionary to list of time tags.
-        /// </summary>
-        /// <param name="dictionary">Dictionary.</param>
-        /// <returns>Time tags</returns>
-        internal static TimeTag[] ToTimeTagList(IReadOnlyDictionary<TextIndex, double> dictionary)
-        {
-            return dictionary.Select(d => new TimeTag(d.Key, d.Value)).ToArray();
+            static TimeTag convertTimeTag(KeyValuePair<LrcParser.Model.TextIndex, int?> timeTag)
+                => new(convertTextIndex(timeTag.Key), timeTag.Value);
+
+            static TextIndex convertTextIndex(LrcParser.Model.TextIndex textIndex)
+            {
+                int index = textIndex.Index;
+                var state = textIndex.State == IndexState.Start ? TextIndex.IndexState.Start : TextIndex.IndexState.End;
+
+                return new TextIndex(index, state);
+            }
+
+            static RubyTag convertRubyTag(LrcParser.Model.RubyTag rubyTag)
+                => new()
+                {
+                    Text = rubyTag.Text,
+                    StartIndex = rubyTag.StartIndex,
+                    EndIndex = rubyTag.EndIndex
+                };
         }
     }
 }
