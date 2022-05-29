cb6551ba152d9e6d269eeaf02492c140b6d05d83
andy840119 <andy840119@gmail.com>
2022-05-29 23:30:31 +0800

    Separate the config and add the comment to each config.

--- osu.Game.Rulesets.Karaoke/Edit/Generator/TimeTags/Ja/JaTimeTagGeneratorConfig.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Generator/TimeTags/Ja/JaTimeTagGeneratorConfig.cs
@@ -2,13 +2,38 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Game.Rulesets.Karaoke.Edit.Generator.Types;
+using osu.Game.Rulesets.Karaoke.Objects;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.Generator.TimeTags.Ja
 {
     public class JaTimeTagGeneratorConfig : TimeTagGeneratorConfig, IHasConfig<JaTimeTagGeneratorConfig>
     {
+        /// <summary>
+        /// Add the <see cref="TimeTag"/> if spacing is next of the alphabet.
+        /// This feature will work only if enable the <see cref="TimeTagGeneratorConfig.CheckWhiteSpace"/>
+        /// </summary>
+        public bool CheckWhiteSpaceAlphabet { get; set; }
+
+        /// <summary>
+        /// Add the <see cref="TimeTag"/> if spacing is next of the digit.
+        /// This feature will work only if enable the <see cref="TimeTagGeneratorConfig.CheckWhiteSpace"/>
+        /// </summary>
+        public bool CheckWhiteSpaceDigit { get; set; }
+
+        /// <summary>
+        /// Add the <see cref="TimeTag"/> if spacing is next of the symbol.
+        /// This feature will work only if enable the <see cref="TimeTagGeneratorConfig.CheckWhiteSpace"/>
+        /// </summary>
+        public bool CheckWhiteSpaceAsciiSymbol { get; set; }
+
+        /// <summary>
+        /// Add the <see cref="TimeTag"/> if character is "ん"
+        /// </summary>
         public bool Checkん { get; set; }
 
+        /// <summary>
+        /// Add the <see cref="TimeTag"/> if character is "っ"
+        /// </summary>
         public bool Checkっ { get; set; }
 
         public JaTimeTagGeneratorConfig CreateDefaultConfig() =>
--- osu.Game.Rulesets.Karaoke/Edit/Generator/TimeTags/TimeTagGeneratorConfig.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Generator/TimeTags/TimeTagGeneratorConfig.cs
@@ -1,22 +1,31 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Game.Rulesets.Karaoke.Objects;
+
 namespace osu.Game.Rulesets.Karaoke.Edit.Generator.TimeTags
 {
     public abstract class TimeTagGeneratorConfig
     {
+        /// <summary>
+        /// Will create a <see cref="TimeTag"/> at the first of the lyric if only contains spacing in the <see cref="Lyric"/>.
+        /// </summary>
         public bool CheckBlankLine { get; set; }
 
+        /// <summary>
+        /// Add end <see cref="TimeTag"/> at the end of the <see cref="Lyric"/>.
+        /// </summary>
         public bool CheckLineEndKeyUp { get; set; }
 
+        /// <summary>
+        /// Will add the <see cref="TimeTag"/> if meet the spacing.
+        /// </summary>
         public bool CheckWhiteSpace { get; set; }
 
+        /// <summary>
+        /// Add the end <see cref="TimeTag"/> instead.
+        /// This feature will work only if enable the <see cref="CheckWhiteSpace"/>.
+        /// </summary>
         public bool CheckWhiteSpaceKeyUp { get; set; }
-
-        public bool CheckWhiteSpaceAlphabet { get; set; }
-
-        public bool CheckWhiteSpaceDigit { get; set; }
-
-        public bool CheckWhiteSpaceAsciiSymbol { get; set; }
     }
 }
