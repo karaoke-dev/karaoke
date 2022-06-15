2e3b0c01a6ac64dc30a9e4d87d0a7961c79dc58b
andy840119 <andy840119@gmail.com>
2022-06-15 20:18:40 +0800

    Add methods for able to set-up and check the property to the beatmap.

--- osu.Game.Rulesets.Karaoke.Tests/Editor/ChangeHandlers/BaseChangeHandlerTest.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Editor/ChangeHandlers/BaseChangeHandlerTest.cs
@@ -43,6 +43,26 @@ namespace osu.Game.Rulesets.Karaoke.Tests.Editor.ChangeHandlers
             Child = changeHandler = new TChangeHandler();
         }
 
+        protected void SetUpEditorBeatmap(Action<EditorBeatmap> action)
+        {
+            AddStep("Prepare testing beatmap", () =>
+            {
+                var editorBeatmap = Dependencies.Get<EditorBeatmap>();
+                action?.Invoke(editorBeatmap);
+            });
+        }
+
+        protected void SetUpKaraokeBeatmap(Action<KaraokeBeatmap> assert)
+        {
+            SetUpEditorBeatmap(editorBeatmap =>
+            {
+                if (editorBeatmap.PlayableBeatmap is not KaraokeBeatmap karaokeBeatmap)
+                    throw new InvalidCastException();
+
+                assert?.Invoke(karaokeBeatmap);
+            });
+        }
+
         protected void TriggerHandlerChanged(Action<TChangeHandler> c)
         {
             AddStep("Trigger change handler", () =>
@@ -50,6 +70,31 @@ namespace osu.Game.Rulesets.Karaoke.Tests.Editor.ChangeHandlers
                 // should reset transaction number in here because it will increase if load testing object.
                 transactionCount = 0;
                 c(changeHandler);
+            });
+        }
+
+        protected void AssertEditorBeatmap(Action<EditorBeatmap> assert)
+        {
+            AddStep("Is result matched", () =>
+            {
+                var editorBeatmap = Dependencies.Get<EditorBeatmap>();
+                assert(editorBeatmap);
+            });
+
+            // even if there's no property changed in the lyric editor, should still trigger the change handler.
+            // because every change handler call should cause one undo step.
+            // also, technically should not call the change handler if there's no possible to change the properties.
+            AssertTransactionOnlyTriggerOnce();
+        }
+
+        protected void AssertKaraokeBeatmap(Action<KaraokeBeatmap> assert)
+        {
+            AssertEditorBeatmap(editorBeatmap =>
+            {
+                if (editorBeatmap.PlayableBeatmap is not KaraokeBeatmap karaokeBeatmap)
+                    throw new InvalidCastException();
+
+                assert?.Invoke(karaokeBeatmap);
             });
         }
 
