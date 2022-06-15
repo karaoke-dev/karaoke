ad21122710c77f22ae976771575d0ba365748bab
andy840119 <andy840119@gmail.com>
2022-06-15 20:39:45 +0800

    Rename this extension for now. Guess will be removed eventually.

--- osu.Game.Rulesets.Karaoke/Beatmaps/KaraokeBeatmapExtension.cs
+++ osu.Game.Rulesets.Karaoke/Beatmaps/KaraokeBeatmapExtension.cs
@@ -1,18 +1,24 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Karaoke.Beatmaps.Metadatas;
-using osu.Game.Rulesets.Karaoke.Objects;
 
 namespace osu.Game.Rulesets.Karaoke.Beatmaps
 {
     public static class KaraokeBeatmapExtension
     {
-        public static bool IsScorable(this IBeatmap beatmap) => beatmap?.HitObjects.OfType<Note>().Any(x => x.Display) ?? false;
+        public static bool IsScorable(this IBeatmap beatmap)
+        {
+            if (beatmap is not KaraokeBeatmap karaokeBeatmap)
+                throw new InvalidCastException();
+
+            return karaokeBeatmap.Scorable;
+        }
 
         public static List<CultureInfo> AvailableTranslates(this IBeatmap beatmap) => (beatmap as KaraokeBeatmap)?.AvailableTranslates ?? new List<CultureInfo>();
 
