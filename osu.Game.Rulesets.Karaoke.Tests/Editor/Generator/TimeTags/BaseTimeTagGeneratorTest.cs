95bb32fc4de40ac28f636e707e6582b75fcb507c
andy840119 <andy840119@gmail.com>
2022-06-22 21:30:14 +0800

    Automated #nullable processing in the test project.

--- osu.Game.Rulesets.Karaoke.Tests/Editor/Generator/TimeTags/BaseTimeTagGeneratorTest.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Editor/Generator/TimeTags/BaseTimeTagGeneratorTest.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+#nullable disable
+
 using osu.Game.Rulesets.Karaoke.Edit.Generator.TimeTags;
 using osu.Game.Rulesets.Karaoke.Objects;
 using osu.Game.Rulesets.Karaoke.Tests.Asserts;
