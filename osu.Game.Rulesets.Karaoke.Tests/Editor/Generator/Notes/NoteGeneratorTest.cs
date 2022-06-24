4cfcecb645847da72c3fb3b701bb8c5a3dcef1cd
andy840119 <andy840119@gmail.com>
2022-06-24 22:54:46 +0800

    Remove the disable null flag in the test project.

--- osu.Game.Rulesets.Karaoke.Tests/Editor/Generator/Notes/NoteGeneratorTest.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Editor/Generator/Notes/NoteGeneratorTest.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable disable
-
 using System.Linq;
 using NUnit.Framework;
 using osu.Game.Rulesets.Karaoke.Edit.Generator.Notes;
--- osu.Game.Rulesets.Karaoke.Tests/Editor/Generator/RomajiTags/RomajiTagGeneratorSelectorTest.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Editor/Generator/RomajiTags/RomajiTagGeneratorSelectorTest.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable disable
-
 using System.Globalization;
 using NUnit.Framework;
 using osu.Game.Rulesets.Karaoke.Edit.Generator.RomajiTags;
