d8be8cf6c796e60ac2b1d2b520d56180a5517867
andy840119 <andy840119@gmail.com>
2022-06-25 16:19:25 +0800

    Remove all nullable disable flag.

--- osu.Game.Rulesets.Karaoke.Tests/Utils/FontUsageUtilsTest.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Utils/FontUsageUtilsTest.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable disable
-
 using NUnit.Framework;
 using osu.Framework.Graphics.Sprites;
 using osu.Game.Rulesets.Karaoke.Skinning.Fonts;
