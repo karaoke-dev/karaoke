d2650cf9a6fea7a555ba8f65e4c7df36b7f74394
andy840119 <andy840119@gmail.com>
2022-06-26 11:06:33 +0800

    Remove the nullable disable flag in the skin elements.

--- osu.Game.Rulesets.Karaoke/Skinning/Groups/GroupByLyricIds.cs
+++ osu.Game.Rulesets.Karaoke/Skinning/Groups/GroupByLyricIds.cs
@@ -1,8 +1,6 @@
 // Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable disable
-
 using System.Collections.Generic;
 using System.Linq;
 using osu.Game.Rulesets.Karaoke.Objects;
