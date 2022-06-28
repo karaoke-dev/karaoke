0dc7c3a414abccfba3f8202939b20e957a4dfe3d
andy840119 <andy840119@gmail.com>
2022-06-28 20:09:07 +0800

    Remove the nullable disable flag in the change handler mode state.

--- osu.Game.Rulesets.Karaoke/Edit/Lyrics/States/Modes/ManageModeState.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/States/Modes/ManageModeState.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable disable
-
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends.Manage;
