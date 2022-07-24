b9b12995eb78eae09a13a3e0cf937924d383552a
andy840119 <andy840119@gmail.com>
2022-07-24 10:12:04 +0800

    Implement the change handler and its test case.

---
+++ osu.Game.Rulesets.Karaoke.Tests/Editor/ChangeHandlers/Lyrics/LyricReferenceChangeHandlerTest.cs
@@ -0,0 +1,35 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers.Lyrics;
+using osu.Game.Rulesets.Karaoke.Objects;
+
+namespace osu.Game.Rulesets.Karaoke.Tests.Editor.ChangeHandlers.Lyrics
+{
+    public class LyricReferenceChangeHandlerTest : BaseHitObjectChangeHandlerTest<LyricReferenceChangeHandler, Lyric>
+    {
+        [Test]
+        public void TestSetLanguageToJapanese()
+        {
+            var lyric = new Lyric
+            {
+                Text = "Referenced lyric"
+            };
+
+            PrepareHitObject(lyric, false);
+
+            PrepareHitObject(new Lyric
+            {
+                Text = "I need the reference lyric."
+            });
+
+            TriggerHandlerChanged(c => c.UpdateReferenceLyric(lyric));
+
+            AssertSelectedHitObject(h =>
+            {
+                Assert.AreEqual(lyric, h.ReferenceLyric);
+            });
+        }
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/Lyrics/ILyricReferenceChangeHandler.cs
@@ -0,0 +1,12 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Karaoke.Objects;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers.Lyrics
+{
+    public interface ILyricReferenceChangeHandler
+    {
+        void UpdateReferenceLyric(Lyric? referenceLyric);
+    }
+}
