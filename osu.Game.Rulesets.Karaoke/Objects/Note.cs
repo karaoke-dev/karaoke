0ccf1b9718d50139c6f259dc8191100dee8b9040
andy840119 <andy840119@gmail.com>
2022-08-06 14:08:08 +0800

    Seems there's no need to add those property.

--- osu.Game.Rulesets.Karaoke/Objects/Note.cs
+++ osu.Game.Rulesets.Karaoke/Objects/Note.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.ComponentModel;
 using Newtonsoft.Json;
 using osu.Framework.Bindables;
 using osu.Game.IO.Serialization;
@@ -102,14 +101,12 @@ namespace osu.Game.Rulesets.Karaoke.Objects
         /// <summary>
         /// Offset time relative to the start time.
         /// </summary>
-        [DefaultValue(0)]
         public double StartTimeOffset { get; set; }
 
         /// <summary>
         /// Offset time relative to the end time.
         /// Negative value means the adjusted time is smaller than actual.
         /// </summary>
-        [DefaultValue(0)]
         public double EndTimeOffset { get; set; }
 
         public int StartIndex { get; set; }
