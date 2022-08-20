38141b39cf0d435c24ba32d0cb24ab22bd7f3116
andy840119 <andy840119@gmail.com>
2022-08-20 23:54:03 +0800

    Use the `LyricPropertySection` in the `SingerEditSection`.

--- osu.Game.Rulesets.Karaoke/Edit/Lyrics/Extends/Singers/SingerEditSection.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Extends/Singers/SingerEditSection.cs
@@ -14,44 +14,24 @@ using osu.Game.Graphics.UserInterfaceV2;
 using osu.Game.Rulesets.Karaoke.Beatmaps.Metadatas;
 using osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers.Lyrics;
 using osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers.Singers;
-using osu.Game.Rulesets.Karaoke.Edit.Components.Containers;
-using osu.Game.Rulesets.Karaoke.Edit.Lyrics.CaretPosition;
-using osu.Game.Rulesets.Karaoke.Edit.Lyrics.States;
 using osu.Game.Rulesets.Karaoke.Graphics.Cursor;
 using osu.Game.Rulesets.Karaoke.Graphics.Sprites;
+using osu.Game.Rulesets.Karaoke.Objects;
 using osuTK;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends.Singers
 {
-    public class SingerEditSection : Section
+    public class SingerEditSection : LyricPropertySection
     {
-        private readonly IBindable<ICaretPosition> bindableCaretPosition = new Bindable<ICaretPosition>();
         private readonly IBindableList<Singer> bindableSingers = new BindableList<Singer>();
         private readonly IBindableList<int> singerIndexes = new BindableList<int>();
         protected override LocalisableString Title => "Singer";
-
-        [Resolved]
-        private ISingersChangeHandler singersChangeHandler { get; set; }
 
         [Resolved]
         private ILyricSingerChangeHandler lyricSingerChangeHandler { get; set; }
 
         public SingerEditSection()
         {
-            // update singer selections from lyric.
-            bindableCaretPosition.BindValueChanged(e =>
-            {
-                singerIndexes.UnbindBindings();
-
-                var lyric = e.NewValue?.Lyric;
-                if (lyric == null)
-                    return;
-
-                // should bind from lyric.
-                // singer index might be able to change from other place like singer editor.
-                singerIndexes.BindTo(lyric.SingersBindable);
-            });
-
             // update singer list.
             bindableSingers.BindCollectionChanged((_, _) =>
             {
@@ -91,11 +71,22 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends.Singers
         }
 
         [BackgroundDependencyLoader]
-        private void load(ILyricCaretState lyricCaretState)
+        private void load(ISingersChangeHandler singersChangeHandler)
         {
             // update singer
             bindableSingers.BindTo(singersChangeHandler.Singers);
-            bindableCaretPosition.BindTo(lyricCaretState.BindableCaretPosition);
+        }
+
+        protected override void OnLyricChanged(Lyric lyric)
+        {
+            singerIndexes.UnbindBindings();
+
+            if (lyric == null)
+                return;
+
+            // should bind from lyric.
+            // singer index might be able to change from other place like singer editor.
+            singerIndexes.BindTo(lyric.SingersBindable);
         }
 
         public class LabelledSingerSwitchButton : LabelledSwitchButton, IHasCustomTooltip<Singer>
