9bd684779b55772858abcf9f324eacaa9cdea80b
andy840119 <andy840119@gmail.com>
2022-08-29 20:26:24 +0800

    Should be able to get content change in the config.

--- osu.Game.Rulesets.Karaoke/Objects/Properties/ReferenceLyricConfig.cs
+++ osu.Game.Rulesets.Karaoke/Objects/Properties/ReferenceLyricConfig.cs
@@ -1,6 +1,7 @@
 // Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using Newtonsoft.Json;
 using osu.Framework.Bindables;
 
@@ -8,6 +9,13 @@ namespace osu.Game.Rulesets.Karaoke.Objects.Properties
 {
     public class ReferenceLyricConfig : IReferenceLyricPropertyConfig
     {
+        public event Action? Changed;
+
+        public ReferenceLyricConfig()
+        {
+            OffsetTimeBindable.ValueChanged += _ => Changed?.Invoke();
+        }
+
         [JsonIgnore]
         public readonly Bindable<double> OffsetTimeBindable = new BindableDouble();
 
