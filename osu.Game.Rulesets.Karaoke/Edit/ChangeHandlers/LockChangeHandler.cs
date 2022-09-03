8c537e8c185f20e71ee7227d1cd7d7fcbfd0bb4c
andy840119 <andy840119@gmail.com>
2022-09-03 18:59:00 +0800

    Lock change handler should inherit the hit object property change handler because this change handler is focus on lock the hit-object,

--- osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/LockChangeHandler.cs
+++ osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/LockChangeHandler.cs
@@ -1,12 +1,13 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using osu.Game.Rulesets.Karaoke.Objects;
 using osu.Game.Rulesets.Karaoke.Objects.Types;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers
 {
-    public class LockChangeHandler : HitObjectChangeHandler<KaraokeHitObject>, ILockChangeHandler
+    public class LockChangeHandler : HitObjectPropertyChangeHandler<KaraokeHitObject>, ILockChangeHandler
     {
         public void Lock(LockState lockState)
         {
@@ -20,6 +21,17 @@ namespace osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers
         public void Unlock()
         {
             Lock(LockState.None);
+        }
+
+        protected override bool IsWritePropertyLocked(KaraokeHitObject hitObject)
+        {
+            // todo: implement.
+            return hitObject switch
+            {
+                Lyric => false,
+                Note => false,
+                _ => throw new NotSupportedException()
+            };
         }
     }
 }
