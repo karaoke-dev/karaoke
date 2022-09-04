d15a0f567b71a2af108691437ddf46e8677ec7f6
andy840119 <andy840119@gmail.com>
2022-09-05 00:00:36 +0800

    Implement the base class for able to check the forbidden change in the hit-object property related change handler.

--- osu.Game.Rulesets.Karaoke.Tests/Editor/ChangeHandlers/Lyrics/LyricPropertyChangeHandlerTest.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Editor/ChangeHandlers/Lyrics/LyricPropertyChangeHandlerTest.cs
@@ -1,14 +1,13 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers.Lyrics;
 using osu.Game.Rulesets.Karaoke.Objects;
 using osu.Game.Rulesets.Karaoke.Objects.Properties;
 
 namespace osu.Game.Rulesets.Karaoke.Tests.Editor.ChangeHandlers.Lyrics
 {
-    public abstract class LyricPropertyChangeHandlerTest<TChangeHandler> : BaseHitObjectChangeHandlerTest<TChangeHandler, Lyric>
+    public abstract class LyricPropertyChangeHandlerTest<TChangeHandler> : BaseHitObjectPropertyChangeHandlerTest<TChangeHandler, Lyric>
         where TChangeHandler : LyricPropertyChangeHandler, new()
     {
         protected Lyric PrepareLyricWithSyncConfig(Lyric referencedLyric, IReferenceLyricPropertyConfig? config = null, bool selected = true)
@@ -22,11 +21,6 @@ namespace osu.Game.Rulesets.Karaoke.Tests.Editor.ChangeHandlers.Lyrics
             PrepareHitObjects(new[] { lyric }, selected);
 
             return lyric;
-        }
-
-        protected void TriggerHandlerChangedWithChangeForbiddenException(Action<TChangeHandler> c)
-        {
-            TriggerHandlerChangedWithException<LyricPropertyChangeHandler.ChangeForbiddenException>(c);
         }
     }
 }
