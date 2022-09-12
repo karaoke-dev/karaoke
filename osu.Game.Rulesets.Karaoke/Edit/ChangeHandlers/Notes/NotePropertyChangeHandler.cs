7bea3eef063a8b532d9e594302a287b9cee46555
andy840119 <andy840119@gmail.com>
2022-09-12 22:30:19 +0800

    Remove un-need spacing.

--- osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/Notes/NotePropertyChangeHandler.cs
+++ osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/Notes/NotePropertyChangeHandler.cs
@@ -42,6 +42,6 @@ namespace osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers.Notes
         }
 
         protected override bool IsWritePropertyLocked(Note note)
-            => HitObjectWritableUtils.IsWriteNotePropertyLocked(note, nameof(Note.Text) , nameof(Note.RubyText) , nameof(Note.Display));
+            => HitObjectWritableUtils.IsWriteNotePropertyLocked(note, nameof(Note.Text), nameof(Note.RubyText), nameof(Note.Display));
     }
 }
