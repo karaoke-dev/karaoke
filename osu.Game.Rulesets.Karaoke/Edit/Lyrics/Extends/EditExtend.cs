afd6e6bc5bd67c8b3478951a6f88a8da05e55906
andy840119 <andy840119@gmail.com>
2022-09-18 13:42:03 +0800

    Inherit the common base class.

--- osu.Game.Rulesets.Karaoke/Edit/Lyrics/Extends/EditExtend.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Extends/EditExtend.cs
@@ -1,53 +1,44 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable disable
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
-using osu.Game.Graphics.Containers;
+using osu.Framework.Testing;
+using osu.Game.Screens.Edit;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Extends
 {
-    public abstract class EditExtend : Container
+    public abstract class EditExtend : EditorRoundedScreenSettings
     {
         public abstract ExtendDirection Direction { get; }
 
         public abstract float ExtendWidth { get; }
 
-        protected override Container<Drawable> Content => content;
+        protected override IReadOnlyList<Drawable> CreateSections() => Array.Empty<Drawable>();
 
-        private readonly Box background;
-        private readonly FillFlowContainer content;
-
-        protected EditExtend()
+        protected IReadOnlyList<Drawable> Children
         {
-            RelativeSizeAxes = Axes.Both;
-            InternalChildren = new Drawable[]
+            get => this.ChildrenOfType<FillFlowContainer>().First().Children;
+            set
             {
-                background = new Box
+                // should delay assign the children after loaded.
+                Schedule(() =>
                 {
-                    Depth = float.MaxValue,
-                    RelativeSizeAxes = Axes.Both,
-                },
-                new OsuScrollContainer
-                {
-                    RelativeSizeAxes = Axes.Both,
-                    Child = content = new FillFlowContainer
-                    {
-                        RelativeSizeAxes = Axes.X,
-                        AutoSizeAxes = Axes.Y,
-                    }
-                }
-            };
+                    this.ChildrenOfType<FillFlowContainer>().First().Children = value;
+                });
+            }
         }
 
         [BackgroundDependencyLoader]
         private void load(ILyricEditorState state, LyricEditorColourProvider colourProvider)
         {
-            background.Colour = colourProvider.Background3(state.Mode);
+            // change the background colour to the lighter one.
+            this.ChildrenOfType<Box>().First().Colour = colourProvider.Background3(state.Mode);
         }
     }
 }
