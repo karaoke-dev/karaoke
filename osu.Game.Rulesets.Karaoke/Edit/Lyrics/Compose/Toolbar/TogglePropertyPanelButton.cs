40ed2dc1480cb2c402d10b8c315220e18034b7b6
andy840119 <andy840119@gmail.com>
2022-09-27 22:32:16 +0800

    Implement base toolbar and some buttons.

---
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Compose/Toolbar/TogglePropertyPanelButton.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Rulesets.Karaoke.Configuration;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Compose.Toolbar
+{
+    public class TogglePropertyPanelButton : LyricEditorConfigButton
+    {
+        protected override KaraokeRulesetLyricEditorSetting Setting => KaraokeRulesetLyricEditorSetting.ShowPropertyPanelInComposer;
+
+        protected override IconUsage Icon => FontAwesome.Solid.FileImage;
+    }
+}
