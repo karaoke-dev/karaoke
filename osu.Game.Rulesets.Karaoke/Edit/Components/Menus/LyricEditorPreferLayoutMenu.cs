b42f3951e8e35b8c526d8d16d632f1830801b29f
andy840119 <andy840119@gmail.com>
2022-09-28 23:36:50 +0800

    Add options for able to switc the layout by the view mode.

---
+++ osu.Game.Rulesets.Karaoke/Edit/Components/Menus/LyricEditorPreferLayoutMenu.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Karaoke.Configuration;
+using osu.Game.Rulesets.Karaoke.Edit.Lyrics;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Components.Menus
+{
+    public class LyricEditorPreferLayoutMenu : EnumMenu<LyricEditorLayout>
+    {
+        public LyricEditorPreferLayoutMenu(KaraokeRulesetLyricEditorConfigManager config, string text)
+            : base(config.GetBindable<LyricEditorLayout>(KaraokeRulesetLyricEditorSetting.LyricEditorPreferLayout), text)
+        {
+        }
+    }
+}
