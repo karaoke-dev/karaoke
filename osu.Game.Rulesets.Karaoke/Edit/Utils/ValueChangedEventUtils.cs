9568b39bcb727c57edae62f5db0f3fe44d3e6631
andy840119 <andy840119@gmail.com>
2022-10-01 17:29:46 +0800

    Add the utils to check if lyric changed. Note that will return false if change the caret position type but the position is the same.

---
+++ osu.Game.Rulesets.Karaoke/Edit/Utils/ValueChangedEventUtils.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Bindables;
+using osu.Game.Rulesets.Karaoke.Edit.Lyrics.CaretPosition;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Utils
+{
+    public static class ValueChangedEventUtils
+    {
+        public static bool LyricChanged(ValueChangedEvent<ICaretPosition?> e)
+        {
+            var oldLyric = e.OldValue?.Lyric;
+            var newLyric = e.NewValue?.Lyric;
+
+            return oldLyric != newLyric;
+        }
+    }
+}
