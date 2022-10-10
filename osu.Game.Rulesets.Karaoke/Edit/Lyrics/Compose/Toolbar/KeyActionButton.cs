0b3427a2e3bf82dddd4a39332cb6d57fa8c02005
andy840119 <andy840119@gmail.com>
2022-10-10 21:50:19 +0800

    Implement the key action button. Will make the highlight if target action has been triggered.

---
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Compose/Toolbar/KeyActionButton.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+using osu.Framework.Input.Bindings;
+using osu.Framework.Input.Events;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Compose.Toolbar
+{
+    public abstract class KeyActionButton : ToolbarButton, IKeyBindingHandler<KaraokeEditAction>, IHasIKeyBindingHandlerOrder
+    {
+        protected abstract KaraokeEditAction EditAction { get; }
+
+        public int KeyBindingHandlerOrder => int.MinValue;
+
+        public bool OnPressed(KeyBindingPressEvent<KaraokeEditAction> e)
+        {
+            if (e.Action != EditAction)
+                return false;
+
+            IconContainer.FadeOut(100).Then().FadeIn();
+
+            return false;
+        }
+
+        public void OnReleased(KeyBindingReleaseEvent<KaraokeEditAction> e)
+        {
+        }
+    }
+}
