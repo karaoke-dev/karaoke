c004c841cc14ab5c7f660ea381a036035d6c3dd9
andy840119 <andy840119@gmail.com>
2022-10-10 10:36:59 +0800

    Add some skin transformers.

---
+++ osu.Game.Rulesets.Karaoke/Skinning/Default/KaraokeDefaultSkinTransformer.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Bindables;
+using osu.Game.Beatmaps;
+using osu.Game.Skinning;
+
+namespace osu.Game.Rulesets.Karaoke.Skinning.Default
+{
+    public class KaraokeDefaultSkinTransformer : SkinTransformer
+    {
+        private readonly KaraokeSkin karaokeSkin;
+
+        public KaraokeDefaultSkinTransformer(ISkin skin, IBeatmap beatmap)
+            : base(skin)
+        {
+            karaokeSkin = new KaraokeSkin(new SkinInfo(), new InternalSkinStorageResourceProvider("Default"));
+        }
+
+        public override IBindable<TValue>? GetConfig<TLookup, TValue>(TLookup lookup)
+            => karaokeSkin.GetConfig<TLookup, TValue>(lookup);
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/Skinning/Triangles/KaraokeTrianglesSkinTransformer.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets.Karaoke.Skinning.Default;
+using osu.Game.Skinning;
+
+namespace osu.Game.Rulesets.Karaoke.Skinning.Triangles
+{
+    public class KaraokeTrianglesSkinTransformer : KaraokeDefaultSkinTransformer
+    {
+        public KaraokeTrianglesSkinTransformer(ISkin skin, IBeatmap beatmap)
+            : base(skin, beatmap)
+        {
+        }
+    }
+}
