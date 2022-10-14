cdb6d7e84698f4b2bd74f73a3cd35f62df10159b
andy840119 <andy840119@gmail.com>
2022-10-14 20:20:31 +0800

    Implement the base class and interface for the caret that contains index in the lyric.

---
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/CaretPosition/Algorithms/IndexCaretPositionAlgorithm.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Karaoke.Objects;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.CaretPosition.Algorithms
+{
+    public abstract class IndexCaretPositionAlgorithm<TCaretPosition> : CaretPositionAlgorithm<TCaretPosition>, IIndexCaretPositionAlgorithm
+        where TCaretPosition : struct, IIndexCaretPosition
+    {
+        protected IndexCaretPositionAlgorithm(Lyric[] lyrics)
+            : base(lyrics)
+        {
+        }
+    }
+}
