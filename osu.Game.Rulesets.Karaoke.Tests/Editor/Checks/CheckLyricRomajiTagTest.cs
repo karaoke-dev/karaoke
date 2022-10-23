b69a1b1a722317c4a9b57d1be314e717d4ba838d
andy840119 <andy840119@gmail.com>
2022-10-24 00:45:28 +0800

    Implement the check for ruby and romaji.

---
+++ osu.Game.Rulesets.Karaoke.Tests/Editor/Checks/CheckLyricRomajiTagTest.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Game.Rulesets.Karaoke.Edit.Checks;
+using osu.Game.Rulesets.Karaoke.Objects;
+using osu.Game.Rulesets.Karaoke.Tests.Helper;
+using static osu.Game.Rulesets.Karaoke.Edit.Checks.CheckLyricRomajiTag;
+
+namespace osu.Game.Rulesets.Karaoke.Tests.Editor.Checks
+{
+    public class CheckLyricRomajiTagTest : HitObjectCheckTest<Lyric, CheckLyricRomajiTag>
+    {
+        [TestCase("karaoke", new[] { "[0,2]:ka", "[2,4]:ra", "[4,5]:o", "[5,7]:ke" })]
+        [TestCase("karaoke", new[] { "[0,7]:karaoke" })]
+        public void TestCheck(string text, string[] romajies)
+        {
+            var lyric = new Lyric
+            {
+                Text = text,
+                RomajiTags = TestCaseTagHelper.ParseRomajiTags(romajies)
+            };
+
+            AssertOk(lyric);
+        }
+
+        [TestCase("karaoke", new[] { "[-1,2]:ka" })]
+        [TestCase("karaoke", new[] { "[7,8]:ke" })]
+        public void TestCheckOutOfRange(string text, string[] romajies)
+        {
+            var lyric = new Lyric
+            {
+                Text = text,
+                RomajiTags = TestCaseTagHelper.ParseRomajiTags(romajies)
+            };
+
+            AssertNotOk<IssueTemplateLyricRomajiOutOfRange>(lyric);
+        }
+
+        [TestCase("karaoke", new[] { "[0,2]:ka", "[1,3]:ra" })]
+        [TestCase("karaoke", new[] { "[0,3]:ka", "[1,2]:ra" })]
+        public void TestCheckOverlapping(string text, string[] romajies)
+        {
+            var lyric = new Lyric
+            {
+                Text = text,
+                RomajiTags = TestCaseTagHelper.ParseRomajiTags(romajies)
+            };
+
+            AssertNotOk<IssueTemplateLyricRomajiOverlapping>(lyric);
+        }
+
+        [TestCase("karaoke", new[] { "[0,3]:" })]
+        public void TestCheckEmptyText(string text, string[] romajies)
+        {
+            var lyric = new Lyric
+            {
+                Text = text,
+                RomajiTags = TestCaseTagHelper.ParseRomajiTags(romajies)
+            };
+
+            AssertNotOk<IssueTemplateLyricRomajiEmptyText>(lyric);
+        }
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/Edit/Checks/CheckLyricRubyTag.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using osu.Game.Rulesets.Edit.Checks.Components;
+using osu.Game.Rulesets.Karaoke.Objects;
+using osu.Game.Rulesets.Karaoke.Utils;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Checks
+{
+    public class CheckLyricRubyTag : CheckLyricTextTag<RubyTag>
+    {
+        protected override string Description => "Lyric with invalid ruby tag.";
+
+        public override IEnumerable<IssueTemplate> PossibleTemplates => new IssueTemplate[]
+        {
+            new IssueTemplateLyricRubyOutOfRange(this),
+            new IssueTemplateLyricRubyOverlapping(this),
+            new IssueTemplateLyricRubyEmptyText(this),
+        };
+
+        protected override IList<RubyTag> GetTextTag(Lyric lyric)
+            => lyric.RubyTags;
+
+        protected override TextTagsUtils.Sorting Sorting => TextTagsUtils.Sorting.Asc;
+
+        protected override Issue GetOutOfRangeIssue(Lyric lyric, RubyTag textTag)
+            => new IssueTemplateLyricRubyOutOfRange(this).Create(lyric, textTag);
+
+        protected override Issue GetOverlappingIssue(Lyric lyric, RubyTag textTag)
+            => new IssueTemplateLyricRubyOverlapping(this).Create(lyric, textTag);
+
+        protected override Issue GetEmptyTextIssue(Lyric lyric, RubyTag textTag)
+            => new IssueTemplateLyricRubyEmptyText(this).Create(lyric, textTag);
+
+        public class IssueTemplateLyricRubyOutOfRange : TextTagIssueTemplate
+        {
+            public IssueTemplateLyricRubyOutOfRange(ICheck check)
+                : base(check, IssueType.Problem, "Ruby tag index is out of range.")
+            {
+            }
+        }
+
+        public class IssueTemplateLyricRubyOverlapping : TextTagIssueTemplate
+        {
+            public IssueTemplateLyricRubyOverlapping(ICheck check)
+                : base(check, IssueType.Problem, "Ruby tag index is overlapping to another ruby.")
+            {
+            }
+        }
+
+        public class IssueTemplateLyricRubyEmptyText : TextTagIssueTemplate
+        {
+            public IssueTemplateLyricRubyEmptyText(ICheck check)
+                : base(check, IssueType.Problem, "Ruby tag has no text.")
+            {
+            }
+        }
+    }
+}
