2bd244418924a1ae97761270c8b9936b31c44f58
andy840119 <andy840119@gmail.com>
2022-10-26 00:02:58 +0800

    Implement the check for time-tag's time in the lyric.

---
+++ osu.Game.Rulesets.Karaoke.Tests/Editor/Checks/CheckLyricTimeTagTest.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Game.Rulesets.Karaoke.Edit.Checks;
+using osu.Game.Rulesets.Karaoke.Objects;
+using osu.Game.Rulesets.Karaoke.Tests.Helper;
+using static osu.Game.Rulesets.Karaoke.Edit.Checks.CheckLyricTimeTag;
+
+namespace osu.Game.Rulesets.Karaoke.Tests.Editor.Checks
+{
+    public class CheckLyricTimeTagTest : HitObjectCheckTest<Lyric, CheckLyricTimeTag>
+    {
+        [TestCase("カラオケ", new[] { "[0,start]:1000", "[1,start]:2000", "[2,start]:3000", "[3,start]:4000", "[3,end]:5000" })]
+        [TestCase("カラオケ", new[] { "[0,start]:1000", "[3,end]:5000" })]
+        [TestCase("カラオケ", new[] { "[0,start]:1000", "[3,end]:5000" })]
+        [TestCase("カラオケ", new[] { "[0,start]:1000" })]
+        public void TestCheck(string text, string[] timeTags)
+        {
+            var lyric = new Lyric
+            {
+                Text = text,
+                TimeTags = TestCaseTagHelper.ParseTimeTags(timeTags)
+            };
+
+            AssertOk(lyric);
+        }
+
+        [TestCase("カラオケ", new[] { "[-1,start]:1000" })]
+        [TestCase("カラオケ", new[] { "[4,start]:4000" })]
+        public void TestCheckOutOfRange(string text, string[] timeTags)
+        {
+            var lyric = new Lyric
+            {
+                Text = text,
+                TimeTags = TestCaseTagHelper.ParseTimeTags(timeTags)
+            };
+
+            AssertNotOk<IssueTemplateLyricTimeTagOutOfRange>(lyric);
+        }
+
+        [TestCase("カラオケ", new[] { "[0,start]:5000", "[3,end]:1000" })]
+        public void TestCheckOverlapping(string text, string[] timeTags)
+        {
+            var lyric = new Lyric
+            {
+                Text = text,
+                TimeTags = TestCaseTagHelper.ParseTimeTags(timeTags)
+            };
+
+            AssertNotOk<IssueTemplateLyricTimeTagOverlapping>(lyric);
+        }
+
+        [TestCase("カラオケ", new[] { "[0,start]:" })]
+        public void TestCheckEmptyTime(string text, string[] timeTags)
+        {
+            var lyric = new Lyric
+            {
+                Text = text,
+                TimeTags = TestCaseTagHelper.ParseTimeTags(timeTags)
+            };
+
+            AssertNotOk<IssueTemplateLyricTimeTagEmptyTime>(lyric);
+        }
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/Edit/Checks/CheckLyricTimeTag.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using System.Linq;
+using osu.Game.Rulesets.Edit.Checks.Components;
+using osu.Game.Rulesets.Karaoke.Edit.Checks.Issues;
+using osu.Game.Rulesets.Karaoke.Objects;
+using osu.Game.Rulesets.Karaoke.Utils;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Checks
+{
+    public class CheckLyricTimeTag : CheckHitObjectProperty<Lyric>
+    {
+        protected override string Description => "Lyric with invalid time-tag.";
+
+        public override IEnumerable<IssueTemplate> PossibleTemplates => new IssueTemplate[]
+        {
+            new IssueTemplateLyricTimeTagOutOfRange(this),
+            new IssueTemplateLyricTimeTagOverlapping(this),
+            new IssueTemplateLyricTimeTagEmptyTime(this),
+        };
+
+        public override IEnumerable<Issue> Check(Lyric lyric)
+        {
+            // todo: maybe config?
+            const GroupCheck group_check = GroupCheck.Asc;
+            const SelfCheck self_check = SelfCheck.BasedOnStart;
+
+            var outOfRangeTags = TimeTagsUtils.FindOutOfRange(lyric.TimeTags, lyric.Text);
+            var overlappingTimeTags = TimeTagsUtils.FindOverlapping(lyric.TimeTags, group_check, self_check).ToArray();
+            var noTimeTimeTags = TimeTagsUtils.FindNoneTime(lyric.TimeTags);
+
+            foreach (var textTag in outOfRangeTags)
+            {
+                yield return new IssueTemplateLyricTimeTagOutOfRange(this).Create(lyric, textTag);
+            }
+
+            foreach (var textTag in overlappingTimeTags)
+            {
+                yield return new IssueTemplateLyricTimeTagOverlapping(this).Create(lyric, textTag);
+            }
+
+            foreach (var textTag in noTimeTimeTags)
+            {
+                yield return new IssueTemplateLyricTimeTagEmptyTime(this).Create(lyric, textTag);
+            }
+        }
+
+        public abstract class TimeTagIssueTemplate : IssueTemplate
+        {
+            protected TimeTagIssueTemplate(ICheck check, IssueType type, string unformattedMessage)
+                : base(check, type, unformattedMessage)
+            {
+            }
+
+            public Issue Create(Lyric lyric, TimeTag timeTag) => new TimeTagIssue(lyric, this, timeTag, timeTag);
+        }
+
+        public class IssueTemplateLyricTimeTagOutOfRange : TimeTagIssueTemplate
+        {
+            public IssueTemplateLyricTimeTagOutOfRange(ICheck check)
+                : base(check, IssueType.Problem, "Time-tag index is out of range.")
+            {
+            }
+        }
+
+        public class IssueTemplateLyricTimeTagOverlapping : TimeTagIssueTemplate
+        {
+            public IssueTemplateLyricTimeTagOverlapping(ICheck check)
+                : base(check, IssueType.Problem, "Time-tag index is overlapping to another time-tag.")
+            {
+            }
+        }
+
+        public class IssueTemplateLyricTimeTagEmptyTime : TimeTagIssueTemplate
+        {
+            public IssueTemplateLyricTimeTagEmptyTime(ICheck check)
+                : base(check, IssueType.Problem, "Time-tag has no time.")
+            {
+            }
+        }
+    }
+}
