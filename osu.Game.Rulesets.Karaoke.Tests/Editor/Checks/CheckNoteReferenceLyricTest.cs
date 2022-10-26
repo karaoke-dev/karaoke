c6b8989d5f01d3498f60d42392ea3081672265a1
andy840119 <andy840119@gmail.com>
2022-10-26 23:39:25 +0800

    Refactor the check for note reference lyric.

---
+++ osu.Game.Rulesets.Karaoke.Tests/Editor/Checks/CheckNoteReferenceLyricTest.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Game.Rulesets.Karaoke.Edit.Checks;
+using osu.Game.Rulesets.Karaoke.Objects;
+using osu.Game.Rulesets.Objects;
+using static osu.Game.Rulesets.Karaoke.Edit.Checks.CheckNoteReferenceLyric;
+
+namespace osu.Game.Rulesets.Karaoke.Tests.Editor.Checks
+{
+    [TestFixture]
+    public class CheckNoteReferenceLyricTest : HitObjectCheckTest<Note, CheckNoteReferenceLyric>
+    {
+        [Test]
+        public void TestCheck()
+        {
+            var lyric = new Lyric();
+            var note = new Note
+            {
+                ReferenceLyric = lyric
+            };
+
+            AssertOk(new HitObject[] { lyric, note });
+        }
+
+        [Test]
+        public void TestCheckNullReferenceLyric()
+        {
+            var note = new Note
+            {
+                ReferenceLyric = null // reference should not be null.
+            };
+
+            AssertNotOk<IssueTemplateNullReferenceLyric>(note);
+        }
+
+        [Test]
+        public void TestCheckInvalidReferenceLyric()
+        {
+            var lyric = new Lyric();
+            var note = new Note
+            {
+                ReferenceLyric = lyric // reference lyric should be in the beatmap.
+            };
+
+            AssertNotOk<IssueTemplateInvalidReferenceLyric>(note);
+        }
+    }
+}
