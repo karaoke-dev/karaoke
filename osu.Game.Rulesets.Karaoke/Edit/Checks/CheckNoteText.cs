4a257ce412cd82008319627f38b0b3872f085f12
andy840119 <andy840119@gmail.com>
2022-10-29 00:39:02 +0800

    Add the fucking check for checking the text and ruby text in the note.

---
+++ osu.Game.Rulesets.Karaoke/Edit/Checks/CheckNoteText.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using osu.Game.Rulesets.Edit.Checks.Components;
+using osu.Game.Rulesets.Karaoke.Objects;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Checks
+{
+    public class CheckNoteText : CheckHitObjectProperty<Note>
+    {
+        protected override string Description => "Note with invalid text.";
+
+        public override IEnumerable<IssueTemplate> PossibleTemplates => new IssueTemplate[]
+        {
+            new IssueTemplateNoteNoText(this),
+        };
+
+        protected override IEnumerable<Issue> Check(Note note)
+        {
+            if (string.IsNullOrWhiteSpace(note.Text))
+                yield return new IssueTemplateNoteNoText(this).Create(note);
+
+            if (note.RubyText != null && string.IsNullOrWhiteSpace(note.RubyText))
+                yield return new IssueTemplateNoteEmptyRubyText(this).Create(note);
+        }
+
+        public class IssueTemplateNoteNoText : IssueTemplate
+        {
+            public IssueTemplateNoteNoText(ICheck check)
+                : base(check, IssueType.Problem, "Note must have text.")
+            {
+            }
+
+            public Issue Create(Note note)
+                => new(note, this);
+        }
+
+        public class IssueTemplateNoteEmptyRubyText : IssueTemplate
+        {
+            public IssueTemplateNoteEmptyRubyText(ICheck check)
+                : base(check, IssueType.Error, "Note's ruby text should be null or has the value.")
+            {
+            }
+
+            public Issue Create(Note note)
+                => new(note, this);
+        }
+    }
+}
