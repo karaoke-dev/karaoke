17e3fde53e23394e90baead3fb06f0f407004b66
andy840119 <andy840119@gmail.com>
2022-10-28 23:25:13 +0800

    Implemeht the checks for the reference lyric.

---
+++ osu.Game.Rulesets.Karaoke.Tests/Editor/Checks/CheckLyricReferenceLyricTest.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Game.Rulesets.Karaoke.Edit.Checks;
+using osu.Game.Rulesets.Karaoke.Objects;
+using osu.Game.Rulesets.Karaoke.Objects.Properties;
+using osu.Game.Rulesets.Objects;
+using static osu.Game.Rulesets.Karaoke.Edit.Checks.CheckLyricReferenceLyric;
+
+namespace osu.Game.Rulesets.Karaoke.Tests.Editor.Checks
+{
+    public class CheckLyricReferenceLyricTest : HitObjectCheckTest<Lyric, CheckLyricReferenceLyric>
+    {
+        [Test]
+        public void TestCheck()
+        {
+            var referencedLyric = new Lyric();
+            var lyric = new Lyric
+            {
+                ReferenceLyric = referencedLyric,
+                ReferenceLyricConfig = new ReferenceLyricConfig(),
+            };
+
+            AssertOk(new HitObject[] { referencedLyric, lyric });
+        }
+
+        [Test]
+        public void TestCheckSelfReference()
+        {
+            var lyric = new Lyric
+            {
+                ReferenceLyricConfig = new ReferenceLyricConfig(),
+            };
+
+            lyric.ReferenceLyric = lyric;
+
+            AssertNotOk<IssueTemplateLyricSelfReference>(lyric);
+        }
+
+        [Test]
+        public void TestCheckInvalidReferenceLyric()
+        {
+            var referencedLyric = new Lyric();
+            var lyric = new Lyric
+            {
+                ReferenceLyric = referencedLyric,
+                ReferenceLyricConfig = new ReferenceLyricConfig(),
+            };
+
+            AssertNotOk<IssueTemplateLyricInvalidReferenceLyric>(lyric);
+        }
+
+        [Test]
+        public void TestCheckNullReferenceLyricConfig()
+        {
+            var referencedLyric = new Lyric();
+            var lyric = new Lyric
+            {
+                ReferenceLyric = referencedLyric,
+            };
+
+            AssertNotOk<IssueTemplateLyricNullReferenceLyricConfig>(new HitObject[] { referencedLyric, lyric });
+        }
+
+        [Test]
+        public void TestCheckHasReferenceLyricConfigIfNoReferenceLyric()
+        {
+            var lyric = new Lyric
+            {
+                ReferenceLyric = null,
+                ReferenceLyricConfig = new ReferenceLyricConfig(),
+            };
+
+            AssertNotOk<IssueTemplateLyricHasReferenceLyricConfigIfNoReferenceLyric>(lyric);
+        }
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/Edit/Checks/CheckLyricReferenceLyric.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using System.Linq;
+using osu.Game.Rulesets.Edit.Checks.Components;
+using osu.Game.Rulesets.Karaoke.Objects;
+using osu.Game.Rulesets.Objects;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Checks
+{
+    public class CheckLyricReferenceLyric : CheckHitObjectProperty<Lyric>
+    {
+        protected override string Description => "Lyric with invalid reference lyric.";
+
+        public override IEnumerable<IssueTemplate> PossibleTemplates => new IssueTemplate[]
+        {
+            new IssueTemplateLyricSelfReference(this),
+        };
+
+        protected override IEnumerable<Issue> Check(Lyric lyric)
+        {
+            yield break;
+        }
+
+        protected override IEnumerable<Issue> CheckAllHitObject(IReadOnlyList<HitObject> hitObjects)
+        {
+            var lyrics = hitObjects.OfType<Lyric>().ToArray();
+
+            foreach (var lyric in lyrics)
+            {
+                if (lyric.ReferenceLyric == lyric)
+                    yield return new IssueTemplateLyricSelfReference(this).Create(lyric);
+
+                if (lyric.ReferenceLyric != null && !lyrics.Contains(lyric.ReferenceLyric))
+                    yield return new IssueTemplateLyricInvalidReferenceLyric(this).Create(lyric);
+
+                if (lyric.ReferenceLyric != null && lyric.ReferenceLyricConfig == null)
+                    yield return new IssueTemplateLyricNullReferenceLyricConfig(this).Create(lyric);
+
+                if (lyric.ReferenceLyric == null && lyric.ReferenceLyricConfig != null)
+                    yield return new IssueTemplateLyricHasReferenceLyricConfigIfNoReferenceLyric(this).Create(lyric);
+            }
+        }
+
+        public class IssueTemplateLyricSelfReference : IssueTemplate
+        {
+            public IssueTemplateLyricSelfReference(ICheck check)
+                : base(check, IssueType.Error, "Lyric should not reference to self.")
+            {
+            }
+
+            public Issue Create(Lyric lyric)
+                => new(lyric, this);
+        }
+
+        public class IssueTemplateLyricInvalidReferenceLyric : IssueTemplate
+        {
+            public IssueTemplateLyricInvalidReferenceLyric(ICheck check)
+                : base(check, IssueType.Error, "Reference lyric does not exist in the beatmap.")
+            {
+            }
+
+            public Issue Create(Lyric lyric)
+                => new(lyric, this);
+        }
+
+        public class IssueTemplateLyricNullReferenceLyricConfig : IssueTemplate
+        {
+            public IssueTemplateLyricNullReferenceLyricConfig(ICheck check)
+                : base(check, IssueType.Error, "Reference lyric should have the config also.")
+            {
+            }
+
+            public Issue Create(Lyric lyric)
+                => new(lyric, this);
+        }
+
+        public class IssueTemplateLyricHasReferenceLyricConfigIfNoReferenceLyric : IssueTemplate
+        {
+            public IssueTemplateLyricHasReferenceLyricConfigIfNoReferenceLyric(ICheck check)
+                : base(check, IssueType.Error, "Should not have the reference lyric config if contains no reference lyric.")
+            {
+            }
+
+            public Issue Create(Lyric lyric)
+                => new(lyric, this);
+        }
+    }
+}
