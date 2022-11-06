3ec3002c7738e9d7538dc838e6df70bb2278b483
andy840119 <andy840119@gmail.com>
2022-11-06 20:48:27 +0800

    Implement the base issue navigator. If pass the issue to here, will navigate to the suitable edit mode and highlight error part for letting user easier to notice that.

---
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/IssueNavigator.cs
@@ -0,0 +1,138 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Game.Rulesets.Edit.Checks.Components;
+using osu.Game.Rulesets.Karaoke.Edit.Checks;
+using osu.Game.Rulesets.Karaoke.Edit.Checks.Issues;
+using osu.Game.Rulesets.Karaoke.Edit.Lyrics.States;
+using osu.Game.Rulesets.Karaoke.Edit.Lyrics.States.Modes;
+using osu.Game.Rulesets.Karaoke.Objects;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics
+{
+    public class IssueNavigator : Component, IIssueNavigator
+    {
+        [Resolved, AllowNull]
+        private ILyricEditorState lyricEditorState { get; set; }
+
+        [Resolved, AllowNull]
+        private ILyricCaretState lyricCaretState { get; set; }
+
+        [Resolved, AllowNull]
+        private IEditRubyModeState editRubyModeState { get; set; }
+
+        [Resolved, AllowNull]
+        private IEditRomajiModeState editRomajiModeState { get; set; }
+
+        [Resolved, AllowNull]
+        private ITimeTagModeState timeTagModeState { get; set; }
+
+        [Resolved, AllowNull]
+        private IEditNoteModeState noteModeState { get; set; }
+
+        public void Navigate(Issue issue)
+        {
+            // navigate to edit mode.
+            var targetEditMode = getNavigateEditMode(issue.Check);
+            if (targetEditMode != null)
+                lyricEditorState.SwitchMode(targetEditMode.Value);
+
+            // navigate to sub-mode if needed.
+            var targetSubMode = getNavigateSubMode(issue);
+            if (targetSubMode != null)
+                lyricEditorState.SwitchSubMode(targetSubMode);
+
+            // navigate to the target lyric.
+            var lyric = getNavigateLyric(issue);
+            if (lyric == null)
+                return;
+
+            lyricCaretState.MoveCaretToTargetPosition(lyric);
+
+            // navigate to the target index in the lyric.
+            object? lyricIndex = getNavigateLyricIndex(issue);
+            if (lyricIndex == null)
+                return;
+
+            var blueprintSelection = getBlueprintSelection(lyricIndex);
+            blueprintSelection?.Select(lyricIndex);
+        }
+
+        private static LyricEditorMode? getNavigateEditMode(ICheck check)
+        {
+            switch (check)
+            {
+                case CheckLyricText:
+                    return LyricEditorMode.Texting;
+
+                case CheckLyricReferenceLyric:
+                    return LyricEditorMode.Reference;
+
+                case CheckLyricLanguage:
+                    return LyricEditorMode.Language;
+
+                case CheckLyricRubyTag:
+                    return LyricEditorMode.EditRuby;
+
+                case CheckLyricRomajiTag:
+                    return LyricEditorMode.EditRomaji;
+
+                case CheckLyricTimeTag:
+                    return LyricEditorMode.EditTimeTag;
+
+                case CheckNoteReferenceLyric:
+                case CheckNoteText:
+                    return LyricEditorMode.EditNote;
+
+                default:
+                    return null;
+            }
+        }
+
+        private static Enum? getNavigateSubMode(Issue issue)
+        {
+            // todo: implement.
+            return null;
+        }
+
+        private static Lyric? getNavigateLyric(Issue issue)
+        {
+            var lyric = issue.HitObjects.OfType<Lyric>().FirstOrDefault();
+            if (lyric != null)
+                return lyric;
+
+            var note = issue.HitObjects.OfType<Note>().FirstOrDefault();
+            if (note != null)
+                return note.ReferenceLyric;
+
+            return null;
+        }
+
+        private static object? getNavigateLyricIndex(Issue issue) =>
+            issue switch
+            {
+                RubyTagIssue rubyTagIssue => rubyTagIssue.RubyTag,
+                RomajiTagIssue romajiTagIssue => romajiTagIssue.RomajiTag,
+                TimeTagIssue timeTagIssue => timeTagIssue.TimeTag,
+                _ => null
+            };
+
+        private IHasBlueprintSelection<TItem>? getBlueprintSelection<TItem>(TItem item) where TItem : class
+        {
+            object[] availableEditModes =
+            {
+                editRubyModeState,
+                editRomajiModeState,
+                timeTagModeState,
+                noteModeState
+            };
+
+            return availableEditModes.OfType<IHasBlueprintSelection<TItem>>().FirstOrDefault();
+        }
+    }
+}
