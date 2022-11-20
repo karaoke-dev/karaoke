88e5b6e8048e8175ca7032eba3ea59a559b159c5
andy840119 <andy840119@gmail.com>
2022-11-20 10:04:05 +0800

    Panel should be only able to place the sections. Like how `EditorRoundedScreenSettings` did.

--- osu.Game.Rulesets.Karaoke/Edit/Lyrics/Compose/Panels/InvalidPanel.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Compose/Panels/InvalidPanel.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
+using osu.Framework.Graphics;
+
 namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Compose.Panels
 {
     public class InvalidPanel : Panel
@@ -8,6 +11,11 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Compose.Panels
         public InvalidPanel()
         {
             Width = 200;
+        }
+
+        protected override IReadOnlyList<Drawable> CreateSections()
+        {
+            return new Drawable[] { };
         }
     }
 }
