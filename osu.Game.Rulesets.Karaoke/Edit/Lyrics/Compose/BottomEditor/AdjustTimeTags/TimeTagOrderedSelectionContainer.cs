6ed106ddfa8f6b33d3ec235fc5ddaf89d9cbee5b
andy840119 <andy840119@gmail.com>
2022-11-22 00:12:28 +0800

    Remove un-need sort call.

--- osu.Game.Rulesets.Karaoke/Edit/Lyrics/Compose/BottomEditor/AdjustTimeTags/TimeTagOrderedSelectionContainer.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Compose/BottomEditor/AdjustTimeTags/TimeTagOrderedSelectionContainer.cs
@@ -3,8 +3,6 @@
 
 #nullable disable
 
-using System.Collections.Generic;
-using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Rulesets.Edit;
@@ -19,51 +17,14 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Compose.BottomEditor.AdjustTimeT
     {
         public override void Add(SelectionBlueprint<TimeTag> drawable)
         {
+            SortInternal();
             base.Add(drawable);
-            bindStartTime(drawable);
         }
 
         public override bool Remove(SelectionBlueprint<TimeTag> drawable, bool disposeImmediately)
         {
-            if (!base.Remove(drawable, disposeImmediately))
-                return false;
-
-            unbindStartTime(drawable);
-            return true;
-        }
-
-        public override void Clear(bool disposeChildren)
-        {
-            base.Clear(disposeChildren);
-            unbindAllStartTimes();
-        }
-
-        private readonly Dictionary<SelectionBlueprint<TimeTag>, IBindable> startTimeMap = new();
-
-        private void bindStartTime(SelectionBlueprint<TimeTag> blueprint)
-        {
-            var bindable = blueprint.Item.TimeBindable.GetBoundCopy();
-
-            bindable.BindValueChanged(_ =>
-            {
-                if (LoadState >= LoadState.Ready)
-                    SortInternal();
-            });
-
-            startTimeMap[blueprint] = bindable;
-        }
-
-        private void unbindStartTime(SelectionBlueprint<TimeTag> blueprint)
-        {
-            startTimeMap[blueprint].UnbindAll();
-            startTimeMap.Remove(blueprint);
-        }
-
-        private void unbindAllStartTimes()
-        {
-            foreach (var kvp in startTimeMap)
-                kvp.Value.UnbindAll();
-            startTimeMap.Clear();
+            SortInternal();
+            return base.Remove(drawable, disposeImmediately);
         }
 
         protected override int Compare(Drawable x, Drawable y)
