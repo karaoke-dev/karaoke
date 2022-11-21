16c70ebee32422a52f8c8d6da32e04d907bf3c2d
andy840119 <andy840119@gmail.com>
2022-11-21 22:07:44 +0800

    Implement the issue section for texting mode and apply into the settings.

--- osu.Game.Rulesets.Karaoke/Edit/Lyrics/Settings/TextingSettings.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Lyrics/Settings/TextingSettings.cs
@@ -1,9 +1,13 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Game.Rulesets.Karaoke.Edit.Lyrics.Settings.Texting;
+using osu.Game.Rulesets.Karaoke.Edit.Lyrics.States.Modes;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Settings
 {
@@ -13,10 +17,36 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Lyrics.Settings
 
         public override float SettingsWidth => 300;
 
-        protected override IReadOnlyList<Drawable> CreateSections() => new Drawable[]
+        private readonly IBindable<TextingEditMode> bindableMode = new Bindable<TextingEditMode>();
+
+        [BackgroundDependencyLoader]
+        private void load(ITextingModeState textingModeState)
         {
-            new TextingEditModeSection(),
-            new TextingSwitchSpecialActionSection()
+            bindableMode.BindTo(textingModeState.BindableEditMode);
+            bindableMode.BindValueChanged(e =>
+            {
+                ReloadSections();
+            }, true);
+        }
+
+        protected override IReadOnlyList<Drawable> CreateSections() => bindableMode.Value switch
+        {
+            TextingEditMode.Typing => new Drawable[]
+            {
+                new TextingEditModeSection(),
+                new TextingSwitchSpecialActionSection(),
+            },
+            TextingEditMode.Split => new Drawable[]
+            {
+                new TextingEditModeSection(),
+                new TextingSwitchSpecialActionSection(),
+            },
+            TextingEditMode.Verify => new Drawable[]
+            {
+                new TextingEditModeSection(),
+                new TextingIssueSection(),
+            },
+            _ => throw new ArgumentOutOfRangeException()
         };
     }
 }
