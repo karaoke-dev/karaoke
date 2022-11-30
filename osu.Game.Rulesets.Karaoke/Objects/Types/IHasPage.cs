7864ac76e8b7a80ef37330ec35c7e1de8b68a3e3
andy840119 <andy840119@gmail.com>
2022-11-30 21:36:43 +0800

    it should be more useful if able to get the page index from the lyric or note.

--- osu.Game.Rulesets.Karaoke/Objects/Types/IHasPage.cs
+++ osu.Game.Rulesets.Karaoke/Objects/Types/IHasPage.cs
@@ -1,11 +1,9 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Game.Rulesets.Karaoke.Beatmaps.Metadatas;
-
 namespace osu.Game.Rulesets.Karaoke.Objects.Types;
 
 public interface IHasPage
 {
-    Page? Page { get; set; }
+    int? PageIndex { get; set; }
 }
