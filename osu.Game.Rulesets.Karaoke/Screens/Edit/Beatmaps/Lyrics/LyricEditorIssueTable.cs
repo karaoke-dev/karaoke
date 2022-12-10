130925c21935eb57f84c6dc98fc40dbd09932dc3
andy840119 <andy840119@gmail.com>
2022-12-10 16:31:44 +0800

    Create base issue table.

--- osu.Game.Rulesets.Karaoke/Screens/Edit/Beatmaps/Lyrics/LyricEditorIssueTable.cs
+++ osu.Game.Rulesets.Karaoke/Screens/Edit/Beatmaps/Lyrics/LyricEditorIssueTable.cs
@@ -1,22 +1,18 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using osu.Framework.Allocation;
-using osu.Framework.Extensions;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Input.Events;
 using osu.Game.Rulesets.Edit.Checks.Components;
-using osu.Game.Screens.Edit;
 using osuTK.Graphics;
 
 namespace osu.Game.Rulesets.Karaoke.Screens.Edit.Beatmaps.Lyrics;
 
-public abstract partial class LyricEditorIssueTable : EditorTable
+public abstract partial class LyricEditorIssueTable : IssueTable
 {
     private const float horizontal_inset = 20;
 
@@ -27,8 +23,6 @@ public abstract partial class LyricEditorIssueTable : EditorTable
     {
         Padding = new MarginPadding { Horizontal = horizontal_inset };
         BackgroundFlow.Padding = new MarginPadding { Horizontal = -horizontal_inset };
-
-        Columns = CreateHeaders();
     }
 
     public new bool ShowHeaders
@@ -41,31 +35,13 @@ public abstract partial class LyricEditorIssueTable : EditorTable
         }
     }
 
-    public IEnumerable<Issue> Issues
+    protected override void OnIssueClicked(Issue issue)
     {
-        set
-        {
-            Content = null;
-            BackgroundFlow.Clear();
-
-            foreach (var issue in value)
-            {
-                BackgroundFlow.Add(new IssueTableRowBackground(issue)
-                {
-                    Action = () =>
-                    {
-                        issueNavigator.Navigate(issue);
-                    },
-                });
-            }
-
-            Content = value.Select(CreateContent).ToArray().ToRectangular();
-        }
+        issueNavigator.Navigate(issue);
     }
 
-    protected abstract TableColumn[] CreateHeaders();
-
-    protected abstract Drawable[] CreateContent(Issue issue);
+    protected override RowBackground CreateRowBackground(Issue issue)
+        => new IssueTableRowBackground(issue);
 
     /// <summary>
     /// Inherit the class just for able to override the colour.
---
+++ osu.Game.Rulesets.Karaoke/Screens/Edit/IssueTable.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using System.Linq;
+using osu.Framework.Extensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Rulesets.Edit.Checks.Components;
+using osu.Game.Screens.Edit;
+
+namespace osu.Game.Rulesets.Karaoke.Screens.Edit;
+
+public abstract partial class IssueTable : EditorTable
+{
+    protected IssueTable()
+    {
+        Columns = CreateHeaders();
+    }
+
+    public IEnumerable<Issue> Issues
+    {
+        set
+        {
+            Content = null;
+            BackgroundFlow.Clear();
+
+            foreach (var issue in value)
+            {
+                BackgroundFlow.Add(CreateRowBackground(issue).With(x =>
+                {
+                    x.Action = () =>
+                    {
+                        OnIssueClicked(issue);
+                    };
+                }));
+            }
+
+            Content = value.Select(CreateContent).ToArray().ToRectangular();
+        }
+    }
+
+    protected abstract void OnIssueClicked(Issue issue);
+
+    protected virtual RowBackground CreateRowBackground(Issue issue) => new(issue);
+
+    protected abstract TableColumn[] CreateHeaders();
+
+    protected abstract Drawable[] CreateContent(Issue issue);
+}
