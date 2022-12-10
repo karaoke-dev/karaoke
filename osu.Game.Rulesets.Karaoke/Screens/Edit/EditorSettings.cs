07b6341dd8c939093166d986d66589a9d9400055
andy840119 <andy840119@gmail.com>
2022-12-10 14:36:55 +0800

    Implement base settings for the whole editor.

---
+++ osu.Game.Rulesets.Karaoke/Screens/Edit/EditorSettings.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Linq;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Testing;
+using osu.Game.Rulesets.Karaoke.Screens.Edit.Beatmaps.Lyrics.Settings;
+using osu.Game.Screens.Edit;
+
+namespace osu.Game.Rulesets.Karaoke.Screens.Edit;
+
+public abstract partial class EditorSettings : EditorRoundedScreenSettings
+{
+    public abstract SettingsDirection Direction { get; }
+
+    public abstract float SettingsWidth { get; }
+
+    protected void ReloadSections()
+    {
+        this.ChildrenOfType<FillFlowContainer>().First().Children = CreateSections();
+    }
+}
