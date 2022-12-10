9f8e8a40330e85a6940208326cc5945581cae181
andy840119 <andy840119@gmail.com>
2022-12-10 14:50:25 +0800

    Move the lyric-editor only logic back.

--- osu.Game.Rulesets.Karaoke/Screens/Edit/Beatmaps/Lyrics/Settings/LyricEditorSettings.cs
+++ osu.Game.Rulesets.Karaoke/Screens/Edit/Beatmaps/Lyrics/Settings/LyricEditorSettings.cs
@@ -3,18 +3,27 @@
 
 using System.Linq;
 using osu.Framework.Allocation;
-using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Containers;
 using osu.Framework.Testing;
 
 namespace osu.Game.Rulesets.Karaoke.Screens.Edit.Beatmaps.Lyrics.Settings
 {
     public abstract partial class LyricEditorSettings : EditorSettings
     {
+        public abstract SettingsDirection Direction { get; }
+
+        public abstract float SettingsWidth { get; }
+
+        protected void ReloadSections()
+        {
+            this.ChildrenOfType<FillFlowContainer>().First().Children = CreateSections();
+        }
+
         [BackgroundDependencyLoader]
         private void load(ILyricEditorState state, LyricEditorColourProvider colourProvider)
         {
             // change the background colour to the lighter one.
-            this.ChildrenOfType<Box>().First().Colour = colourProvider.Background3(state.Mode);
+            ChangeBackgroundColour(colourProvider.Background3(state.Mode));
         }
     }
 }
