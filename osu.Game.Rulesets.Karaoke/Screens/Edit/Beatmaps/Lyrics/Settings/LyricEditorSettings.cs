4811f239a8da37f72028b4a6e554d522da31c8c2
andy840119 <andy840119@gmail.com>
2022-12-11 21:24:02 +0800

    Move the function into base class.

--- osu.Game.Rulesets.Karaoke/Screens/Edit/Beatmaps/Lyrics/Settings/LyricEditorSettings.cs
+++ osu.Game.Rulesets.Karaoke/Screens/Edit/Beatmaps/Lyrics/Settings/LyricEditorSettings.cs
@@ -1,10 +1,7 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Linq;
 using osu.Framework.Allocation;
-using osu.Framework.Graphics.Containers;
-using osu.Framework.Testing;
 
 namespace osu.Game.Rulesets.Karaoke.Screens.Edit.Beatmaps.Lyrics.Settings
 {
@@ -13,11 +10,6 @@ namespace osu.Game.Rulesets.Karaoke.Screens.Edit.Beatmaps.Lyrics.Settings
         public abstract SettingsDirection Direction { get; }
 
         public abstract float SettingsWidth { get; }
-
-        protected void ReloadSections()
-        {
-            this.ChildrenOfType<FillFlowContainer>().First().Children = CreateSections();
-        }
 
         [BackgroundDependencyLoader]
         private void load(ILyricEditorState state, LyricEditorColourProvider colourProvider)
--- osu.Game.Rulesets.Karaoke/Screens/Edit/EditorSettings.cs
+++ osu.Game.Rulesets.Karaoke/Screens/Edit/EditorSettings.cs
@@ -3,6 +3,7 @@
 
 using System.Linq;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Testing;
 using osu.Game.Screens.Edit;
@@ -11,6 +12,11 @@ namespace osu.Game.Rulesets.Karaoke.Screens.Edit;
 
 public abstract partial class EditorSettings : EditorRoundedScreenSettings
 {
+    protected void ReloadSections()
+    {
+        this.ChildrenOfType<FillFlowContainer>().First().Children = CreateSections();
+    }
+
     protected void ChangeBackgroundColour(Colour4 colour4)
     {
         this.ChildrenOfType<Box>().First().Colour = colour4;
