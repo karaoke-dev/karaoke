9ce4e7572c6a45b32fe489857bffc13fe9424949
andy840119 <andy840119@gmail.com>
2022-12-11 20:49:58 +0800

    Implement the page editor verifier and it's interface.

---
+++ osu.Game.Rulesets.Karaoke/Screens/Edit/Beatmaps/Pages/IPageEditorVerifier.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Bindables;
+using osu.Game.Rulesets.Edit.Checks.Components;
+
+namespace osu.Game.Rulesets.Karaoke.Screens.Edit.Beatmaps.Pages;
+
+public interface IPageEditorVerifier
+{
+    IBindableList<Issue> Issues { get; }
+
+    void Refresh();
+
+    void Navigate(Issue issue);
+}
