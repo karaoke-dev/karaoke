8f63613cb189f75af81e0f59add174e04ae73f84
andy840119 <andy840119@gmail.com>
2022-12-18 16:52:53 +0800

    Implement the page generator.

--- osu.Game.Rulesets.Karaoke/Edit/Generator/Beatmaps/Pages/PageGeneratorConfig.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Generator/Beatmaps/Pages/PageGeneratorConfig.cs
@@ -1,1 +1,22 @@
-﻿
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Karaoke.Edit.Checks;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Generator.Beatmaps.Pages;
+
+public class PageGeneratorConfig : IHasConfig<PageGeneratorConfig>
+{
+    public double MinTime { get; set; }
+
+    public double MaxTime { get; set; }
+
+    public bool ClearExistPages { get; set; }
+
+    public PageGeneratorConfig CreateDefaultConfig() => new()
+    {
+        MinTime = CheckBeatmapPageInfo.MIN_INTERVAL,
+        MaxTime = CheckBeatmapPageInfo.MAX_INTERVAL,
+        ClearExistPages = false
+    };
+}
