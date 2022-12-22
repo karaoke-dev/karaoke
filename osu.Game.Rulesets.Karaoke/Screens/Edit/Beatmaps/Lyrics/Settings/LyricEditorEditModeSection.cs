bc0187c1ee75decd0439daf73a139a417b1c2dca
andy840119 <andy840119@gmail.com>
2022-12-23 00:23:46 +0800

    Move the same naming into the base class.

--- osu.Game.Rulesets.Karaoke/Screens/Edit/Beatmaps/Lyrics/Settings/LyricEditorEditModeSection.cs
+++ osu.Game.Rulesets.Karaoke/Screens/Edit/Beatmaps/Lyrics/Settings/LyricEditorEditModeSection.cs
@@ -32,8 +32,6 @@ namespace osu.Game.Rulesets.Karaoke.Screens.Edit.Beatmaps.Lyrics.Settings
     public abstract partial class LyricEditorEditModeSection<TEditMode> : EditModeSection<TEditMode>
         where TEditMode : Enum
     {
-        protected sealed override LocalisableString Title => "Edit mode";
-
         [Resolved]
         private ILyricSelectionState lyricSelectionState { get; set; }
 
