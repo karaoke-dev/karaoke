f7bd2390efaf056fc7c4c3e110d9b61dc08a620a
andy840119 <andy840119@gmail.com>
2022-12-24 11:16:27 +0800

    Inherit ther base class for able to share the same test method.

--- osu.Game.Rulesets.Karaoke.Tests/Editor/Generator/Beatmaps/BaseBeatmapDetectorTest.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Editor/Generator/Beatmaps/BaseBeatmapDetectorTest.cs
@@ -1,8 +1,6 @@
 // Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
-using NUnit.Framework;
 using osu.Game.Rulesets.Karaoke.Beatmaps;
 using osu.Game.Rulesets.Karaoke.Edit.Generator;
 using osu.Game.Rulesets.Karaoke.Edit.Generator.Beatmaps;
@@ -10,44 +8,9 @@ using osu.Game.Rulesets.Karaoke.Edit.Generator.Beatmaps;
 namespace osu.Game.Rulesets.Karaoke.Tests.Editor.Generator.Beatmaps
 {
     public abstract class BaseBeatmapDetectorTest<TDetector, TObject, TConfig>
-        : BaseGeneratorTest<TConfig>
-        where TDetector : BeatmapPropertyDetector<TObject, TConfig> where TConfig : IHasConfig<TConfig>, new()
+        : BasePropertyDetectorTest<TDetector, KaraokeBeatmap, TObject, TConfig>
+        where TDetector : BeatmapPropertyDetector<TObject, TConfig>
+        where TConfig : IHasConfig<TConfig>, new()
     {
-        protected static TDetector GenerateDetector(TConfig config)
-        {
-            if (Activator.CreateInstance(typeof(TDetector), config) is not TDetector detector)
-                throw new ArgumentNullException(nameof(detector));
-
-            return detector;
-        }
-
-        protected static void CheckCanDetect(KaraokeBeatmap beatmap, bool canDetect, TConfig config)
-        {
-            var detector = GenerateDetector(config);
-
-            CheckCanDetect(beatmap, canDetect, detector);
-        }
-
-        protected static void CheckCanDetect(KaraokeBeatmap beatmap, bool canDetect, TDetector detector)
-        {
-            bool actual = detector.CanDetect(beatmap);
-            Assert.AreEqual(canDetect, actual);
-        }
-
-        protected void CheckDetectResult(KaraokeBeatmap beatmap, TObject expected, TConfig config)
-        {
-            var detector = GenerateDetector(config);
-
-            CheckDetectResult(beatmap, expected, detector);
-        }
-
-        protected void CheckDetectResult(KaraokeBeatmap beatmap, TObject expected, TDetector detector)
-        {
-            // create time tag and actually time tag.
-            var actual = detector.Detect(beatmap);
-            AssertEqual(expected, actual);
-        }
-
-        protected abstract void AssertEqual(TObject expected, TObject actual);
     }
 }
--- osu.Game.Rulesets.Karaoke.Tests/Editor/Generator/Lyrics/BaseLyricGeneratorTest.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Editor/Generator/Lyrics/BaseLyricGeneratorTest.cs
@@ -1,8 +1,6 @@
 // Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
-using NUnit.Framework;
 using osu.Game.Rulesets.Karaoke.Edit.Generator;
 using osu.Game.Rulesets.Karaoke.Edit.Generator.Lyrics;
 using osu.Game.Rulesets.Karaoke.Objects;
@@ -10,44 +8,9 @@ using osu.Game.Rulesets.Karaoke.Objects;
 namespace osu.Game.Rulesets.Karaoke.Tests.Editor.Generator.Lyrics
 {
     public abstract class BaseLyricGeneratorTest<TGenerator, TObject, TConfig>
-        : BaseGeneratorTest<TConfig>
-        where TGenerator : LyricPropertyGenerator<TObject, TConfig> where TConfig : IHasConfig<TConfig>, new()
+        : BasePropertyGeneratorTest<TGenerator, Lyric, TObject, TConfig>
+        where TGenerator : LyricPropertyGenerator<TObject, TConfig>
+        where TConfig : IHasConfig<TConfig>, new()
     {
-        protected static TGenerator GenerateGenerator(TConfig config)
-        {
-            if (Activator.CreateInstance(typeof(TGenerator), config) is not TGenerator generator)
-                throw new ArgumentNullException(nameof(generator));
-
-            return generator;
-        }
-
-        protected static void CheckCanGenerate(Lyric lyric, bool canGenerate, TConfig config)
-        {
-            var generator = GenerateGenerator(config);
-
-            CheckCanGenerate(lyric, canGenerate, generator);
-        }
-
-        protected static void CheckCanGenerate(Lyric lyric, bool canGenerate, TGenerator generator)
-        {
-            bool actual = generator.CanGenerate(lyric);
-            Assert.AreEqual(canGenerate, actual);
-        }
-
-        protected void CheckGenerateResult(Lyric lyric, TObject expected, TConfig config)
-        {
-            var generator = GenerateGenerator(config);
-
-            CheckGenerateResult(lyric, expected, generator);
-        }
-
-        protected void CheckGenerateResult(Lyric lyric, TObject expected, TGenerator generator)
-        {
-            // create time tag and actually time tag.
-            var actual = generator.Generate(lyric);
-            AssertEqual(expected, actual);
-        }
-
-        protected abstract void AssertEqual(TObject expected, TObject actual);
     }
 }
