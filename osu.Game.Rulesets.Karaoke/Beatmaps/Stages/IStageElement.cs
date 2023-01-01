944a3b665139a1213cd49443d64572f6f9e01ca8
andy840119 <andy840119@gmail.com>
2023-01-02 00:35:31 +0800

    Implement the base interface for stage element. Element means the component in the stage info that use can create/remove and give it a name.

---
+++ osu.Game.Rulesets.Karaoke/Beatmaps/Stages/IStageElement.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace osu.Game.Rulesets.Karaoke.Beatmaps.Stages;
+
+public interface IStageElement
+{
+    public int ID { get; set; }
+
+    public string Name { get; set; }
+}
