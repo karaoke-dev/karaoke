50bcefd5593efe2bf152e1adb26619daeca0962b
andy840119 <andy840119@gmail.com>
2023-01-02 00:34:48 +0800

    Implement base stage info class. Will have feature for able to get the layout/style info by HitObject.

---
+++ osu.Game.Rulesets.Karaoke/Beatmaps/Stages/StageInfo.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Collections.Generic;
+using osu.Game.Rulesets.Karaoke.Objects;
+
+namespace osu.Game.Rulesets.Karaoke.Beatmaps.Stages;
+
+public abstract class StageInfo
+{
+    // todo: very not sure should be better to return the applier or style/layout data.
+    public IEnumerable<object> GetStageAppliers(KaraokeHitObject hitObject)
+    {
+        var elements = GetStageElements(hitObject);
+
+        return hitObject switch
+        {
+            Lyric => ConvertToLyricStageAppliers(elements),
+            Note => ConvertToNoteStageAppliers(elements),
+            _ => Array.Empty<object>()
+        };
+    }
+
+    protected IEnumerable<IStageElement> GetStageElements(KaraokeHitObject hitObject) =>
+        hitObject switch
+        {
+            Lyric lyric => GetLyricStageElements(lyric),
+            Note note => GetNoteStageElements(note),
+            _ => Array.Empty<IStageElement>()
+        };
+
+    protected abstract IEnumerable<IStageElement> GetLyricStageElements(Lyric lyric);
+
+    protected abstract IEnumerable<IStageElement> GetNoteStageElements(Note note);
+
+    protected abstract IEnumerable<object> ConvertToLyricStageAppliers(IEnumerable<IStageElement> elements);
+
+    protected abstract IEnumerable<object> ConvertToNoteStageAppliers(IEnumerable<IStageElement> elements);
+}
