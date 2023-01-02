9fea07db78bf91925b336e51071a42174ba9e875
andy840119 <andy840119@gmail.com>
2023-01-02 14:19:16 +0800

    Implement the change handler and the test case.

---
+++ osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/Beatmaps/IBeatmapClassicStageChangeHandler.cs
@@ -0,0 +1,12 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using osu.Game.Rulesets.Karaoke.Beatmaps.Stages.Classic;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers.Beatmaps;
+
+public interface IBeatmapClassicStageChangeHandler
+{
+    void EditLayoutDefinition(Action<ClassicLyricLayoutDefinition> action);
+}
