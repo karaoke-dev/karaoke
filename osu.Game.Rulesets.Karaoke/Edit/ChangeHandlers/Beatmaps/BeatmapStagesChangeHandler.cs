2f1fb24fc20cb419267c3d512622b0de43b8af08
andy840119 <andy840119@gmail.com>
2023-01-03 23:31:50 +0800

    Implement beatmap stages change handler for able to add or remvoe the stage.

---
+++ osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/Beatmaps/BeatmapStagesChangeHandler.cs
@@ -0,0 +1,39 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Linq;
+using osu.Game.Rulesets.Karaoke.Beatmaps;
+using osu.Game.Rulesets.Karaoke.Beatmaps.Stages;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers.Beatmaps;
+
+public partial class BeatmapStagesChangeHandler : BeatmapPropertyChangeHandler, IBeatmapStagesChangeHandler
+{
+    public void AddStageInfoToBeatmap<TStageInfo>() where TStageInfo : StageInfo, new()
+    {
+        PerformBeatmapChanged(beatmap =>
+        {
+            var stage = getStageInfo<TStageInfo>(beatmap);
+            if (stage != null)
+                throw new InvalidOperationException($"{nameof(TStageInfo)} already exist in the beatmap.");
+
+            beatmap.StageInfos.Add(new TStageInfo());
+        });
+    }
+
+    public void RemoveStageInfoFromBeatmap<TStageInfo>() where TStageInfo : StageInfo, new()
+    {
+        PerformBeatmapChanged(beatmap =>
+        {
+            var stage = getStageInfo<TStageInfo>(beatmap);
+            if (stage == null)
+                throw new InvalidOperationException($"There's no {nameof(TStageInfo)} in the beatmap.");
+
+            beatmap.StageInfos.Remove(stage);
+        });
+    }
+
+    private TStageInfo? getStageInfo<TStageInfo>(KaraokeBeatmap beatmap) where TStageInfo : StageInfo
+        => beatmap.StageInfos.OfType<TStageInfo>().FirstOrDefault();
+}
