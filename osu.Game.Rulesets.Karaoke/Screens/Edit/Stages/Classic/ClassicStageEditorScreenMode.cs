8e0bdd11771b1e7490530199561b5749efaa63b3
andy840119 <andy840119@gmail.com>
2023-01-14 17:15:54 +0800

    Implement the edit mode for switch the screen in the class stage editor.

---
+++ osu.Game.Rulesets.Karaoke/Screens/Edit/Stages/Classic/ClassicStageEditorScreenMode.cs
@@ -0,0 +1,15 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.ComponentModel;
+
+namespace osu.Game.Rulesets.Karaoke.Screens.Edit.Stages.Classic;
+
+public enum ClassicStageEditorScreenMode
+{
+    [Description("Stage")]
+    Stage,
+
+    [Description("Config")]
+    Config,
+}
