63a1c19c596538def7b9c4fb1804376ab6407a99
andy840119 <andy840119@gmail.com>
2023-01-15 13:41:50 +0800

    Implement state provide for able to provide the edit mode and the category.

--- osu.Game.Rulesets.Karaoke/Screens/Edit/Stages/Classic/Stage/StageScreen.cs
+++ osu.Game.Rulesets.Karaoke/Screens/Edit/Stages/Classic/Stage/StageScreen.cs
@@ -1,12 +1,30 @@
 // Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Bindables;
+
 namespace osu.Game.Rulesets.Karaoke.Screens.Edit.Stages.Classic.Stage;
 
-public partial class StageScreen : ClassicStageScreen
+public partial class StageScreen : ClassicStageScreen, IStageEditorStateProvider
 {
+    public IBindable<StageEditorEditMode> BindableEditMode => bindableEditMode;
+    public IBindable<StageEditorEditCategory> BindableEditCategory => bindableCategory;
+
+    private readonly Bindable<StageEditorEditMode> bindableEditMode = new();
+    private readonly Bindable<StageEditorEditCategory> bindableCategory = new();
+
     public StageScreen()
         : base(ClassicStageEditorScreenMode.Stage)
     {
+    }
+
+    public void ChangeEditMode(StageEditorEditMode mode)
+    {
+        bindableEditMode.Value = mode;
+    }
+
+    public void ChangeEditCategory(StageEditorEditCategory mode)
+    {
+        bindableCategory.Value = mode;
     }
 }
