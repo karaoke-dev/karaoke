3e6856ef57ce5a97f7ca3b945f7336028fb1a009
andy840119 <andy840119@gmail.com>
2023-01-15 13:58:56 +0800

    Apply the stage settings into the stage screen.

--- osu.Game.Rulesets.Karaoke/Screens/Edit/Stages/Classic/Stage/StageScreen.cs
+++ osu.Game.Rulesets.Karaoke/Screens/Edit/Stages/Classic/Stage/StageScreen.cs
@@ -1,10 +1,15 @@
 // Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Allocation;
 using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Rulesets.Karaoke.Screens.Edit.Stages.Classic.Stage.Settings;
 
 namespace osu.Game.Rulesets.Karaoke.Screens.Edit.Stages.Classic.Stage;
 
+[Cached(typeof(IStageEditorStateProvider))]
 public partial class StageScreen : ClassicStageScreen, IStageEditorStateProvider
 {
     public IBindable<StageEditorEditMode> BindableEditMode => bindableEditMode;
@@ -16,6 +21,26 @@ public partial class StageScreen : ClassicStageScreen, IStageEditorStateProvider
     public StageScreen()
         : base(ClassicStageEditorScreenMode.Stage)
     {
+        Child = new GridContainer
+        {
+            RelativeSizeAxes = Axes.Both,
+            ColumnDimensions = new[]
+            {
+                new Dimension(),
+                new Dimension(GridSizeMode.Absolute, 250),
+            },
+            Content = new[]
+            {
+                new Drawable[]
+                {
+                    new StageEditor
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                    },
+                    new StageSettings(),
+                },
+            }
+        };
     }
 
     public void ChangeEditMode(StageEditorEditMode mode)
