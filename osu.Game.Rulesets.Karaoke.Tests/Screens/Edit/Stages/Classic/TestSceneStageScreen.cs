d26be09cd529a8b4d1c4c13e5047983a0c7d8baf
andy840119 <andy840119@gmail.com>
2023-01-15 22:22:21 +0800

    Add more test to make the that visit all the category and edit mode.

--- osu.Game.Rulesets.Karaoke.Tests/Screens/Edit/Stages/Classic/TestSceneStageScreen.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Screens/Edit/Stages/Classic/TestSceneStageScreen.cs
@@ -1,6 +1,8 @@
 // Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using NUnit.Framework;
 using osu.Game.Rulesets.Karaoke.Screens.Edit.Stages.Classic.Stage;
 
 namespace osu.Game.Rulesets.Karaoke.Tests.Screens.Edit.Stages.Classic;
@@ -8,4 +10,28 @@ namespace osu.Game.Rulesets.Karaoke.Tests.Screens.Edit.Stages.Classic;
 public partial class TestSceneStageScreen : ClassicStageScreenTestScene<StageScreen>
 {
     protected override StageScreen CreateEditorScreen() => new();
+
+    [Test]
+    public void TestSwitchCategoryAndEditMode()
+    {
+        if (Child is not StageScreen stageScreen)
+            throw new InvalidOperationException();
+
+        AddWaitStep("wait for editor to load", 5);
+
+        foreach (var category in Enum.GetValues<StageEditorEditCategory>())
+        {
+            foreach (var editMode in Enum.GetValues<StageEditorEditMode>())
+            {
+                AddLabel($"{Enum.GetName(category)} category with {Enum.GetName(editMode)} mode");
+
+                AddStep($"switch to mode {Enum.GetName(editMode)}", () =>
+                {
+                    stageScreen.ChangeEditCategory(category);
+                    stageScreen.ChangeEditMode(editMode);
+                });
+                AddWaitStep("wait for switch to new mode", 5);
+            }
+        }
+    }
 }
