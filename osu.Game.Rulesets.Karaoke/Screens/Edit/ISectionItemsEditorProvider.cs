cc9bed9b84aa8bf1ab1714d9416c0f53ea66204f
andy840119 <andy840119@gmail.com>
2023-01-17 21:37:08 +0800

    Implement section editor for able to edit the list of items in the section. Also, naming some methods by follow how RearrangeableListContainer<TModel> did.

---
+++ osu.Game.Rulesets.Karaoke/Screens/Edit/ISectionItemsEditorProvider.cs
@@ -0,0 +1,11 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+
+namespace osu.Game.Rulesets.Karaoke.Screens.Edit;
+
+public interface ISectionItemsEditorProvider
+{
+    void UpdateDisplayOrder(Drawable drawable, int order);
+}
