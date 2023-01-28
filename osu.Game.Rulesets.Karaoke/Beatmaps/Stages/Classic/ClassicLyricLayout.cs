406690a15dff4d9b6cd4d2e9ebb330c0209abec2
andy840119 <andy840119@gmail.com>
2023-01-28 23:49:45 +0800

    Make everything in the lyric layout as bindable. Also, because line and the offset and alignment might affect the line order, so will increase the version if change the value in here.

--- osu.Game.Rulesets.Karaoke/Beatmaps/Stages/Classic/ClassicLyricLayout.cs
+++ osu.Game.Rulesets.Karaoke/Beatmaps/Stages/Classic/ClassicLyricLayout.cs
@@ -1,12 +1,20 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using Newtonsoft.Json;
+using osu.Framework.Bindables;
 using osu.Game.Rulesets.Karaoke.Objects;
+using osu.Game.Rulesets.Karaoke.Utils;
 
 namespace osu.Game.Rulesets.Karaoke.Beatmaps.Stages.Classic;
 
 public class ClassicLyricLayout : IStageElement, IComparable<ClassicLyricLayout>
 {
+    private readonly Bindable<int> orderVersion = new();
+
+    public IBindable<int> OrderVersion => orderVersion;
+
     public ClassicLyricLayout(int id)
     {
         ID = id;
@@ -17,26 +25,66 @@ public class ClassicLyricLayout : IStageElement, IComparable<ClassicLyricLayout>
     /// </summary>
     public int ID { get; protected set; }
 
+    [JsonIgnore]
+    public readonly Bindable<string> NameBindable = new();
+
     /// <summary>
     /// Name of the element.
     /// </summary>
-    public string Name { get; set; } = string.Empty;
+    public string Name
+    {
+        get => NameBindable.Value;
+        set => NameBindable.Value = value;
+    }
+
+    [JsonIgnore]
+    public readonly Bindable<ClassicLyricLayoutAlignment> AlignmentBindable = new(ClassicLyricLayoutAlignment.Center);
 
     /// <summary>
     /// <see cref="Lyric"/>'s alignment.
     /// </summary>
-    public ClassicLyricLayoutAlignment Alignment { get; set; } = ClassicLyricLayoutAlignment.Center;
+    public ClassicLyricLayoutAlignment Alignment
+    {
+        get => AlignmentBindable.Value;
+        set
+        {
+            AlignmentBindable.Value = value;
+            orderVersion.Value++;
+        }
+    }
+
+    [JsonIgnore]
+    public readonly Bindable<float> HorizontalMarginBindable = new();
 
     /// <summary>
     /// <see cref="Lyric"/>'s horizontal margin.
     /// </summary>
-    public int HorizontalMargin { get; set; }
+    public float HorizontalMargin
+    {
+        get => HorizontalMarginBindable.Value;
+        set
+        {
+            HorizontalMarginBindable.Value = value;
+            orderVersion.Value++;
+        }
+    }
+
+    [JsonIgnore]
+    public readonly Bindable<int> LineBindable = new();
 
     /// <summary>
     /// <see cref="Lyric"/>'s line number.
     /// <see cref="Lyric"/> will at bottom if <see cref="Line"/> is zero.
     /// </summary>
-    public int Line { get; set; }
+    public int Line
+    {
+        get => LineBindable.Value;
+        set
+        {
+            LineBindable.Value = value;
+            orderVersion.Value++;
+        }
+    }
 
     public int CompareTo(ClassicLyricLayout? other)
     {
