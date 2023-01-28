0f174a429b17cf28209fc65228dbedc512027fac
andy840119 <andy840119@gmail.com>
2023-01-28 23:54:48 +0800

    Make everything in the lyric layout as bindable. Also, because line and the offset and alignment might affect the line order, so will increase the version if change the value in here.

--- osu.Game.Rulesets.Karaoke/Beatmaps/Stages/Classic/ClassicStyle.cs
+++ osu.Game.Rulesets.Karaoke/Beatmaps/Stages/Classic/ClassicStyle.cs
@@ -2,6 +2,8 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using Newtonsoft.Json;
+using osu.Framework.Bindables;
 using osu.Game.Rulesets.Karaoke.Objects;
 using osu.Game.Rulesets.Karaoke.Utils;
 
@@ -9,6 +11,10 @@ namespace osu.Game.Rulesets.Karaoke.Beatmaps.Stages.Classic;
 
 public class ClassicStyle : IStageElement, IComparable<ClassicStyle>
 {
+    private readonly Bindable<int> orderVersion = new();
+
+    public IBindable<int> OrderVersion => orderVersion;
+
     public ClassicStyle(int id)
     {
         ID = id;
@@ -19,20 +25,41 @@ public class ClassicStyle : IStageElement, IComparable<ClassicStyle>
     /// </summary>
     public int ID { get; protected set; }
 
+    [JsonIgnore]
+    public readonly Bindable<string> NameBindable = new();
+
     /// <summary>
     /// Name of the element.
     /// </summary>
-    public string Name { get; set; } = string.Empty;
+    public string Name
+    {
+        get => NameBindable.Value;
+        set => NameBindable.Value = value;
+    }
+
+    [JsonIgnore]
+    public readonly Bindable<int?> LyricStyleIndexBindable = new();
 
     /// <summary>
     /// <see cref="Lyric"/>'s skin lookup index.
     /// </summary>
-    public int? LyricStyleIndex { get; set; }
+    public int? LyricStyleIndex
+    {
+        get => LyricStyleIndexBindable.Value;
+        set => LyricStyleIndexBindable.Value = value;
+    }
+
+    [JsonIgnore]
+    public readonly Bindable<int?> NoteStyleIndexBindable = new();
 
     /// <summary>
     /// <see cref="Note"/>'s skin lookup index.
     /// </summary>
-    public int? NoteStyleIndex { get; set; }
+    public int? NoteStyleIndex
+    {
+        get => NoteStyleIndexBindable.Value;
+        set => NoteStyleIndexBindable.Value = value;
+    }
 
     public int CompareTo(ClassicStyle? other)
     {
