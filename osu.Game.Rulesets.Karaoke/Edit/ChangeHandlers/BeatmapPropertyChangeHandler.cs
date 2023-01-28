5667ddb681c7b3e55a0fcf2cf024044910db3f25
andy840119 <andy840119@gmail.com>
2023-01-28 13:15:22 +0800

    Should be able to interact with selected hit-object in the beatmap change handler.

--- osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/BeatmapPropertyChangeHandler.cs
+++ osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/BeatmapPropertyChangeHandler.cs
@@ -6,22 +6,31 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using osu.Framework.Allocation;
+using osu.Framework.Caching;
 using osu.Framework.Graphics;
 using osu.Game.Rulesets.Karaoke.Beatmaps;
 using osu.Game.Rulesets.Karaoke.Edit.Utils;
 using osu.Game.Rulesets.Karaoke.Objects;
+using osu.Game.Rulesets.Objects;
 using osu.Game.Screens.Edit;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers;
 
 public partial class BeatmapPropertyChangeHandler : Component
 {
+    private readonly Cached changingCache = new();
+
     [Resolved, AllowNull]
     private EditorBeatmap beatmap { get; set; }
 
     protected KaraokeBeatmap KaraokeBeatmap => EditorBeatmapUtils.GetPlayableBeatmap(beatmap);
 
     protected IEnumerable<Lyric> Lyrics => KaraokeBeatmap.HitObjects.OfType<Lyric>();
+
+    protected BeatmapPropertyChangeHandler()
+    {
+        changingCache.Validate();
+    }
 
     protected void PerformBeatmapChanged(Action<KaraokeBeatmap> action)
     {
@@ -39,6 +48,40 @@ public partial class BeatmapPropertyChangeHandler : Component
                 beatmap.EndChange();
 
             throw;
+        }
+    }
+
+    protected void PerformOnSelection<T>(Action<T> action) where T : HitObject
+    {
+        if (!changingCache.IsValid)
+            throw new NotSupportedException("Cannot trigger the change while applying another change.");
+
+        if (beatmap.SelectedHitObjects.Count == 0)
+            throw new NotSupportedException($"Should contain at least one selected {nameof(T)}");
+
+        changingCache.Invalidate();
+
+        try
+        {
+            // should trigger the UpdateState() in the editor beatmap only if there's no active state.
+            beatmap.PerformOnSelection(h =>
+            {
+                if (h is T tHitObject)
+                    action(tHitObject);
+            });
+        }
+        catch
+        {
+            // We should make sure that editor beatmap will end the change if still changing.
+            // will goes to here if have exception in the change handler.
+            if (beatmap.TransactionActive)
+                beatmap.EndChange();
+
+            throw;
+        }
+        finally
+        {
+            changingCache.Validate();
         }
     }
 }
