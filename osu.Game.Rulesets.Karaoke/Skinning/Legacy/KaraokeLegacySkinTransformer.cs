e062da530eea4f87307f492f2cf62d93861feab7
andy840119 <andy840119@gmail.com>
2023-03-02 19:28:52 +0800

    Should just use "as container" instead.
    See:
    https://github.com/ppy/osu/pull/22647/files

--- osu.Game.Rulesets.Karaoke/Skinning/Legacy/KaraokeLegacySkinTransformer.cs
+++ osu.Game.Rulesets.Karaoke/Skinning/Legacy/KaraokeLegacySkinTransformer.cs
@@ -9,6 +9,7 @@ using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Karaoke.UI.HUD;
 using osu.Game.Rulesets.Scoring;
 using osu.Game.Skinning;
+using Container = osu.Framework.Graphics.Containers.Container;
 
 namespace osu.Game.Rulesets.Karaoke.Skinning.Legacy
 {
@@ -34,7 +35,7 @@ namespace osu.Game.Rulesets.Karaoke.Skinning.Legacy
                     switch (targetComponent.Target)
                     {
                         case SkinComponentsContainerLookup.TargetArea.MainHUDComponents:
-                            var components = base.GetDrawableComponent(lookup) as SkinnableTargetComponentsContainer ?? getTargetComponentsContainerFromOtherPlace();
+                            var components = base.GetDrawableComponent(lookup) as Container ?? getTargetComponentsContainerFromOtherPlace();
                             components?.Add(new SettingButtonsDisplay
                             {
                                 Anchor = Anchor.CentreRight,
@@ -67,10 +68,10 @@ namespace osu.Game.Rulesets.Karaoke.Skinning.Legacy
                     return base.GetDrawableComponent(lookup);
             }
 
-            SkinnableTargetComponentsContainer? getTargetComponentsContainerFromOtherPlace() =>
+            Container? getTargetComponentsContainerFromOtherPlace() =>
                 Skin switch
                 {
-                    LegacySkin legacySkin => new TempLegacySkin(legacySkin.SkinInfo.Value).GetDrawableComponent(lookup) as SkinnableTargetComponentsContainer,
+                    LegacySkin legacySkin => new TempLegacySkin(legacySkin.SkinInfo.Value).GetDrawableComponent(lookup) as Container,
                     _ => throw new InvalidCastException()
                 };
         }
