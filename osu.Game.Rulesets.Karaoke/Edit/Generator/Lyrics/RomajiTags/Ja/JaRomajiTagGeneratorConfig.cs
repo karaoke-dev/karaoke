ef51fea7d8b85c97865c237a8c0cabe3c6c8183c
andy840119 <andy840119@gmail.com>
2023-03-04 16:16:52 +0800

    Use the setting source in the romaji generator config.

--- osu.Game.Rulesets.Karaoke/Edit/Generator/Lyrics/RomajiTags/Ja/JaRomajiTagGeneratorConfig.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Generator/Lyrics/RomajiTags/Ja/JaRomajiTagGeneratorConfig.cs
@@ -1,15 +1,13 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Bindables;
+
 namespace osu.Game.Rulesets.Karaoke.Edit.Generator.Lyrics.RomajiTags.Ja
 {
     public class JaRomajiTagGeneratorConfig : RomajiTagGeneratorConfig
     {
-        /// <summary>
-        /// Generate romaji as uppercase.
-        /// </summary>
-        public bool Uppercase { get; set; }
-
-        public JaRomajiTagGeneratorConfig CreateDefaultConfig() => new();
+        [ConfigSource("Uppercase", "Export romaji with uppercase.")]
+        public Bindable<bool> Uppercase { get; } = new BindableBool();
     }
 }
