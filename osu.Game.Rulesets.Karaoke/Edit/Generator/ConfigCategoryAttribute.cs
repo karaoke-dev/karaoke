22f0b51befa118c7cd6056bec7c975e42586b9db
andy840119 <andy840119@gmail.com>
2023-03-09 22:28:17 +0800

    Implement `IEquatable` for able to compare the category.

--- osu.Game.Rulesets.Karaoke/Edit/Generator/ConfigCategoryAttribute.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Generator/ConfigCategoryAttribute.cs
@@ -6,12 +6,31 @@ using osu.Framework.Localisation;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.Generator;
 
-public class ConfigCategoryAttribute : Attribute
+public class ConfigCategoryAttribute : Attribute, IEquatable<ConfigCategoryAttribute>
 {
     public LocalisableString Category { get; }
 
     public ConfigCategoryAttribute(string category)
     {
         Category = category;
+    }
+
+    public bool Equals(ConfigCategoryAttribute? other)
+    {
+        return Category == other?.Category;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj switch
+        {
+            ConfigCategoryAttribute category => Equals(category),
+            _ => false
+        };
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(base.GetHashCode(), Category);
     }
 }
