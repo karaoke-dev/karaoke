c0b4fd8bc09ce865f8074dc32d67cecbfdbc58f8
andy840119 <andy840119@gmail.com>
2023-03-28 21:27:00 +0800

    Should fill the start time and duration if got the invalid flag.

--- osu.Game.Rulesets.Karaoke/Beatmaps/KaraokeBeatmapProcessor.cs
+++ osu.Game.Rulesets.Karaoke/Beatmaps/KaraokeBeatmapProcessor.cs
@@ -5,6 +5,7 @@ using System;
 using System.Linq;
 using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Karaoke.Beatmaps.Patterns;
+using osu.Game.Rulesets.Karaoke.Beatmaps.Stages;
 using osu.Game.Rulesets.Karaoke.Objects;
 using osu.Game.Rulesets.Karaoke.Objects.Workings;
 
@@ -55,6 +56,18 @@ namespace osu.Game.Rulesets.Karaoke.Beatmaps
         {
             switch (flag)
             {
+                case LyricWorkingProperty.StartTime:
+                    lyric.StartTime = getStartTime();
+                    break;
+
+                case LyricWorkingProperty.Duration:
+                    lyric.Duration = getDuration();
+                    break;
+
+                case LyricWorkingProperty.Timing:
+                    // start time and duration should be set by other condition.
+                    break;
+
                 case LyricWorkingProperty.Page:
                     var pageInfo = Beatmap.PageInfo;
                     lyric.PageIndex = pageInfo.GetPageIndexAt(lyric.LyricStartTime);
@@ -66,6 +79,18 @@ namespace osu.Game.Rulesets.Karaoke.Beatmaps
 
                 default:
                     throw new ArgumentOutOfRangeException();
+            }
+
+            double getStartTime()
+            {
+                (double? startTime, double? _) = getWorkingStage()?.GetStartAndEndTime(lyric) ?? new Tuple<double?, double?>(null, null);
+                return startTime ?? 0;
+            }
+
+            double getDuration()
+            {
+                (double? startTime, double? endTime) = getWorkingStage()?.GetStartAndEndTime(lyric) ?? new Tuple<double?, double?>(null, null);
+                return endTime - startTime ?? 0;
             }
         }
 
@@ -86,6 +111,10 @@ namespace osu.Game.Rulesets.Karaoke.Beatmaps
                     throw new ArgumentOutOfRangeException();
             }
         }
+
+        // todo: should use better way to get the correct stage.
+        private StageInfo? getWorkingStage()
+            => Beatmap.StageInfos.FirstOrDefault();
 
         private Lyric? findLyricById(int? id) =>
             id == null ? null : Beatmap.HitObjects.OfType<Lyric>().Single(x => x.ID == id);
