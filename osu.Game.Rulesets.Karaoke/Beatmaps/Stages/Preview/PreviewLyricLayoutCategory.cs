9d2d6aab2365c711d79dde2639044430223e331d
andy840119 <andy840119@gmail.com>
2023-04-05 18:30:43 +0800

    Implement the preview lyric layout stage element and the category. There's no bindable in the element class because those property does not have the editor to edit them.

---
+++ osu.Game.Rulesets.Karaoke/Beatmaps/Stages/Preview/PreviewLyricLayoutCategory.cs
@@ -0,0 +1,16 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Karaoke.Objects;
+
+namespace osu.Game.Rulesets.Karaoke.Beatmaps.Stages.Preview;
+
+public class PreviewLyricLayoutCategory : StageElementCategory<PreviewLyricLayout, Lyric>
+{
+    protected override PreviewLyricLayout CreateElement(int id) => new(id);
+
+    public void ReCalculate()
+    {
+        // todo: do something.
+    }
+}
