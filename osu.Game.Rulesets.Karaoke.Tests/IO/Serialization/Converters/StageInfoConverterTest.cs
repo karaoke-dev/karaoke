fb4b1a90636fffe0a672713f0e20f60a51af3d97
andy840119 <andy840119@gmail.com>
2023-04-09 15:44:47 +0800

    Implement the serializer for able to save/load the default stage info.

--- osu.Game.Rulesets.Karaoke.Tests/IO/Serialization/Converters/StageInfoConverterTest.cs
+++ osu.Game.Rulesets.Karaoke.Tests/IO/Serialization/Converters/StageInfoConverterTest.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json;
 using NUnit.Framework;
 using osu.Game.Rulesets.Karaoke.Beatmaps.Stages;
 using osu.Game.Rulesets.Karaoke.Beatmaps.Stages.Classic;
+using osu.Game.Rulesets.Karaoke.Beatmaps.Stages.Preview;
 using osu.Game.Rulesets.Karaoke.IO.Serialization.Converters;
 using osu.Game.Rulesets.Karaoke.Tests.Asserts;
 
@@ -30,6 +31,26 @@ public class StageInfoConverterTest : BaseSingleConverterTest<StageInfoConverter
 
         var expected = new ClassicStageInfo();
         var actual = (ClassicStageInfo)JsonConvert.DeserializeObject<StageInfo>(json, CreateSettings())!;
+        ObjectAssert.ArePropertyEqual(expected, actual);
+    }
+
+    [Test]
+    public void TestPreviewStageInfoSerializer()
+    {
+        var stageInfo = new PreviewStageInfo();
+
+        const string expected = "{\"$type\":\"preview\",\"stage_definition\":{\"lines_of_lyric\":5,\"offset\":100.0}}";
+        string actual = JsonConvert.SerializeObject(stageInfo, CreateSettings());
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void TestPreviewStageInfoDeserializer()
+    {
+        const string json = "{\"$type\":\"preview\",\"stage_definition\":{\"lines_of_lyric\":5,\"offset\":100.0}}";
+
+        var expected = new PreviewStageInfo();
+        var actual = (PreviewStageInfo)JsonConvert.DeserializeObject<StageInfo>(json, CreateSettings())!;
         ObjectAssert.ArePropertyEqual(expected, actual);
     }
 }
