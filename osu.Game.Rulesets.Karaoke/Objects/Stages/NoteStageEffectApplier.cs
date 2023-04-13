4ab0846bdbf7b7ad25bcfe018f605b68e30711cd
andy840119 <andy840119@gmail.com>
2023-04-13 22:40:30 +0800

    Implement the base applier for the lyric and the note.

---
+++ osu.Game.Rulesets.Karaoke/Objects/Stages/NoteStageEffectApplier.cs
@@ -0,0 +1,15 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using osu.Game.Rulesets.Karaoke.Beatmaps.Stages;
+
+namespace osu.Game.Rulesets.Karaoke.Objects.Stages;
+
+public abstract class NoteStageEffectApplier : StageEffectApplier
+{
+    protected NoteStageEffectApplier(IEnumerable<StageElement> elements, StageDefinition definition)
+        : base(elements, definition)
+    {
+    }
+}
