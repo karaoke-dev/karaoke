bf3db052a8f21c90cc83b320b3152c3600f1734c
andy840119 <andy840119@gmail.com>
2023-04-14 23:39:19 +0800

    Define the applier method.

--- osu.Game.Rulesets.Karaoke/Objects/Stages/Classic/LyricClassicStageEffectApplier.cs
+++ osu.Game.Rulesets.Karaoke/Objects/Stages/Classic/LyricClassicStageEffectApplier.cs
@@ -2,8 +2,11 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
+using osu.Framework.Graphics.Transforms;
 using osu.Game.Rulesets.Karaoke.Beatmaps.Stages;
 using osu.Game.Rulesets.Karaoke.Beatmaps.Stages.Classic;
+using osu.Game.Rulesets.Karaoke.Objects.Drawables;
+using osu.Game.Rulesets.Objects.Drawables;
 
 namespace osu.Game.Rulesets.Karaoke.Objects.Stages.Classic;
 
@@ -12,5 +15,20 @@ public class LyricClassicStageEffectApplier : LyricStageEffectApplier<ClassicSta
     public LyricClassicStageEffectApplier(IEnumerable<StageElement> elements, ClassicStageDefinition definition)
         : base(elements, definition)
     {
+    }
+
+    protected override void UpdateInitialTransforms(TransformSequence<DrawableLyric> transformSequence, StageElement element)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void UpdateStartTimeStateTransforms(TransformSequence<DrawableLyric> transformSequence, StageElement element)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void UpdateHitStateTransforms(TransformSequence<DrawableLyric> transformSequence, ArmedState state, StageElement element)
+    {
+        throw new System.NotImplementedException();
     }
 }
--- osu.Game.Rulesets.Karaoke/Objects/Stages/StageEffectApplier.cs
+++ osu.Game.Rulesets.Karaoke/Objects/Stages/StageEffectApplier.cs
@@ -1,7 +1,10 @@
 // Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Transforms;
 using osu.Game.Rulesets.Karaoke.Beatmaps.Stages;
 using osu.Game.Rulesets.Objects.Drawables;
 
@@ -12,11 +15,84 @@ public abstract class StageEffectApplier<TStageDefinition, TDrawableHitObject> :
     where TDrawableHitObject : DrawableHitObject
 {
     private readonly IEnumerable<StageElement> elements;
-    private readonly TStageDefinition definition;
+
+    public readonly TStageDefinition Definition;
 
     protected StageEffectApplier(IEnumerable<StageElement> elements, TStageDefinition definition)
     {
         this.elements = elements;
-        this.definition = definition;
+        Definition = definition;
     }
+
+    /// <summary>
+    /// Apply (generally fade-in) transforms leading into the <see cref="KaraokeHitObject"/> start time.
+    /// </summary>
+    /// <param name="drawableHitObject"></param>
+    public void UpdateInitialTransforms(DrawableHitObject drawableHitObject)
+    {
+        if (drawableHitObject is not TDrawableHitObject drawable)
+            throw new InvalidCastException();
+
+        drawable.ClearTransforms();
+
+        var transform = drawable.FadeOut();
+
+        foreach (var element in elements)
+        {
+            // todo: not very sure how to combine the transform.
+            UpdateInitialTransforms(transform, element);
+        }
+    }
+
+    /// <summary>
+    /// Apply passive transforms at the <see cref="KaraokeHitObject"/>'s StartTime.
+    /// </summary>
+    /// <param name="drawableHitObject"></param>
+    public void UpdateStartTimeStateTransforms(DrawableHitObject drawableHitObject)
+    {
+        if (drawableHitObject is not TDrawableHitObject drawable)
+            throw new InvalidCastException();
+
+        drawable.ClearTransforms();
+
+        var transform = drawable.Delay(0);
+
+        foreach (var element in elements)
+        {
+            // todo: not very sure how to combine the transform.
+            UpdateStartTimeStateTransforms(transform, element);
+        }
+
+        drawable.FadeIn();
+    }
+
+    /// <summary>
+    /// Apply transforms based on the current <see cref="ArmedState"/>.
+    /// This call is offset by (HitObject.EndTime + Result.Offset), equivalent to when the user hit the object.
+    /// </summary>
+    /// <param name="drawableHitObject"></param>
+    /// <param name="state"></param>
+    public void UpdateHitStateTransforms(DrawableHitObject drawableHitObject, ArmedState state)
+    {
+        if (drawableHitObject is not TDrawableHitObject drawable)
+            throw new InvalidCastException();
+
+        drawable.ClearTransforms();
+
+        var transform = drawable.Delay(0);
+
+        foreach (var element in elements)
+        {
+            // todo: not very sure how to combine the transform.
+            UpdateHitStateTransforms(transform, state, element);
+        }
+
+        drawable.FadeOut();
+    }
+
+    protected abstract void UpdateInitialTransforms(TransformSequence<TDrawableHitObject> transformSequence, StageElement element);
+
+    protected abstract void UpdateStartTimeStateTransforms(TransformSequence<TDrawableHitObject> transformSequence, StageElement element);
+
+    protected abstract void UpdateHitStateTransforms(TransformSequence<TDrawableHitObject> transformSequence, ArmedState state, StageElement element);
 }
