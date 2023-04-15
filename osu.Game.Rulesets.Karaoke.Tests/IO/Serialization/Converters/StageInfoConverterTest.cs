3f3c6e2ffefa04cc7a28824a66f4146d30777214
andy840119 <andy840119@gmail.com>
2023-04-15 22:27:27 +0800

    Fix the test case broken.

--- osu.Game.Rulesets.Karaoke.Tests/IO/Serialization/Converters/StageInfoConverterTest.cs
+++ osu.Game.Rulesets.Karaoke.Tests/IO/Serialization/Converters/StageInfoConverterTest.cs
@@ -39,7 +39,7 @@ public class StageInfoConverterTest : BaseSingleConverterTest<StageInfoConverter
     {
         var stageInfo = new PreviewStageInfo();
 
-        const string expected = "{\"$type\":\"preview\",\"stage_definition\":{\"blue_level\":0.5,\"dim_level\":0.5,\"fading_time\":300.0,\"fading_offset_position\":64.0,\"fade_in_easing\":21,\"fade_out_easing\":21,\"moving_in_easing\":21,\"move_out_easing\":21,\"inactive_alpha\":0.5,\"active_easing\":21,\"inactive_easing\":21,\"number_of_lyrics\":5,\"lyric_height\":64.0,\"line_moving_time\":350.0,\"line_moving_easing\":22,\"line_moving_offset_time\":50.0}}";
+        const string expected = "{\"$type\":\"preview\",\"stage_definition\":{\"blue_level\":0.5,\"dim_level\":0.5,\"fading_time\":300.0,\"fading_offset_position\":64.0,\"fade_in_easing\":22,\"fade_out_easing\":22,\"moving_in_easing\":22,\"move_out_easing\":22,\"inactive_alpha\":0.3,\"active_time\":350.0,\"active_easing\":22,\"number_of_lyrics\":5,\"lyric_height\":64.0,\"line_moving_time\":500.0,\"line_moving_easing\":22,\"line_moving_offset_time\":50.0}}";
         string actual = JsonConvert.SerializeObject(stageInfo, CreateSettings());
         Assert.AreEqual(expected, actual);
     }
@@ -47,7 +47,7 @@ public class StageInfoConverterTest : BaseSingleConverterTest<StageInfoConverter
     [Test]
     public void TestPreviewStageInfoDeserializer()
     {
-        const string json = "{\"$type\":\"preview\",\"stage_definition\":{\"blue_level\":0.5,\"dim_level\":0.5,\"fading_time\":300.0,\"fading_offset_position\":64.0,\"fade_in_easing\":21,\"fade_out_easing\":21,\"moving_in_easing\":21,\"move_out_easing\":21,\"inactive_alpha\":0.5,\"active_easing\":21,\"inactive_easing\":21,\"number_of_lyrics\":5,\"lyric_height\":64.0,\"line_moving_time\":350.0,\"line_moving_easing\":22,\"line_moving_offset_time\":50.0}}";
+        const string json = "{\"$type\":\"preview\",\"stage_definition\":{\"blue_level\":0.5,\"dim_level\":0.5,\"fading_time\":300.0,\"fading_offset_position\":64.0,\"fade_in_easing\":22,\"fade_out_easing\":22,\"moving_in_easing\":22,\"move_out_easing\":22,\"inactive_alpha\":0.3,\"active_time\":350.0,\"active_easing\":22,\"number_of_lyrics\":5,\"lyric_height\":64.0,\"line_moving_time\":500.0,\"line_moving_easing\":22,\"line_moving_offset_time\":50.0}}";
 
         var expected = new PreviewStageInfo();
         var actual = (PreviewStageInfo)JsonConvert.DeserializeObject<StageInfo>(json, CreateSettings())!;
