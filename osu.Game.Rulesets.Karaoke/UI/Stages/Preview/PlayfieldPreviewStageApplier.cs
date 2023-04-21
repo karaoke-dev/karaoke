25eaf40d6d601b72a2dda10029b8eeac284fe6c8
andy840119 <andy840119@gmail.com>
2023-04-21 21:13:54 +0800

    Adjust the position for better experience.

--- osu.Game.Rulesets.Karaoke/UI/Stages/Preview/PlayfieldPreviewStageApplier.cs
+++ osu.Game.Rulesets.Karaoke/UI/Stages/Preview/PlayfieldPreviewStageApplier.cs
@@ -21,26 +21,31 @@ public class PlayfieldPreviewStageApplier : PlayfieldStageApplier<PreviewStageDe
     {
         transformSequence.TransformAddStageComponent(new BeatmapCoverInfo
         {
-            Size = new Vector2(displayNotePlayfield ? 200 : 300),
+            Size = new Vector2(displayNotePlayfield ? 200 : 400),
             Anchor = Anchor.Centre,
             Origin = Anchor.Centre,
-            X = -200,
+            X = displayNotePlayfield ? -350 : -250,
             Y = displayNotePlayfield ? 100 : 0,
         }).FadeIn(300);
     }
 
     protected override void UpdateLyricPlayfieldArrangement(TransformSequence<LyricPlayfield> transformSequence, bool displayNotePlayfield)
     {
-        transformSequence.TransformTo(nameof(LyricPlayfield.Anchor), Anchor.BottomRight)
-                         .TransformTo(nameof(LyricPlayfield.Origin), Anchor.BottomRight)
+        transformSequence.TransformTo(nameof(LyricPlayfield.Anchor), Anchor.Centre)
+                         .TransformTo(nameof(LyricPlayfield.Origin), Anchor.TopLeft)
                          .TransformTo(nameof(LyricPlayfield.Size), new Vector2(0.5f))
+                         .MoveToX(displayNotePlayfield ? -214 : 0) // lyric and the beatmap cover should be closer if has note playfield.
+                         .MoveToY(displayNotePlayfield ? 0 : -32) // lyric playfield should be upper if there's no note playfield.
                          .Then()
                          .FadeIn(100);
     }
 
     protected override void UpdateNotePlayfieldArrangement(TransformSequence<ScrollingNotePlayfield> transformSequence)
     {
-        transformSequence.TransformTo(nameof(LyricPlayfield.Padding), new MarginPadding(50))
+        transformSequence.TransformTo(nameof(LyricPlayfield.Anchor), Anchor.Centre)
+                         .TransformTo(nameof(LyricPlayfield.Origin), Anchor.Centre)
+                         .MoveToY(-200)
+                         .TransformTo(nameof(LyricPlayfield.Padding), new MarginPadding(50))
                          .Then()
                          .FadeIn(100);
     }
