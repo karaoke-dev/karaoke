229f2cbbf9a03eea8bca624f5f6422564ade696f
andy840119 <andy840119@gmail.com>
2023-04-22 14:59:26 +0800

    Implement the base stage info generator. Note that every stage info should have it's own generator, even generate result is still need to manual adjust.

---
+++ osu.Game.Rulesets.Karaoke/Edit/Generator/Beatmaps/Stages/StageInfoGenerator.cs
@@ -0,0 +1,15 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Karaoke.Beatmaps.Stages;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Generator.Beatmaps.Stages;
+
+public abstract class StageInfoGenerator<TStageInfoConfig> : BeatmapPropertyGenerator<StageInfo, TStageInfoConfig>
+    where TStageInfoConfig : StageInfoGeneratorConfig, new()
+{
+    protected StageInfoGenerator(TStageInfoConfig config)
+        : base(config)
+    {
+    }
+}
