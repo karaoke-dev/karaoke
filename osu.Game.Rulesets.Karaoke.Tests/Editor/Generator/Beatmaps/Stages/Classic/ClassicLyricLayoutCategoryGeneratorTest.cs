8a2518518c092e2696061642c059be0f33c4c730
andy840119 <andy840119@gmail.com>
2023-04-22 23:15:42 +0800

    Implement the generator for able to generate the `ClassicLyricLayoutCategory`.

---
+++ osu.Game.Rulesets.Karaoke.Tests/Editor/Generator/Beatmaps/Stages/Classic/ClassicLyricLayoutCategoryGeneratorTest.cs
@@ -0,0 +1,141 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using NUnit.Framework;
+using osu.Game.Rulesets.Karaoke.Beatmaps;
+using osu.Game.Rulesets.Karaoke.Beatmaps.Stages.Classic;
+using osu.Game.Rulesets.Karaoke.Edit.Generator.Beatmaps.Stages.Classic;
+using osu.Game.Rulesets.Karaoke.Objects;
+using osu.Game.Rulesets.Karaoke.Tests.Helper;
+
+namespace osu.Game.Rulesets.Karaoke.Tests.Editor.Generator.Beatmaps.Stages.Classic;
+
+public class ClassicLyricLayoutCategoryGeneratorTest
+    : BaseLyricStageElementCategoryGeneratorTest<ClassicLyricLayoutCategoryGenerator, ClassicLyricLayoutCategory, ClassicLyricLayout, ClassicLyricLayoutCategoryGeneratorConfig>
+{
+    [Test]
+    public void TestCanGenerate()
+    {
+        var config = GeneratorDefaultConfig();
+        var beatmap = new KaraokeBeatmap
+        {
+            HitObjects = new List<KaraokeHitObject>
+            {
+                TestCaseTagHelper.ParseLyric("[1000,3000]:lyric1"),
+                TestCaseTagHelper.ParseLyric("[4000,6000]:lyric2"),
+                TestCaseTagHelper.ParseLyric("[7000,9000]:lyric3"),
+                TestCaseTagHelper.ParseLyric("[10000,12000]:lyric4"),
+            }
+        };
+
+        CheckCanGenerate(beatmap, true, config);
+    }
+
+    [Test]
+    public void TestCanGenerateWithNonLyricBeatmap()
+    {
+        var config = GeneratorDefaultConfig();
+        var beatmap = new KaraokeBeatmap();
+        CheckCanGenerate(beatmap, false, config);
+    }
+
+    [Test]
+    public void TestGenerate()
+    {
+        var config = GeneratorDefaultConfig();
+
+        var lyric1 = TestCaseTagHelper.ParseLyric("[1000,3000]:lyric1", 1);
+        var lyric2 = TestCaseTagHelper.ParseLyric("[4000,6000]:lyric2", 2);
+        var lyric3 = TestCaseTagHelper.ParseLyric("[7000,9000]:lyric3", 3);
+        var lyric4 = TestCaseTagHelper.ParseLyric("[10000,12000]:lyric4", 4);
+        var beatmap = new KaraokeBeatmap
+        {
+            HitObjects = new List<KaraokeHitObject>
+            {
+                lyric1,
+                lyric2,
+                lyric3,
+                lyric4,
+            }
+        };
+
+        var expected = new ClassicLyricLayoutCategory();
+        var layout1 = expected.AddElement();
+        var layout2 = expected.AddElement();
+        expected.AddToMapping(layout1, lyric1);
+        expected.AddToMapping(layout2, lyric2);
+        expected.AddToMapping(layout1, lyric3);
+        expected.AddToMapping(layout2, lyric4);
+
+        CheckGenerateResult(beatmap, expected, config);
+    }
+
+    [Test]
+    public void TestGenerateWithThreeLyrics()
+    {
+        var config = GeneratorDefaultConfig(x => x.LyricRowAmount.Value = 3);
+
+        var lyric1 = TestCaseTagHelper.ParseLyric("[1000,3000]:lyric1", 1);
+        var lyric2 = TestCaseTagHelper.ParseLyric("[4000,6000]:lyric2", 2);
+        var lyric3 = TestCaseTagHelper.ParseLyric("[7000,9000]:lyric3", 3);
+        var lyric4 = TestCaseTagHelper.ParseLyric("[10000,12000]:lyric4", 4);
+        var lyric5 = TestCaseTagHelper.ParseLyric("[10000,12000]:lyric5", 5);
+        var beatmap = new KaraokeBeatmap
+        {
+            HitObjects = new List<KaraokeHitObject>
+            {
+                lyric1,
+                lyric2,
+                lyric3,
+                lyric4,
+                lyric5,
+            }
+        };
+
+        var expected = new ClassicLyricLayoutCategory();
+        var layout1 = expected.AddElement();
+        var layout2 = expected.AddElement();
+        var layout3 = expected.AddElement();
+        expected.AddToMapping(layout1, lyric1);
+        expected.AddToMapping(layout2, lyric2);
+        expected.AddToMapping(layout3, lyric3);
+        expected.AddToMapping(layout1, lyric4);
+        expected.AddToMapping(layout2, lyric5);
+
+        CheckGenerateResult(beatmap, expected, config);
+    }
+
+    [Test]
+    public void TestGenerateWithNotMapping()
+    {
+        var config = GeneratorDefaultConfig(x => x.ApplyMappingToTheLyric.Value = false);
+
+        var lyric1 = TestCaseTagHelper.ParseLyric("[1000,3000]:lyric1", 1);
+        var lyric2 = TestCaseTagHelper.ParseLyric("[4000,6000]:lyric2", 2);
+        var lyric3 = TestCaseTagHelper.ParseLyric("[7000,9000]:lyric3", 3);
+        var lyric4 = TestCaseTagHelper.ParseLyric("[10000,12000]:lyric4", 4);
+        var beatmap = new KaraokeBeatmap
+        {
+            HitObjects = new List<KaraokeHitObject>
+            {
+                lyric1,
+                lyric2,
+                lyric3,
+                lyric4,
+            }
+        };
+
+        var expected = new ClassicLyricLayoutCategory();
+        expected.AddElement();
+        expected.AddElement();
+
+        CheckGenerateResult(beatmap, expected, config);
+    }
+
+    protected override void AssertEqual(ClassicLyricLayout expected, ClassicLyricLayout actual)
+    {
+        // we did not care about the default value that generator created.
+        Assert.AreEqual(expected.ID, actual.ID);
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/Edit/Generator/Beatmaps/Stages/Classic/ClassicLyricLayoutCategoryGeneratorConfig.cs
@@ -0,0 +1,36 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Bindables;
+using osu.Game.Rulesets.Karaoke.Beatmaps.Stages.Classic;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Generator.Beatmaps.Stages.Classic;
+
+public class ClassicLyricLayoutCategoryGeneratorConfig : GeneratorConfig
+{
+    /// <summary>
+    /// How may lyric can be in the stage at the same time.
+    /// </summary>
+    [ConfigSource("Lyric amount", "How may lyric can be in the stage at the same time.")]
+    public BindableInt LyricRowAmount { get; } = new(2)
+    {
+        MinValue = 2,
+        MaxValue = 4,
+    };
+
+    /// <summary>
+    /// Should auto-create the mapping to the lyric or mapping by user.
+    /// </summary>
+    [ConfigSource("Apply mapping to the lyric", "Auto-apply the mapping or mapping by user.")]
+    public BindableBool ApplyMappingToTheLyric { get; } = new(true);
+
+    /// <summary>
+    /// Adjust the <see cref="ClassicLyricLayout.HorizontalMargin"/> in the <see cref="ClassicLyricLayout"/>
+    /// </summary>
+    [ConfigSource("Horizontal margin", "The margin between lyric and the border of the playfield.")]
+    public BindableFloat HorizontalMargin { get; } = new(50)
+    {
+        MinValue = 32,
+        MaxValue = 100
+    };
+}
