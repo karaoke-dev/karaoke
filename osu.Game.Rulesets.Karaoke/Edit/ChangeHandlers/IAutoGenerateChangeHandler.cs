6f03fe6b04e7941f4cdf0a4e9fd1fc553101fddd
andy840119 <andy840119@gmail.com>
2023-04-24 22:49:41 +0800

    Adjust the interface. This interface is focus on Generate the item by the template type.

--- osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/IAutoGenerateChangeHandler.cs
+++ osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/IAutoGenerateChangeHandler.cs
@@ -1,19 +1,17 @@
 // Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
-
 namespace osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers;
 
 /// <summary>
 /// This interface is defined checking able to generate or detect the property, and make the change for the property.
 /// </summary>
-/// <typeparam name="TEnum"></typeparam>
-public interface IAutoGenerateChangeHandler<in TEnum> where TEnum : struct, Enum
+/// <typeparam name="TType"></typeparam>
+public interface IAutoGenerateChangeHandler<in TType>
 {
-    bool CanGenerate(TEnum category);
+    bool CanGenerate<T>() where T : TType;
 
-    void AutoGenerate(TEnum category);
+    void AutoGenerate<T>() where T : TType;
 }
 
 /// <summary>
