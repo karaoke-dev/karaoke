e43b2d0a70c9ea94ef3a89f02acd2f4f92c736ed
andy840119 <andy840119@gmail.com>
2023-04-29 16:31:32 +0800

    Remove the logic in the legacy skin transformers.

--- osu.Game.Rulesets.Karaoke/Skinning/Legacy/KaraokeLegacySkinTransformer.cs
+++ osu.Game.Rulesets.Karaoke/Skinning/Legacy/KaraokeLegacySkinTransformer.cs
@@ -6,10 +6,8 @@ using System.ComponentModel;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Game.Beatmaps;
-using osu.Game.Rulesets.Karaoke.UI.HUD;
 using osu.Game.Rulesets.Scoring;
 using osu.Game.Skinning;
-using Container = osu.Framework.Graphics.Containers.Container;
 
 namespace osu.Game.Rulesets.Karaoke.Skinning.Legacy
 {
@@ -31,22 +29,6 @@ namespace osu.Game.Rulesets.Karaoke.Skinning.Legacy
         {
             switch (lookup)
             {
-                case SkinComponentsContainerLookup targetComponent:
-                    switch (targetComponent.Target)
-                    {
-                        case SkinComponentsContainerLookup.TargetArea.MainHUDComponents:
-                            var components = base.GetDrawableComponent(lookup) as Container ?? getTargetComponentsContainerFromOtherPlace();
-                            components?.Add(new SettingButtonsDisplay
-                            {
-                                Anchor = Anchor.CentreRight,
-                                Origin = Anchor.CentreRight,
-                            });
-                            return components;
-
-                        default:
-                            return base.GetDrawableComponent(lookup);
-                    }
-
                 case GameplaySkinComponentLookup<HitResult> resultComponent:
                     return getResult(resultComponent.Component);
 
@@ -67,13 +49,6 @@ namespace osu.Game.Rulesets.Karaoke.Skinning.Legacy
                 default:
                     return base.GetDrawableComponent(lookup);
             }
-
-            Container? getTargetComponentsContainerFromOtherPlace() =>
-                Skin switch
-                {
-                    LegacySkin legacySkin => new TempLegacySkin(legacySkin.SkinInfo.Value).GetDrawableComponent(lookup) as Container,
-                    _ => throw new InvalidCastException()
-                };
         }
 
         private Drawable? getResult(HitResult result)
@@ -84,14 +59,5 @@ namespace osu.Game.Rulesets.Karaoke.Skinning.Legacy
 
         public override IBindable<TValue>? GetConfig<TLookup, TValue>(TLookup lookup)
             => karaokeSkin.GetConfig<TLookup, TValue>(lookup);
-
-        // it's a temp class for just getting SkinnableTarget.MainHUDComponents
-        private class TempLegacySkin : LegacySkin
-        {
-            public TempLegacySkin(SkinInfo skin)
-                : base(skin, null, null)
-            {
-            }
-        }
     }
 }
