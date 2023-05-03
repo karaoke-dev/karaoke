b09483fe90b8cbade15475883d29fafdc02097c3
andy840119 <andy840119@gmail.com>
2023-05-03 22:51:20 +0800

    Implement the caret position algorithm for able to navigate to the lyric with target char.

---
+++ osu.Game.Rulesets.Karaoke/Screens/Edit/Beatmaps/Lyrics/CaretPosition/Algorithms/CharIndexCaretPositionAlgorithm.cs
@@ -0,0 +1,20 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Karaoke.Objects;
+
+namespace osu.Game.Rulesets.Karaoke.Screens.Edit.Beatmaps.Lyrics.CaretPosition.Algorithms;
+
+public class CharIndexCaretPositionAlgorithm : TextCaretPositionAlgorithm<CharIndexCaretPosition>
+{
+    public CharIndexCaretPositionAlgorithm(Lyric[] lyrics)
+        : base(lyrics)
+    {
+    }
+
+    protected override CharIndexCaretPosition CreateCaretPosition(Lyric lyric, int index, CaretGenerateType generateType = CaretGenerateType.Action) => new(lyric, index, generateType);
+
+    protected override int GetMinIndex(string text) => 0;
+
+    protected override int GetMaxIndex(string text) => text.Length - 1;
+}
---
+++ osu.Game.Rulesets.Karaoke/Screens/Edit/Beatmaps/Lyrics/CaretPosition/CharIndexCaretPosition.cs
@@ -0,0 +1,22 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Karaoke.Objects;
+
+namespace osu.Game.Rulesets.Karaoke.Screens.Edit.Beatmaps.Lyrics.CaretPosition;
+
+public readonly struct CharIndexCaretPosition : ITextCaretPosition
+{
+    public CharIndexCaretPosition(Lyric lyric, int charIndex, CaretGenerateType generateType = CaretGenerateType.Action)
+    {
+        Lyric = lyric;
+        Index = charIndex;
+        GenerateType = generateType;
+    }
+
+    public Lyric Lyric { get; }
+
+    public int Index { get; }
+
+    public CaretGenerateType GenerateType { get; }
+}
