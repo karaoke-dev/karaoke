2b3c8958c262985b9f29fffcc7db4cf3c35d3a9a
andy840119 <andy840119@gmail.com>
2023-05-12 12:35:17 +0800

    Create the stage applier for the preview lyric in the setting menu. This applier might be able to use in the skin editor also.

---
+++ osu.Game.Rulesets.Karaoke/Screens/Settings/Previews/Gameplay/PreviewLyricStageDefinition.cs
@@ -0,0 +1,13 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Karaoke.Beatmaps.Stages;
+
+namespace osu.Game.Rulesets.Karaoke.Screens.Settings.Previews.Gameplay;
+
+/// <summary>
+/// Definition for preview single lyric.
+/// </summary>
+public class PreviewLyricStageDefinition : StageDefinition
+{
+}
---
+++ osu.Game.Rulesets.Karaoke/Screens/Settings/Previews/Gameplay/PreviewLyricStageElement.cs
@@ -0,0 +1,14 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Karaoke.Beatmaps.Stages;
+
+namespace osu.Game.Rulesets.Karaoke.Screens.Settings.Previews.Gameplay;
+
+public class PreviewLyricStageElement : StageElement
+{
+    public PreviewLyricStageElement()
+        : base(0)
+    {
+    }
+}
