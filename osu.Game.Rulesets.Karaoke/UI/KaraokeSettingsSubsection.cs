e126a11c98d4f197125f6c04f91f3649c9a1d8a8
andy840119 <andy840119@gmail.com>
2023-05-12 14:43:34 +0800

    Remove the config from the main setting because main screen does not support popover.

--- osu.Game.Rulesets.Karaoke/UI/KaraokeSettingsSubsection.cs
+++ osu.Game.Rulesets.Karaoke/UI/KaraokeSettingsSubsection.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Globalization;
 using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
@@ -65,12 +64,6 @@ public partial class KaraokeSettingsSubsection : RulesetSettingsSubsection
                 LabelText = KaraokeSettingsSubsectionStrings.Translate,
                 TooltipText = KaraokeSettingsSubsectionStrings.TranslateTooltip,
                 Current = config.GetBindable<bool>(KaraokeRulesetSetting.UseTranslate)
-            },
-            new SettingsLanguage
-            {
-                LabelText = KaraokeSettingsSubsectionStrings.PreferLanguage,
-                TooltipText = KaraokeSettingsSubsectionStrings.PreferLanguageTooltip,
-                Current = config.GetBindable<CultureInfo>(KaraokeRulesetSetting.PreferLanguage)
             },
             // Device
             new SettingsMicrophoneDeviceDropdown
