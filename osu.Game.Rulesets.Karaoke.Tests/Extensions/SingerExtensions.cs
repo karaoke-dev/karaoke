f1ea47779acdd833ef95cf64ff929d910ae52730
andy840119 <andy840119@gmail.com>
2023-07-02 19:42:20 +0800

    Create the testing tool to override the singer id.

---
+++ osu.Game.Rulesets.Karaoke.Tests/Extensions/SingerExtensions.cs
@@ -0,0 +1,24 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using osu.Game.Rulesets.Karaoke.Beatmaps;
+using osu.Game.Rulesets.Karaoke.Beatmaps.Metadatas.Types;
+
+namespace osu.Game.Rulesets.Karaoke.Tests.Extensions;
+
+public static class SingerExtensions
+{
+    public static TSinger ChangeId<TSinger>(this TSinger singer, ElementId id)
+        where TSinger : ISinger
+    {
+        // get id from the singer and override the id.
+        var propertyInfo = singer.GetType().GetProperty(nameof(ISinger.ID));
+        if (propertyInfo == null)
+            throw new InvalidOperationException();
+
+        propertyInfo.SetValue(singer, id);
+
+        return singer;
+    }
+}
