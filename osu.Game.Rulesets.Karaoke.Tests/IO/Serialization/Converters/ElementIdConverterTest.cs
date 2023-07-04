d7d27e5a9fa957b6d63f3097eda4b91f65975d8c
andy840119 <andy840119@gmail.com>
2023-07-04 23:24:47 +0800

    Let the converter able to support the null case.

--- osu.Game.Rulesets.Karaoke.Tests/IO/Serialization/Converters/ElementIdConverterTest.cs
+++ osu.Game.Rulesets.Karaoke.Tests/IO/Serialization/Converters/ElementIdConverterTest.cs
@@ -12,7 +12,7 @@ public class ElementIdConverterTest : BaseSingleConverterTest<ElementIdConverter
 {
     [TestCase("1234567", "\"1234567\"")]
     [TestCase("", "\"\"")]
-    [TestCase(null, "null")]
+    [TestCase(null, "null")] // support the case with nullable property.
     public void TestSerialize(string? id, string json)
     {
         var elementId = createElementId(id);
@@ -22,22 +22,19 @@ public class ElementIdConverterTest : BaseSingleConverterTest<ElementIdConverter
 
     [TestCase("\"1234567\"", "1234567")]
     [TestCase("\"\"", "")]
-    [TestCase("null", "")] // should make sure that create the ElementId.Empty if not receive the id.
+    [TestCase("null", null)] // support the case with nullable property.
     public void TestDeserialize(string json, string? id)
     {
         var expected = createElementId(id);
-        var result = JsonConvert.DeserializeObject<ElementId>(json, CreateSettings());
+        var result = JsonConvert.DeserializeObject<ElementId?>(json, CreateSettings());
         Assert.AreEqual(expected, result);
     }
 
-    private static ElementId? createElementId(string? str)
-    {
-        if (str == null)
-            return null;
-
-        if (str == string.Empty)
-            return ElementId.Empty;
-
-        return new ElementId(str);
-    }
+    private static ElementId? createElementId(string? str) =>
+        str switch
+        {
+            null => null,
+            "" => ElementId.Empty,
+            _ => new ElementId(str)
+        };
 }
