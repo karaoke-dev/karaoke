22668016a977192cc42e0a9348c2df5941ea825d
andy840119 <andy840119@gmail.com>
2023-07-21 00:24:29 +0800

    Re-write the helper for more flexibility.

--- osu.Game.Rulesets.Karaoke.Tests/Helper/TestCaseTagHelper.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Helper/TestCaseTagHelper.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -18,157 +19,203 @@ namespace osu.Game.Rulesets.Karaoke.Tests.Helper;
 
 public static class TestCaseTagHelper
 {
+    private const string char_index_range_str = @"\[(?<start>[-0-9]+)(?:,(?<end>[-0-9]+))?\]";
+    private const string time_index_regex = @"\[(?<index>[-0-9]+),(?<state>start|end)]";
+    private const string time_range_str = @"\[(?<startTime>[-0-9]+)(?:,(?<endTime>[-0-9]+))?\]";
+    private const string id_str = "(?<id>[-0-9]+)]";
+
+    private static string getStringPropertyRegex(char prefix, string propertyName)
+        => @$"(?:{prefix}(?<{propertyName}>[^\s]+))?";
+
+    private static string getNumberPropertyRegex(char prefix, string propertyName)
+        => $"(?:{prefix}(?<{propertyName}>[-0-9]+|s*|))?";
+
+    private static string generateRegex(string regexPrefix, IEnumerable<string> regexProperties)
+        => regexPrefix + string.Join("", regexProperties);
+
+    private static TObject getMatchByStatement<TObject>(string? str, string regexStr, Func<Match?, TObject> returnValue)
+    {
+        if (string.IsNullOrEmpty(str))
+            return returnValue(null);
+
+        var regex = new Regex(regexStr);
+        var result = regex.Match(str);
+        if (!result.Success)
+            throw new RegexMatchTimeoutException(nameof(str));
+
+        return returnValue(result);
+    }
+
     /// <summary>
     /// Process test case ruby string format into <see cref="RubyTag"/>
     /// </summary>
     /// <example>
-    /// [0]:ruby
-    /// [0,3]:ruby
+    /// [0]:ruby        -> has range index with text. <br/>
+    /// [0,3]:ruby      -> has range index with text. <br/>
+    /// [0]:            -> has range index with empty text. <br/>
+    /// [0,3]:          -> has range index with empty text. <br/>
+    /// [0]             -> has range index with empty text. <br/>
+    /// [0,3]           -> has range index with empty text. <br/>
     /// </example>
     /// <param name="str">Ruby tag string format</param>
     /// <returns><see cref="RubyTag"/>Ruby tag object</returns>
     public static RubyTag ParseRubyTag(string? str)
     {
-        if (string.IsNullOrEmpty(str))
-            return new RubyTag();
-
-        var regex = new Regex("\\[(?<start>[-0-9]+)(?:,(?<end>[-0-9]+))?\\]:(?<ruby>.*$)");
-        var result = regex.Match(str);
-        if (!result.Success)
-            throw new RegexMatchTimeoutException(nameof(str));
-
-        string startValue = result.Groups["start"].Value;
-        string endValue = result.Groups["end"].Value;
-
-        int startIndex = int.Parse(startValue);
-        int endIndex = int.Parse(string.IsNullOrEmpty(endValue) ? startValue : endValue);
-        string text = result.Groups["ruby"].Value;
-
-        return new RubyTag
+        string regex = generateRegex(char_index_range_str, new[]
         {
-            StartIndex = startIndex,
-            EndIndex = endIndex,
-            Text = text,
-        };
+            getStringPropertyRegex(':', "ruby"),
+        });
+
+        return getMatchByStatement<RubyTag>(str, regex, result =>
+        {
+            if (result == null)
+                return new RubyTag();
+
+            int startIndex = result.GetGroupValue<int>("start");
+            int? endIndex = result.GetGroupValue<int?>("end");
+            string text = result.GetGroupValue<string>("ruby");
+
+            return new RubyTag
+            {
+                StartIndex = startIndex,
+                EndIndex = endIndex ?? startIndex,
+                Text = text,
+            };
+        });
     }
 
     /// <summary>
     /// Process test case romaji string format into <see cref="RomajiTag"/>
     /// </summary>
     /// <example>
-    /// [0]:romaji
-    /// [0,3]:romaji
+    /// [0]:romaji          -> has range index with text. <br/>
+    /// [0,3]:romaji        -> has range index with text. <br/>
+    /// [0]:                -> has range index with empty text. <br/>
+    /// [0,3]:              -> has range index with empty text. <br/>
+    /// [0]                 -> has range index with empty text. <br/>
+    /// [0,3]               -> has range index with empty text. <br/>
     /// </example>
     /// <param name="str">Romaji tag string format</param>
     /// <returns><see cref="RomajiTag"/>Romaji tag object</returns>
     public static RomajiTag ParseRomajiTag(string? str)
     {
-        if (string.IsNullOrEmpty(str))
-            return new RomajiTag();
-
-        var regex = new Regex("\\[(?<start>[-0-9]+)(?:,(?<end>[-0-9]+))?\\]:(?<romaji>.*$)");
-        var result = regex.Match(str);
-        if (!result.Success)
-            throw new RegexMatchTimeoutException(nameof(str));
-
-        string startValue = result.Groups["start"].Value;
-        string endValue = result.Groups["end"].Value;
-
-        int startIndex = int.Parse(startValue);
-        int endIndex = int.Parse(string.IsNullOrEmpty(endValue) ? startValue : endValue);
-        string text = result.Groups["romaji"].Value;
-
-        return new RomajiTag
+        string regex = generateRegex(char_index_range_str, new[]
         {
-            StartIndex = startIndex,
-            EndIndex = endIndex,
-            Text = text,
-        };
+            getStringPropertyRegex(':', "romaji"),
+        });
+
+        return getMatchByStatement<RomajiTag>(str, regex, result =>
+        {
+            if (result == null)
+                return new RomajiTag();
+
+            int startIndex = result.GetGroupValue<int>("start");
+            int? endIndex = result.GetGroupValue<int?>("end");
+            string text = result.GetGroupValue<string>("romaji");
+
+            return new RomajiTag
+            {
+                StartIndex = startIndex,
+                EndIndex = endIndex ?? startIndex,
+                Text = text,
+            };
+        });
     }
 
     /// <summary>
     /// Process test case time tag string format into <see cref="TimeTag"/>
     /// </summary>
     /// <example>
-    /// [0,start]:1000
+    /// [0,start]:1000              -> has time-tag index with time. <br/>
+    /// [0,start]                   -> has time-tag index with no time. <br/>
+    /// [0,start]:                  -> has time-tag index with no time. <br/>
     /// </example>
     /// <param name="str">Time tag string format</param>
     /// <returns><see cref="TimeTag"/>Time tag object</returns>
     public static TimeTag ParseTimeTag(string? str)
     {
-        if (string.IsNullOrEmpty(str))
-            return new TimeTag(new TextIndex());
+        string regex = generateRegex(time_index_regex, new[]
+        {
+            getNumberPropertyRegex(':', "time"),
+        });
 
-        var regex = new Regex("(?<index>[-0-9]+),(?<state>start|end)]:(?<time>[-0-9]+|s*|)");
-        var result = regex.Match(str);
-        if (!result.Success)
-            throw new RegexMatchTimeoutException(nameof(str));
+        return getMatchByStatement<TimeTag>(str, regex, result =>
+        {
+            if (result == null)
+                return new TimeTag(new TextIndex());
 
-        int index = result.GetGroupValue<int>("index");
-        var state = result.GetGroupValue<string>("state") == "start" ? TextIndex.IndexState.Start : TextIndex.IndexState.End;
-        int? time = result.GetGroupValue<int?>("time");
+            int index = result.GetGroupValue<int>("index");
+            var state = result.GetGroupValue<string>("state") == "start" ? TextIndex.IndexState.Start : TextIndex.IndexState.End;
+            int? time = result.GetGroupValue<int?>("time");
 
-        return new TimeTag(new TextIndex(index, state), time);
+            return new TimeTag(new TextIndex(index, state), time);
+        });
     }
 
     /// <summary>
     /// Process test case text index string format into <see cref="TextIndex"/>
     /// </summary>
     /// <example>
-    /// [0,start]
+    /// [0,start]   -> has time-tag index with time. <br/>
+    /// [0,end]     -> has time-tag index with time. <br/>
     /// </example>
     /// <param name="str">Text tag string format</param>
     /// <returns><see cref="TimeTag"/>Text tag object</returns>
     public static TextIndex ParseTextIndex(string? str)
     {
-        if (string.IsNullOrEmpty(str))
-            return new TextIndex();
+        string regex = generateRegex(time_index_regex, Array.Empty<string>());
 
-        var regex = new Regex("(?<index>[-0-9]+),(?<state>start|end)]");
-        var result = regex.Match(str);
-        if (!result.Success)
-            throw new RegexMatchTimeoutException(nameof(str));
+        return getMatchByStatement(str, regex, result =>
+        {
+            if (result == null)
+                return new TextIndex();
 
-        int index = result.GetGroupValue<int>("index");
-        var state = result.GetGroupValue<string>("state") == "start" ? TextIndex.IndexState.Start : TextIndex.IndexState.End;
+            int index = result.GetGroupValue<int>("index");
+            var state = result.GetGroupValue<string>("state") == "start" ? TextIndex.IndexState.Start : TextIndex.IndexState.End;
 
-        return new TextIndex(index, state);
+            return new TextIndex(index, state);
+        });
     }
 
     /// <summary>
     /// Process test case lyric string format into <see cref="Lyric"/>
     /// </summary>
     /// <example>
-    /// [1000,3000]:karaoke
+    /// [1000,3000]:karaoke     -> has time-range and lyric. <br/>
+    /// [1000,3000]:            -> has time-range. <br/>
+    /// [1000,3000]             -> has time-range. <br/>
     /// </example>
     /// <param name="str">Lyric string format</param>
     /// <param name="id">Id if needed</param>
     /// <returns><see cref="Lyric"/>Lyric object</returns>
     public static Lyric ParseLyric(string str, int? id = null)
     {
-        if (string.IsNullOrEmpty(str))
-            return new Lyric();
-
-        var regex = new Regex("(?<startTime>[-0-9]+),(?<endTime>[-0-9]+)]:(?<lyric>.*$)");
-        var result = regex.Match(str);
-        if (!result.Success)
-            throw new RegexMatchTimeoutException(nameof(str));
-
-        double startTime = result.GetGroupValue<double>("startTime");
-        double endTime = result.GetGroupValue<double>("endTime");
-        string text = result.GetGroupValue<string>("lyric");
-
-        return new Lyric
+        string regex = generateRegex(time_range_str, new[]
         {
-            StartTime = startTime,
-            Duration = endTime - startTime,
-            Text = text,
-            TimeTags = new[]
+            getStringPropertyRegex(':', "lyric"),
+        });
+
+        return getMatchByStatement<Lyric>(str, regex, result =>
+        {
+            if (result == null)
+                return new Lyric();
+
+            double startTime = result.GetGroupValue<double>("startTime");
+            double endTime = result.GetGroupValue<double>("endTime");
+            string text = result.GetGroupValue<string>("lyric");
+
+            return new Lyric
             {
-                new TimeTag(new TextIndex(0), startTime),
-                new TimeTag(new TextIndex(text.Length - 1, TextIndex.IndexState.End), endTime),
-            },
-        }.ChangeId(id != null ? TestCaseElementIdHelper.CreateElementIdByNumber(id.Value) : ElementId.Empty);
+                StartTime = startTime,
+                Duration = endTime - startTime,
+                Text = text,
+                TimeTags = new[]
+                {
+                    new TimeTag(new TextIndex(0), startTime),
+                    new TimeTag(new TextIndex(text.Length - 1, TextIndex.IndexState.End), endTime),
+                },
+            }.ChangeId(id != null ? TestCaseElementIdHelper.CreateElementIdByNumber(id.Value) : ElementId.Empty);
+        });
     }
 
     /// <summary>
@@ -202,26 +249,26 @@ public static class TestCaseTagHelper
     /// Process test case singer string format into <see cref="Singer"/>
     /// </summary>
     /// <example>
-    /// [0]name:singer001
-    /// [0]romaji:singer001
-    /// [0]eg:singer001
+    /// [0]name:singer001       -> singer with id and name. <br/>
+    /// [0]romaji:singer001     -> singer with id and romaji. <br/>
+    /// [0]eg:singer001         -> singer with id and english name. <br/>
     /// </example>
     /// <param name="str">Singer string format</param>
     /// <returns><see cref="Singer"/>sSinger object</returns>
     public static Singer ParseSinger(string? str)
     {
-        if (string.IsNullOrEmpty(str))
-            return new Singer().ChangeId(ElementId.Empty);
+        string regex = generateRegex(id_str, Array.Empty<string>());
 
-        var regex = new Regex("(?<id>[-0-9]+)]");
-        var result = regex.Match(str);
-        if (!result.Success)
-            throw new RegexMatchTimeoutException(nameof(str));
+        return getMatchByStatement<Singer>(str, regex, result =>
+        {
+            if (result == null)
+                return new Singer().ChangeId(ElementId.Empty);
 
-        // todo : implementation
-        int id = result.GetGroupValue<int>("id");
+            // todo : implementation
+            int id = result.GetGroupValue<int>("id");
 
-        return new Singer().ChangeId(id);
+            return new Singer().ChangeId(id);
+        });
     }
 
     public static RubyTag[] ParseRubyTags(IEnumerable<string?> strings)
