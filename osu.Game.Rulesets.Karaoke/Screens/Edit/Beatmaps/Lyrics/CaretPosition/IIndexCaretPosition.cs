125e325d9368b03afe271d31be3615bf276b424e
andy840119 <andy840119@gmail.com>
2023-08-13 10:30:35 +0800

    Add interface to mark that all index caret position should be possible to compare the index. Because we need to get the left side and the right side of the caret position if drag the range of index in the future.

--- osu.Game.Rulesets.Karaoke/Screens/Edit/Beatmaps/Lyrics/CaretPosition/IIndexCaretPosition.cs
+++ osu.Game.Rulesets.Karaoke/Screens/Edit/Beatmaps/Lyrics/CaretPosition/IIndexCaretPosition.cs
@@ -1,8 +1,21 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+
 namespace osu.Game.Rulesets.Karaoke.Screens.Edit.Beatmaps.Lyrics.CaretPosition;
 
-public interface IIndexCaretPosition : ICaretPosition
+public interface IIndexCaretPosition : ICaretPosition, IComparable<IIndexCaretPosition>
 {
+    public static bool operator >(IIndexCaretPosition caretPosition1, IIndexCaretPosition caretPosition2)
+        => caretPosition1.CompareTo(caretPosition2) > 0;
+
+    public static bool operator >=(IIndexCaretPosition caretPosition1, IIndexCaretPosition caretPosition2)
+        => caretPosition1.CompareTo(caretPosition2) >= 0;
+
+    public static bool operator <(IIndexCaretPosition caretPosition1, IIndexCaretPosition caretPosition2)
+        => caretPosition1.CompareTo(caretPosition2) < 0;
+
+    public static bool operator <=(IIndexCaretPosition caretPosition1, IIndexCaretPosition caretPosition2)
+        => caretPosition1.CompareTo(caretPosition2) <= 0;
 }
