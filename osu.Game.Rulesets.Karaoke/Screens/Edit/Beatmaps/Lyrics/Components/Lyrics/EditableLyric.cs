164baefe9cbf8ba053a713189bb61f17de5c356f
andy840119 <andy840119@gmail.com>
2023-09-22 21:47:13 +0800

    Classification events.

--- osu.Game.Rulesets.Karaoke/Screens/Edit/Beatmaps/Lyrics/Components/Lyrics/EditableLyric.cs
+++ osu.Game.Rulesets.Karaoke/Screens/Edit/Beatmaps/Lyrics/Components/Lyrics/EditableLyric.cs
@@ -46,6 +46,8 @@ public partial class EditableLyric : InteractableLyric, IEditableLyricState
         InternalChildren.OfType<BaseLayer>().ForEach(x => x.TriggerDisallowEditEffect(BindableMode.Value));
     }
 
+    #region Hover
+
     protected override bool OnMouseMove(MouseMoveEvent e)
     {
         if (!lyricCaretState.CaretEnabled)
@@ -70,6 +72,30 @@ public partial class EditableLyric : InteractableLyric, IEditableLyricState
 
         return base.OnMouseMove(e);
     }
+
+    protected override void OnHoverLost(HoverLostEvent e)
+    {
+        base.OnHoverLost(e);
+
+        if (!lyricCaretState.CaretEnabled)
+            return;
+
+        // lost hover caret and time-tag caret
+        lyricCaretState.ClearHoverCaretPosition();
+    }
+
+    #endregion
+
+    #region Click
+
+    protected override bool OnClick(ClickEvent e)
+    {
+        return lyricCaretState.ConfirmHoverCaretPosition();
+    }
+
+    #endregion
+
+    #region Drag
 
     protected override bool OnDragStart(DragStartEvent e)
     {
@@ -122,21 +148,7 @@ public partial class EditableLyric : InteractableLyric, IEditableLyricState
             _ => null,
         };
 
-    protected override void OnHoverLost(HoverLostEvent e)
-    {
-        base.OnHoverLost(e);
-
-        if (!lyricCaretState.CaretEnabled)
-            return;
-
-        // lost hover caret and time-tag caret
-        lyricCaretState.ClearHoverCaretPosition();
-    }
-
-    protected override bool OnClick(ClickEvent e)
-    {
-        return lyricCaretState.ConfirmHoverCaretPosition();
-    }
+    #endregion
 
     protected override bool OnDoubleClick(DoubleClickEvent e)
     {
