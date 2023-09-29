ceaf3a33f1ba33af27312eb6d062e0719a2ffe56
andy840119 <andy840119@gmail.com>
2023-09-29 17:11:27 +0800

    Remove the base text-tag class.

--- osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/Lyrics/ILyricRubyTagsChangeHandler.cs
+++ osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/Lyrics/ILyricRubyTagsChangeHandler.cs
@@ -1,10 +1,16 @@
 // Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using osu.Game.Rulesets.Karaoke.Objects;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers.Lyrics;
 
-public interface ILyricRubyTagsChangeHandler : ILyricTextTagsChangeHandler<RubyTag>
+public interface ILyricRubyTagsChangeHandler : ILyricListPropertyChangeHandler<RubyTag>
 {
+    void SetIndex(RubyTag rubyTag, int? startIndex, int? endIndex);
+
+    void ShiftingIndex(IEnumerable<RubyTag> rubyTags, int offset);
+
+    void SetText(RubyTag rubyTag, string text);
 }
