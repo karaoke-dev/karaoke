14bbd8f1c3853387eb8406da387869d0f75f1293
andy840119 <andy840119@gmail.com>
2023-12-17 19:45:14 +0800

    Issue tooltip should have it's own text flow container. Follow how "TextLink" did in the osu.game to create the customized text part.

--- osu.Game.Rulesets.Karaoke/Screens/Edit/Components/Issues/IssuesToolTip.cs
+++ osu.Game.Rulesets.Karaoke/Screens/Edit/Components/Issues/IssuesToolTip.cs
@@ -1,12 +1,15 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
-using System.ComponentModel;
+using System.Collections.Generic;
+using osu.Framework.Allocation;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Containers;
 using osu.Game.Rulesets.Edit.Checks.Components;
-using osu.Game.Rulesets.Karaoke.Graphics.Containers;
 using osu.Game.Rulesets.Karaoke.Graphics.Cursor;
 using osuTK;
 using osuTK.Graphics;
@@ -15,11 +18,11 @@ namespace osu.Game.Rulesets.Karaoke.Screens.Edit.Components.Issues;
 
 public partial class IssuesToolTip : BackgroundToolTip<Issue[]>
 {
-    private readonly MessageContainer invalidMessage;
+    private readonly MessageTextFlowContainer invalidMessage;
 
     public IssuesToolTip()
     {
-        Child = invalidMessage = new MessageContainer(s => s.Font = s.Font.With(size: 14))
+        Child = invalidMessage = new MessageTextFlowContainer
         {
             Width = 300,
             AutoSizeAxes = Axes.Y,
@@ -29,7 +32,7 @@ public partial class IssuesToolTip : BackgroundToolTip<Issue[]>
         };
     }
 
-    private Issue[] lastIssues = Array.Empty<Issue>();
+    private Issue[]? lastIssues;
 
     public override void SetContent(Issue[] issues)
     {
@@ -41,23 +44,67 @@ public partial class IssuesToolTip : BackgroundToolTip<Issue[]>
         // clear exist warning.
         invalidMessage.Clear();
 
-        foreach (var issue in issues)
-        {
-            switch (issue)
-            {
-                // print normal message
-                case Issue:
-                    invalidMessage.AddAlertParagraph(issue.Template.GetMessage());
-                    break;
-
-                // Should throw exception because every issue message should be printed.
-                default:
-                    throw new InvalidEnumArgumentException(nameof(issue));
-            }
-        }
-
         // show no problem message
         if (issues.Length == 0)
+        {
             invalidMessage.AddSuccessParagraph("Seems no issue in this lyric.");
+        }
+        else
+        {
+            foreach (var issue in issues)
+            {
+                invalidMessage.AddIssueParagraph(issue);
+            }
+        }
+    }
+
+    private partial class MessageTextFlowContainer : OsuTextFlowContainer
+    {
+        private const int font_size = 14;
+
+        [Resolved]
+        private OsuColour colours { get; set; } = null!;
+
+        public MessageTextFlowContainer()
+            : base(s => s.Font = s.Font.With(size: font_size))
+        {
+        }
+
+        public void AddIssueParagraph(Issue issue)
+        {
+            NewParagraph();
+
+            AddPart(new IssueIconTextPart(issue));
+            AddText($" {issue}");
+        }
+
+        public void AddSuccessParagraph(string text)
+        {
+            NewParagraph();
+            AddIcon(FontAwesome.Solid.Check, icon =>
+            {
+                icon.Colour = colours.Green;
+            });
+            AddText($" {text}");
+        }
+
+        private class IssueIconTextPart : TextPart
+        {
+            private readonly Issue issue;
+
+            public IssueIconTextPart(Issue issue)
+            {
+                this.issue = issue;
+            }
+
+            protected override IEnumerable<Drawable> CreateDrawablesFor(TextFlowContainer textFlowContainer)
+            {
+                yield return new IssueIcon
+                {
+                    Size = new Vector2(font_size),
+                    Issue = issue,
+                };
+            }
+        }
     }
 }
