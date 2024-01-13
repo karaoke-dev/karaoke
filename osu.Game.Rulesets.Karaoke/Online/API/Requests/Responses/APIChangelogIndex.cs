65e67bd1051a67292ce46b3cb3da0d30e90f9c60
andy840119 <andy840119@gmail.com>
2024-01-13 17:04:06 +0800

    Add the comment.

--- osu.Game.Rulesets.Karaoke/Online/API/Requests/Responses/APIChangelogIndex.cs
+++ osu.Game.Rulesets.Karaoke/Online/API/Requests/Responses/APIChangelogIndex.cs
@@ -8,9 +8,18 @@ namespace osu.Game.Rulesets.Karaoke.Online.API.Requests.Responses;
 
 public class APIChangelogIndex
 {
+    /// <summary>
+    /// All available builds with no content.
+    /// </summary>
     public List<APIChangelogBuild> Builds { get; set; } = new();
 
+    /// <summary>
+    /// All preview builds display in the main page.
+    /// </summary>
     public List<APIChangelogBuild> PreviewBuilds { get; set; } = new();
 
+    /// <summary>
+    /// All available years that will be shown in the sidebar.
+    /// </summary>
     public int[] Years { get; set; } = Array.Empty<int>();
 }
