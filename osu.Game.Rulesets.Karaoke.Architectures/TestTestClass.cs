103631fdff64715fd7d801379efb0b2c6ee5da03
andy840119 <andy840119@gmail.com>
2024-05-19 11:58:48 +0800

    Add the test class to check the assert using.

--- osu.Game.Rulesets.Karaoke.Architectures/TestTestClass.cs
+++ osu.Game.Rulesets.Karaoke.Architectures/TestTestClass.cs
@@ -1,10 +1,13 @@
 // Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using System.Linq;
+using ArchUnitNET.Domain;
 using ArchUnitNET.Domain.Extensions;
 using NUnit.Framework;
 using osu.Framework.Testing;
+using osu.Game.Rulesets.Karaoke.Tests;
 using osu.Game.Tests.Visual;
 
 namespace osu.Game.Rulesets.Karaoke.Architectures;
@@ -71,6 +74,27 @@ public class TestTestClass : BaseTest
                 {
                     Assert.IsTrue(testMethod.NameStartsWith("Test"), $"Test method {testMethod} should start with 'Test'.");
                 }
+            }
+        });
+    }
+
+    [Test]
+    [Project.KaraokeTest(true)]
+    public void CheckAssertion()
+    {
+        var architecture = GetProjectArchitecture();
+        var tests = architecture.GetAllTestClass();
+        var assert = architecture.GetClassOfType(typeof(Assert));
+
+        Assertion(() =>
+        {
+            foreach (var test in tests)
+            {
+                var assertions = test.GetCalledMethods().Where(x => EqualityComparer<IType>.Default.Equals(x.DeclaringType, assert)).ToArray();
+
+                // Assertion.
+                Assert.IsFalse(assertions.Any(x => x.NameStartsWith("True")), $"Should use {nameof(Assert.IsTrue)} instead of {nameof(Assert.True)} in the {test}.");
+                Assert.IsFalse(assertions.Any(x => x.NameStartsWith("False")), $"Should use {nameof(Assert.IsFalse)} instead of {nameof(Assert.False)} in the {test}.");
             }
         });
     }
