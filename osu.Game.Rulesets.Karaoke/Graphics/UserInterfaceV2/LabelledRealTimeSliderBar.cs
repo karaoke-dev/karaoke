388f8c143c3d442528e02c1c9c6d3d6c35d6e237
andy840119 <andy840119@gmail.com>
2024-05-22 22:00:29 +0800

    Upgrade the lazer version and fix the breaking change.
    see:
    https://github.com/ppy/osu/commit/d7d569cf4e68acdbcc9cec844337f93bdb207a54

--- osu.Game.Rulesets.Karaoke/Graphics/UserInterfaceV2/LabelledRealTimeSliderBar.cs
+++ osu.Game.Rulesets.Karaoke/Graphics/UserInterfaceV2/LabelledRealTimeSliderBar.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Numerics;
+using System;
 using osu.Framework.Graphics;
 using osu.Game.Graphics.UserInterfaceV2;
 using osu.Game.Overlays.Settings;
@@ -9,7 +9,7 @@ using osu.Game.Overlays.Settings;
 namespace osu.Game.Rulesets.Karaoke.Graphics.UserInterfaceV2;
 
 public partial class LabelledRealTimeSliderBar<TNumber> : LabelledSliderBar<TNumber>
-    where TNumber : struct, INumber<TNumber>, IMinMaxValue<TNumber>
+    where TNumber : struct, IEquatable<TNumber>, IComparable<TNumber>, IConvertible
 {
     protected override SettingsSlider<TNumber> CreateComponent()
         => base.CreateComponent().With(x => x.TransferValueOnCommit = false);
