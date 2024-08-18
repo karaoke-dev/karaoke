c8df918931bcc07282dbccd5e6cd450c4b824ab1
andy840119 <andy840119@gmail.com>
2024-08-18 19:46:13 +0800

    Remove the action toolbar.

--- osu.Game.Rulesets.Karaoke/Screens/Edit/Beatmaps/Lyrics/Content/Compose/Toolbar/KeyActionButton.cs
+++ osu.Game.Rulesets.Karaoke/Screens/Edit/Beatmaps/Lyrics/Content/Compose/Toolbar/KeyActionButton.cs
@@ -9,7 +9,7 @@ namespace osu.Game.Rulesets.Karaoke.Screens.Edit.Beatmaps.Lyrics.Content.Compose
 /// <summary>
 /// Button that able to receive the <see cref="KaraokeEditAction"/> event.
 /// </summary>
-public abstract partial class KeyActionButton : ActionButton, IKeyBindingHandler<KaraokeEditAction>, IHasIKeyBindingHandlerOrder
+public abstract partial class KeyActionButton : ToolbarButton, IKeyBindingHandler<KaraokeEditAction>, IHasIKeyBindingHandlerOrder
 {
     protected abstract KaraokeEditAction EditAction { get; }
 
