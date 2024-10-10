77ec46f82e1f191a9bfa391b25adf0b4fdc5c4f1
andy840119 <andy840119@gmail.com>
2024-10-10 17:38:33 +0800

    Create command generator for the preview lyric.

--- osu.Game.Rulesets.Karaoke/Screens/Settings/Previews/Gameplay/PreviewLyricStageElement.cs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
-// See the LICENCE file in the repository root for full licence text.
-
-using osu.Game.Rulesets.Karaoke.Stages;
-
-namespace osu.Game.Rulesets.Karaoke.Screens.Settings.Previews.Gameplay;
-
-public class PreviewLyricStageElement : StageElement;
