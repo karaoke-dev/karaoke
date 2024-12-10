d12293d7d30f5b4e8191359888540e3323af1f1d
andy840119 <andy840119@gmail.com>
2024-12-10 21:17:01 +0800

    feat(stage): P1.3 - Replace the object id into class instance. Also, give the category a default value.

--- osu.Game.Rulesets.Karaoke/Stages/Infos/Preview/PreviewStyleCategory.cs
+++ osu.Game.Rulesets.Karaoke/Stages/Infos/Preview/PreviewStyleCategory.cs
@@ -1,6 +1,7 @@
 // Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Game.Rulesets.Karaoke.Graphics.Sprites;
 using osu.Game.Rulesets.Karaoke.Objects;
 
 namespace osu.Game.Rulesets.Karaoke.Stages.Infos.Preview;
@@ -8,5 +9,8 @@ namespace osu.Game.Rulesets.Karaoke.Stages.Infos.Preview;
 public class PreviewStyleCategory : StageElementCategory<PreviewStyle, Lyric>
 {
     protected override PreviewStyle CreateDefaultElement()
-        => new();
+        => new()
+        {
+            LyricStyle = LyricStyle.CreateDefault(),
+        };
 }
