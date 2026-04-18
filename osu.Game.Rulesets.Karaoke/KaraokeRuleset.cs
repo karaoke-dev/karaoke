ea521109b6bdc150ee5acb0f64b5b1a715816639
andy840119 <andy840119@gmail.com>
2026-04-18 15:13:45 +0800

    Remove the beatmap info component because base component in the osu.Game is deleted.
    see:
    - https://github.com/ppy/osu/pull/36752/changes

--- osu.Game.Rulesets.Karaoke/KaraokeRuleset.cs
+++ osu.Game.Rulesets.Karaoke/KaraokeRuleset.cs
@@ -249,11 +249,6 @@ public partial class KaraokeRuleset : Ruleset
         // Always display song info
         var statistic = new List<StatisticItem>
         {
-            new("Info", () => new BeatmapInfoGraph(playableBeatmap)
-            {
-                RelativeSizeAxes = Axes.X,
-                Height = info_height,
-            }),
             new("Metadata", () => new BeatmapMetadataGraph(playableBeatmap)
             {
                 RelativeSizeAxes = Axes.X,
